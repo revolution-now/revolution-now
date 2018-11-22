449c0370bd0bb67327c9d6fe7d907a7ecfca69d4
David P. Sicilia <dpsicilia@gmail.com>
2018-11-22 13:33:08 -0500

    Cleaning up some SDL CMake stuff

--- cmake/sdl2-helper.cmake
+++ cmake/sdl2-helper.cmake
@@ -1,12 +1,14 @@
-# This function sets <name>_LIBRARY and <name>_INCLUDE_DIR.
+# This function sets <name>_LIBRARY and <name>_INCLUDE_DIR but
+# where <name> is made uppercase.
 function( find_sdl2_lib name )
     find_package( ${name} REQUIRED )
     if( NOT "${${name}_FOUND}" )
         message( FATAL_ERROR "Cannot find ${name}." )
     endif()
-    string( TOUPPER ${name} UPPER_NAME )
-    message( STATUS
-      "${UPPER_NAME}_INCLUDE_DIR: ${${UPPER_NAME}_INCLUDE_DIR}" )
-    message( STATUS
-      "${UPPER_NAME}_LIBRARY: ${${UPPER_NAME}_LIBRARY}" )
+    # For debugging
+    #string( TOUPPER ${name} UPPER_NAME )
+    #message( STATUS
+    #  "${UPPER_NAME}_INCLUDE_DIR: ${${UPPER_NAME}_INCLUDE_DIR}" )
+    #message( STATUS
+    #  "${UPPER_NAME}_LIBRARY: ${${UPPER_NAME}_LIBRARY}" )
 endfunction()
