8224585f8595ce2eaf1293dadfbedc264644916e
David P. Sicilia <dpsicilia@gmail.com>
2018-12-11 18:55:34 -0500

    Restore main to window test

--- src/main.cpp
+++ src/main.cpp
@@ -45,59 +45,24 @@ void game() {
 
   //(void)create_unit_on_map( e_unit_type::free_colonist, 2_y,
   //                          3_x );
-  (void)create_unit_on_map( e_unit_type::free_colonist, 2_y,
-                            4_x );
-  (void)create_unit_on_map( e_unit_type::caravel, 2_y, 2_x );
+  //(void)create_unit_on_map( e_unit_type::free_colonist, 2_y,
+  //                          4_x );
+  //(void)create_unit_on_map( e_unit_type::caravel, 2_y, 2_x );
   //(void)create_unit_on_map( e_unit_type::caravel, 2_y, 1_x );
 
-  while( turn() != e_turn_result::quit ) {}
+  // while( turn() != e_turn_result::quit ) {}
 
   // font_test();
-  // gui::test_window();
+  gui::test_window();
 
   cleanup();
-}
-
-template<typename T>
-ostream& operator<<( ostream& out, std::vector<T> v ) {
-  out << "[";
-  bool need_comma = false;
-  for( auto const& item : v ) {
-    if( need_comma ) out << ",";
-    need_comma = true;
-    out << item;
-  }
-  out << "]";
-  return out;
 }
 
 } // namespace rn
 
 int main( int /*unused*/, char** /*unused*/ ) try {
-  init_logging( spdlog::level::debug );
+  init_logging( spdlog::level::info );
   load_configs();
-  // clang-format off
-  logger->info( "config/rn.ucl.hello:                      {}", util::to_string( config_rn.hello                   ) );
-  logger->info( "config/rn.ucl.one:                        {}", util::to_string( config_rn.one                     ) );
-  logger->info( "config/rn.ucl.two:                        {}", util::to_string( config_rn.two                     ) );
-  logger->info( "config/rn.ucl.fruit.apples:               {}", util::to_string( config_rn.fruit.apples            ) );
-  logger->info( "config/rn.ucl.fruit.oranges:              {}", util::to_string( config_rn.fruit.oranges           ) );
-  logger->info( "config/rn.ucl.fruit.grapes:               {}", util::to_string( config_rn.fruit.grapes            ) );
-  logger->info( "config/rn.ucl.fruit.description:          {}", util::to_string( config_rn.fruit.description       ) );
-  logger->info( "config/rn.ucl.fruit.hello.world:          {}", util::to_string( config_rn.fruit.hello.world       ) );
-  logger->info( "config/window.ucl.game_title:             {}", util::to_string( config_window.game_title          ) );
-  logger->info( "config/window.ucl.game_version:           {}", util::to_string( config_window.game_version        ) );
-  logger->info( "config/window.ucl.coordinates:            {}", config_window.coordinates );
-  logger->info( "config/window.ucl.grapes:                 {}", util::to_string( *config_window.grapes             ) );
-  logger->info( "config/window.ucl.window_error.title:     {}", util::to_string( config_window.window_error.title  ) );
-  logger->info( "config/window.ucl.window_error.x_size:    {}", util::to_string( config_window.window_error.x_size ) );
-  logger->info( "config/window.ucl.window_error.show:      {}", util::to_string( config_window.window_error.show   ) );
-  logger->info( "config/window.ucl.window_error.positions: {}", config_window.window_error.positions );
-  logger->info( "config/window.ucl.window_error.world:     {}", util::to_string( *config_window.window_error.world ) );
-  logger->info( "config/window.ucl.widths:                 {}", util::to_string( config_window.widths              ) );
-  logger->info( "config/window.ucl.fruit->oranges:         {}", util::to_string( config_window.fruit->oranges      ) );
-
-  // clang-format on
   game();
   return 0;
 
