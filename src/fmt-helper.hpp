2baa9e587ee6eb0edd40bca40cdbf9fa25c29a13
David P. Sicilia <dpsicilia@gmail.com>
2019-01-10 17:28:06 -0500

    Improve variant formatting

--- src/fmt-helper.hpp
+++ src/fmt-helper.hpp
@@ -15,6 +15,9 @@
 // {fmt}
 #include "fmt/format.h"
 
+// base-util
+#include "base-util/misc.hpp"
+
 // C++ standard library
 #include <optional>
 #include <type_traits>
@@ -46,48 +49,17 @@
 #define DEFINE_FORMAT_( type, ... ) \
   DEFINE_FORMAT_IMPL( (void)o;, type, __VA_ARGS__ )
 
-namespace rn {
-
-// This function exists for the purpose of  having  the  compiler
-// deduce the Indexes variadic integer arguments that we can then
-// use  to  index  the variant; it probably is not useful to call
-// this method directly (it is called by to_string).
-template<typename Variant, size_t... Indexes>
-std::string variant_elems_to_string(
-    Variant const &v,
-    std::index_sequence<Indexes...> /*unused*/ ) {
-  std::string res;
-  // Unary right fold of template parameter pack.
-  ( ( res += ( Indexes == v.index() )
-                 ? ::fmt::format( "{}", std::get<Indexes>( v ) )
-                 : "" ),
-    ... );
-  return res;
-}
-
-template<typename... Args>
-std::string variant_to_string( std::variant<Args...> const &v ) {
-  auto is = std::make_index_sequence<sizeof...( Args )>();
-  return variant_elems_to_string( v, is );
-}
-
-} // namespace rn
-
 namespace fmt {
 
 // {fmt} formatter for formatting variants whose constituent
 // types are all so formattable.
 template<typename... Ts>
-struct formatter<std::variant<Ts...>> {
-  template<typename ParseContext>
-  constexpr auto parse( ParseContext &ctx ) {
-    return ctx.begin();
-  }
-  template<typename FormatContext>
-  auto format( std::variant<Ts...> const &o,
-               FormatContext &            ctx ) {
-    return format_to( ctx.begin(),
-                      ::rn::variant_to_string( o ) );
+struct formatter<std::variant<Ts...>> : dynamic_formatter<> {
+  using V = std::variant<Ts...>;
+  using B = dynamic_formatter<>;
+  template<typename Context>
+  auto format( V const &v, Context &ctx ) {
+    return std::visit( LC( B::format( _, ctx ) ), v );
   }
 };
 
