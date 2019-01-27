de11108b4872072f8e38cf3dc0b2d457f2ea23ca
David P. Sicilia <dpsicilia@gmail.com>
2019-01-27 14:57:42 -0500

    Add the scelta external module

---
+++ src/variant.hpp
@@ -0,0 +1,18 @@
+/****************************************************************
+**variant.hpp
+*
+* Project: Revolution Now
+*
+* Created by dsicilia on 2019-01-27.
+*
+* Description: Utilities for handling variants.
+*
+*****************************************************************/
+#pragma once
+
+#include "core-config.hpp"
+
+// Scelta
+#include <scelta.hpp>
+
+namespace rn {} // namespace rn
