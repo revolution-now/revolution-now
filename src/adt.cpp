5ed43e680cc6bcacffef0266aeff38864f4b65db
David P. Sicilia <dpsicilia@gmail.com>
2019-02-01 17:01:17 -0500

    Adding namespaces into ADT

--- src/adt.cpp
+++ src/adt.cpp
@@ -13,33 +13,35 @@
 // Revolution Now
 #include "variant.hpp"
 
-namespace rn {
+struct __marker_start {};
 
-namespace {} // namespace
-
-// struct __marker_start {};
-
-// ADT( MyAdtType,
+// ADT( Test,
 //     ( case_a ),                         //
 //     ( case_b ),                         //
 //     ( case_c, ( int, x ), ( int, y ) ), //
 //     ( case_d, ( double, f ) )           //
 //)
 
+struct __marker_end {};
+
+namespace rn {
+
+namespace {} // namespace
+
 void harness() {
-  // MyAdtType o;
-  // MyAdtType o2;
+  // Test_t o;
+  // Test_t o2;
 
   // if( o == o2 ) {}
 
-  // auto matcher = scelta::match( []( case_a ) {}, //
-  //                              []( case_b ) {}, //
-  //                              []( case_c c ) {
-  //                                (void)c.x;
-  //                                (void)c.y;
-  //                              },
-  //                              []( case_d d ) { (void)d.f; }
-  //                              );
+  // auto matcher =
+  //    scelta::match( []( Test::case_a ) {}, //
+  //                   []( Test::case_b ) {}, //
+  //                   []( Test::case_c c ) {
+  //                     (void)c.x;
+  //                     (void)c.y;
+  //                   },
+  //                   []( Test::case_d d ) { (void)d.f; } );
   // matcher( o );
 }
 
--- src/adt.hpp
+++ src/adt.hpp
@@ -20,13 +20,6 @@
 
 namespace rn {
 
-// ADT( MyAdtType,
-//     ( case_a ),               //
-//     ( case_b ),               //
-//     ( case_c, int x, int y ), //
-//     ( case_d, double f; )     //
-//)
-
 #define PAIR_TO_DECL( type, var ) type var
 #define PAIR_TO_DECL_TUPLE( a ) PAIR_TO_DECL a
 
@@ -50,9 +43,11 @@ namespace rn {
 #define ADT_MAKE_STRUCT_TUPLE( a ) ADT_MAKE_STRUCT a
 
 #define ADT_IMPL( name, ... )                       \
+  namespace name {                                  \
   PP_MAP_SEMI( ADT_MAKE_STRUCT_TUPLE, __VA_ARGS__ ) \
-  using name =                                      \
-      std::variant<PP_MAP_COMMAS( HEAD_TUPLE, __VA_ARGS__ )>;
+  }                                                 \
+  using name##_t = std::variant<PP_MAP_PREPEND_NS(  \
+      name, PP_MAP_COMMAS( HEAD_TUPLE, __VA_ARGS__ ) )>;
 
 #define ADT( ... ) EVAL( ADT_IMPL( __VA_ARGS__ ) )
 
