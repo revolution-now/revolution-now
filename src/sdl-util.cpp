4254cbbb46fdaad26638d5011ef95fcc0a1203c7
David P. Sicilia <dpsicilia@gmail.com>
2019-06-22 23:11:48 -0400

    Add logic to measure power state

--- src/sdl-util.cpp
+++ src/sdl-util.cpp
@@ -72,6 +72,38 @@ void check_compile_link_version(
   }
 }
 
+MachinePowerInfo machine_power_info() {
+  MachinePowerInfo info;
+
+  int  seconds_left, battery_percent;
+  auto status =
+      ::SDL_GetPowerInfo( &seconds_left, &battery_percent );
+  switch( status ) {
+    // Not plugged in, running on the battery.
+    case SDL_POWERSTATE_ON_BATTERY:
+      info.power_state = e_power_state::on_battery;
+      break;
+    // Plugged in, no battery available.
+    case SDL_POWERSTATE_NO_BATTERY:
+      info.power_state = e_power_state::plugged_no_battery;
+      break;
+    // Plugged in, charging battery.
+    case SDL_POWERSTATE_CHARGING:
+      info.power_state = e_power_state::plugged_charging;
+      break;
+    // Plugged in, battery charged.
+    case SDL_POWERSTATE_CHARGED:
+      info.power_state = e_power_state::plugged_charged;
+      break;
+    // Cannot determine power status.
+    case SDL_POWERSTATE_UNKNOWN:
+    default: info.power_state = e_power_state::unknown; break;
+  }
+  if( battery_percent != -1 )
+    info.battery_percentage = battery_percent;
+  return info;
+}
+
 ::SDL_Rect to_SDL( Rect const& rect ) {
   ::SDL_Rect res;
   res.x = rect.x._;
@@ -100,6 +132,11 @@ ND Rect from_SDL( ::SDL_Rect const& rect ) {
 void init_sdl() {
   CHECK( ::SDL_Init( SDL_INIT_EVERYTHING ) >= 0,
          "sdl could not initialize" );
+
+  auto power_info = machine_power_info();
+  lg.info( "Power Info: {}, Battery: {}%",
+           power_info.power_state,
+           power_info.battery_percentage );
 }
 
 // All the functions in this method should not cause problems
--- src/sdl-util.hpp
+++ src/sdl-util.hpp
@@ -15,6 +15,7 @@
 // Revolution Now
 #include "color.hpp"
 #include "coord.hpp"
+#include "enum.hpp"
 #include "util.hpp"
 
 // base-util
@@ -83,6 +84,24 @@ private:
 };
 
 using TextureRef = std::reference_wrapper<Texture>;
+
+enum class e_( power_state,        //
+               unknown,            //
+               on_battery,         //
+               plugged_no_battery, //
+               plugged_charging,   //
+               plugged_charged     //
+);
+
+struct MachinePowerInfo {
+  e_power_state power_state;
+  // If known, this will be populated; [0, 100].
+  Opt<int> battery_percentage{};
+};
+
+// This will return as much info about the power situation as the
+// system can give us.
+MachinePowerInfo machine_power_info();
 
 // Will throw an error is the game is being run with a different
 // major version of the given SDL library (e.g., mixer, ttf,
