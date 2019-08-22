5c6d60edd1796cedc53e17be38f5d3eee5dee899
David P. Sicilia <dpsicilia@gmail.com>
2019-08-22 19:39:35 -0400

    Use templated ADT for drag&drop state

--- src/dragdrop.hpp
+++ src/dragdrop.hpp
@@ -13,6 +13,7 @@
 #include "core-config.hpp"
 
 // Revolution Now
+#include "adt.hpp"
 #include "aliases.hpp"
 #include "coord.hpp"
 #include "input.hpp"
@@ -25,6 +26,23 @@
 // C++ standard library
 #include <variant>
 
+ADT_T( rn,                                       //
+       TEMPLATE( DragSrcT, DragDstT, DragArcT ), //
+       DragState,                                //
+       ( none ),                                 //
+       ( in_progress,                            //
+         ( DragSrcT, src ),                      //
+         ( Opt<DragDstT>, dst ),                 //
+         ( Opt<Texture>, tx ) ),                 //
+       ( complete,                               //
+         ( DragArcT, arc ) ),                    //
+       ( rubber_band,                            //
+         ( Coord, current ),                     //
+         ( Coord, dest ),                        //
+         ( double, percent ),                    //
+         ( Opt<Texture>, tx ) )                  //
+);
+
 namespace rn {
 
 // To be used as a base class in the CRTP.
@@ -32,34 +50,8 @@ template<typename Child, typename DragSrcT, typename DragDstT,
          typename DragArcT>
 class DragAndDrop {
 protected:
-  // FIXME: make this an ADT outside class, but need templates.
-  struct DragState {
-    struct none {};
-    struct in_progress {
-      DragSrcT      src;
-      Opt<DragDstT> dst;
-      Opt<Texture>  tx;
-    };
-    struct complete {
-      DragArcT arc;
-    };
-    struct rubber_band {
-      Coord        current;
-      Coord        dest;
-      double       percent;
-      Opt<Texture> tx;
-    };
-  };
-  // The `none` state should be first.
-  using DragState_t = std::variant<    //
-      typename DragState::none,        //
-      typename DragState::in_progress, //
-      typename DragState::complete,    //
-      typename DragState::rubber_band  //
-      >;
-
 public:
-  DragAndDrop() : state_{typename DragState::none{}} {}
+  DragAndDrop() : state_{NoneT{}} {}
 
   Child const& child() const {
     return *static_cast<Child const*>( this );
@@ -67,28 +59,27 @@ public:
   Child& child() { return *static_cast<Child*>( this ); }
 
   void handle_on_frame_start() {
-    if_v( state_, typename DragState::complete, p_arc ) {
+    if_v( state_, CompleteT, p_arc ) {
       child().perform_drag( p_arc->arc );
-      state_ = typename DragState::none{};
+      state_ = NoneT{};
       return;
     }
-    if_v( state_, typename DragState::rubber_band, p_band ) {
+    if_v( state_, RubberBandT, p_band ) {
       p_band->percent += 0.15;
-      if( p_band->percent > 1.0 )
-        state_ = typename DragState::none{};
+      if( p_band->percent > 1.0 ) state_ = NoneT{};
     }
   }
 
   void handle_draw( Texture& tx ) const {
     switch_( state_ ) {
-      case_( typename DragState::in_progress ) {
+      case_( InProgressT ) {
         if( val.tx ) {
           auto mouse_pos = input::current_mouse_position();
           copy_texture( *val.tx, tx,
                         mouse_pos - val.tx->size() / Scale{2} );
         }
       }
-      case_( typename DragState::rubber_band ) {
+      case_( RubberBandT ) {
         CHECK( val.tx.has_value() );
         auto  mouse_pos = input::current_mouse_position();
         auto  delta     = val.dest - val.current;
@@ -103,7 +94,7 @@ public:
   }
 
   Plane::DragInfo handle_can_drag( Coord origin ) {
-    CHECK( util::holds<typename DragState::none>( state_ ) );
+    CHECK( util::holds<NoneT>( state_ ) );
     auto maybe_drag_in_progress = try_drag_start( origin );
     if( maybe_drag_in_progress ) {
       state_ = std::move( *maybe_drag_in_progress );
@@ -113,22 +104,20 @@ public:
   }
 
   void handle_on_drag( Coord current ) {
-    if_v( state_, typename DragState::in_progress,
-          in_progress ) {
+    if_v( state_, InProgressT, in_progress ) {
       in_progress->dst = child().drag_dst( current );
     }
   }
 
   bool handle_on_drag_finished( Coord const& drag_start,
                                 Coord const& drag_end ) {
-    if_v( state_, typename DragState::in_progress,
-          in_progress ) {
+    if_v( state_, InProgressT, in_progress ) {
       if( in_progress->dst ) {
         auto maybe_drag_arc = child().drag_arc(
             in_progress->src, *in_progress->dst );
         if( maybe_drag_arc &&
             child().can_perform_drag( *maybe_drag_arc ) ) {
-          state_ = typename DragState::complete{
+          state_ = CompleteT{
               /*arc=*/*maybe_drag_arc //
           };
           return true;
@@ -136,14 +125,14 @@ public:
       }
 
       if( in_progress->tx.has_value() ) {
-        state_ = typename DragState::rubber_band{
+        state_ = RubberBandT{
             /*current=*/drag_end,
             /*dest=*/drag_start,
             /*percent=*/0.0,
             /*<Texture> tx=*/std::move( in_progress->tx ),
         };
       } else {
-        state_ = typename DragState::none{};
+        state_ = NoneT{};
       }
       return true;
     }
@@ -151,14 +140,22 @@ public:
   }
 
 private:
-  Opt<typename DragState::in_progress> try_drag_start(
-      Coord const& origin ) const {
-    Opt<typename DragState::in_progress> res;
+  using NoneT = DragState::none<DragSrcT, DragDstT, DragArcT>;
+  using InProgressT =
+      DragState::in_progress<DragSrcT, DragDstT, DragArcT>;
+  using CompleteT =
+      DragState::complete<DragSrcT, DragDstT, DragArcT>;
+  using RubberBandT =
+      DragState::rubber_band<DragSrcT, DragDstT, DragArcT>;
+
+private:
+  Opt<InProgressT> try_drag_start( Coord const& origin ) const {
+    Opt<InProgressT> res;
     auto maybe_being_dragged = child().drag_src( origin );
     if( maybe_being_dragged ) {
       auto const& drag_src = *maybe_being_dragged;
 
-      res = typename DragState::in_progress{
+      res = InProgressT{
           /*src=*/drag_src,
           /*dst=*/std::nullopt,
           /*tx=*/child().draw_dragged_item( drag_src ),
@@ -168,7 +165,7 @@ private:
   }
 
 private:
-  DragState_t state_;
+  DragState_t<DragSrcT, DragDstT, DragArcT> state_;
 };
 
 } // namespace rn
