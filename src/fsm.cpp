f66efcd3d8a927b4261f80c545642bd7934b9965
David P. Sicilia <dpsicilia@gmail.com>
2019-09-05 19:11:57 -0400

    Adding support for event transitions for fsm

--- src/fsm.cpp
+++ src/fsm.cpp
@@ -11,7 +11,9 @@
 #include "fsm.hpp"
 
 // Revolution Now
+#include "adt.hpp"
 #include "fmt-helper.hpp"
+#include "logging.hpp"
 
 // C++ standard library
 #include <string>
@@ -21,17 +23,93 @@ using namespace std;
 
 namespace rn {
 
-namespace {} // namespace
+namespace {
+
+adt_rn_( BallState,           //
+         ( none ),            //
+         ( bouncing,          //
+           ( int, height ) ), //
+         ( rolling,           //
+           ( int, speed ) ),  //
+         ( spinning )         //
+);
+
+adt_rn_( BallEvent,           //
+         ( do_nothing ),      //
+         ( start_bouncing,    //
+           ( int, height ) ), //
+         ( stop_bouncing ),   //
+         ( start_rolling,     //
+           ( int, speed ) ),  //
+         ( stop_rolling ),    //
+         ( start_spinning ),  //
+         ( stop_spinning )    //
+);
+
+using BallTransitionMap = FSM_TRANSITIONS(
+    BallState,
+    BallEvent
+    // clang-format off
+   ,((none,     do_nothing    ),  /*->*/  none    )
+   ,((none,     start_spinning),  /*->*/  spinning)
+   ,((none,     start_rolling ),  /*->*/  rolling )
+   ,((none,     start_bouncing),  /*->*/  bouncing)
+   ,((rolling,  stop_rolling  ),  /*->*/  none    )
+   ,((spinning, stop_spinning ),  /*->*/  none    )
+   ,((bouncing, stop_bouncing ),  /*->*/  none    )
+    // clang-format on
+);
+
+struct BallFsm : public fsm<BallFsm, BallState_t, BallEvent_t,
+                            BallTransitionMap> {
+  using Parent::transition;
+
+  BallState_t transition( BallState::none const&,
+                          BallEvent::start_rolling const& event,
+                          BallState::rolling const& ) {
+    return BallState_t{BallState::rolling{event.speed}};
+  }
+
+  BallState_t transition( BallState::none const&,
+                          BallEvent::start_bouncing const& event,
+                          BallState::bouncing const& ) {
+    return BallState_t{BallState::bouncing{event.height}};
+  }
+};
+
+} // namespace
 
 void test_fsm() {
-  using TestVariant  = std::variant<int, double>;
-  using TestVariant2 = std::variant<long, string>;
+  BallFsm ball;
+  lg.info( "ball state: {}", ball.state() );
 
-  auto visitor = []( auto const& o1, auto const& o2 ) {
-    fmt::print( "{}-{}\n", o1, o2 );
-  };
+  ball.send_event( BallEvent::start_rolling{5} );
+  ball.process_events();
+  lg.info( "ball state: {}", ball.state() );
 
-  visit( visitor, TestVariant{}, TestVariant2{} );
+  ball.send_event( BallEvent::stop_rolling{} );
+  ball.process_events();
+  lg.info( "ball state: {}", ball.state() );
+
+  ball.send_event( BallEvent::start_spinning{} );
+  ball.process_events();
+  lg.info( "ball state: {}", ball.state() );
+
+  ball.send_event( BallEvent::stop_spinning{} );
+  ball.process_events();
+  lg.info( "ball state: {}", ball.state() );
+
+  ball.send_event( BallEvent::start_bouncing{4} );
+  ball.process_events();
+  lg.info( "ball state: {}", ball.state() );
+
+  ball.send_event( BallEvent::stop_bouncing{} );
+  ball.process_events();
+  lg.info( "ball state: {}", ball.state() );
+
+  ball.send_event( BallEvent::do_nothing{} );
+  ball.process_events();
+  lg.info( "ball state: {}", ball.state() );
 }
 
 } // namespace rn
--- src/fsm.hpp
+++ src/fsm.hpp
@@ -16,14 +16,24 @@
 #include "errors.hpp"
 #include "flat-queue.hpp"
 
+#include "base-util/pp.hpp"
+#include "base-util/type-map.hpp"
+
 // C++ standard library
 #include <variant>
 
 namespace rn {
 
-template<typename ChildT, typename StateT, typename EventT>
+template<typename ChildT, typename StateT, typename EventT,
+         typename TransitionMap>
 class fsm {
-  fsm() : state_{}, events_{} {}
+protected:
+  using Parent = fsm<ChildT, StateT, EventT, TransitionMap>;
+
+public:
+  fsm() : state_{}, events_{} {
+    state_ = child().initial_state();
+  }
 
   // Queue an event, but do not process it immediately.
   void send_event( EventT const& event ) {
@@ -37,8 +47,10 @@ class fsm {
 
   // Process all pending events.
   void process_events() {
-    while( auto maybe_event_ref = events_.front() )
+    while( auto maybe_event_ref = events_.front() ) {
       process_event( maybe_event_ref->get() );
+      events_.pop();
+    }
   }
 
   StateT const& state() const { return state_; }
@@ -51,51 +63,87 @@ class fsm {
     return res;
   }
 
+protected:
+  // Default transition function.
+  template<typename S1, typename E, typename S2>
+  static StateT transition( S1 const&, E const&, S2 const& ) {
+    // Unfortunately it seems that empty structs still report
+    // having a size of one, so we'll just assume that if the
+    // size is one that it is empty.
+    static_assert(
+        sizeof( S2 ) <= 1,
+        "should not default-construct a state with data "
+        "members.  Please provide a custom override of "
+        "`transition` to handle this case." );
+    static_assert(
+        sizeof( E ) <= 1,
+        "cannot default-construct a target state from an event "
+        "containing data members as this could be an indication "
+        "of data loss.  Please provide a custom override of "
+        "`transition` to handle this case." );
+    return StateT{S2{}};
+  }
+
 private:
+  static StateT initial_state() { return StateT{}; }
+
   ChildT const& child() const {
     return *static_cast<ChildT const*>( this );
   }
   ChildT& child() { return *static_cast<ChildT*>( this ); }
 
-  struct NoTransition {};
-
-  StateT transition_auto( StateT const& state,
-                          EventT const& event,
-                          NoTransition const& ) {
-    FATAL( "state {} cannot receive the event {}", state,
-           event );
-  }
-
-  template<typename T>
-  StateT transition_auto( StateT const& state,
-                          EventT const& event,
-                          T const&      new_state ) {
-    return StateT{new_state};
-  }
-
-  StateT transition( StateT const& state, EventT const& event ) {
-
-  }
-
   void process_event( EventT const& event ) {
-    // auto transition = [this]( auto const& state,
-    //                          auto const& event ) {
-    //  return child().transition( state, event,
-    //  Get<Transitionmap, ...> );
-    //};
-    // state_ = std::visit( transition, state_, event );
+    auto visitor = [this]( auto const& state,
+                           auto const& event ) -> StateT {
+      using state1_t = std::decay_t<decltype( state )>;
+      using event_t  = std::decay_t<decltype( event )>;
+      using key_t    = std::pair<state1_t, event_t>;
 
-    // Then it will look for a function with this signature:
-    //
-    //   void action( State1_t const&, Event1_t const&, State2_t
-    //   const& );
-    //
-    // and if one exists it will call it to perform an action.
+      struct NoTransition {};
+      using state2_t = Get<TransitionMap, key_t, NoTransition>;
+
+      if constexpr( std::is_same_v<state2_t, NoTransition> ) {
+        FATAL( "state {} cannot receive the event {}", state,
+               event );
+      } else {
+        return child().transition( state, event, state2_t{} );
+      }
+    };
+    state_ = std::visit( visitor, state_, event );
   }
 
   StateT             state_;
   flat_queue<EventT> events_;
 };
+
+/****************************************************************
+** Transition Map Macros
+*****************************************************************/
+// Example:
+//
+//    using TestMap = FSM_TRANSITIONS( BallState, BallEvent,
+//        ((none, start_spinning), /*->*/ spinning),
+//        ((none, start_rolling),  /*->*/ rolling)
+//    );
+
+#define FSM_TRANSITIONS( ... ) \
+  EVAL( FSM_TRANSITION_MAP_IMPL( __VA_ARGS__ ) )
+
+#define FSM_TRANSITION_MAP_IMPL( state_t_name, event_t_name, \
+                                 ... )                       \
+  TypeMap<PP_MAP_TUPLE_COMMAS(                               \
+      FSM_TO_KV_PAIR,                                        \
+      PP_MAP_PREPEND2_TUPLE( state_t_name, event_t_name,     \
+                             __VA_ARGS__ ) )>
+
+#define FSM_TO_KV_PAIR( state_t_name, event_t_name,          \
+                        state1_event, state2 )               \
+  KV<FSM_TO_PAIR PREPEND_TUPLE2( state_t_name, event_t_name, \
+                                 state1_event ),             \
+     state_t_name::state2>
+
+#define FSM_TO_PAIR( state_t_name, event_t_name, a, b ) \
+  std::pair<state_t_name::a, event_t_name::b>
 
 /****************************************************************
 ** Testing
