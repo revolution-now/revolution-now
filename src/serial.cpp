3d2ff8bf869f864e49e3c77cccd8826551eedb8e
David P. Sicilia <dpsicilia@gmail.com>
2019-10-01 00:03:48 -0400

    Add CapnProto support

--- src/serial.cpp
+++ src/serial.cpp
@@ -19,6 +19,11 @@
 #include <google/protobuf/text_format.h>
 #include "testing.pb.h"
 
+// CapnProto
+#include <capnp/message.h>
+#include <capnp/serialize-packed.h>
+#include "testing.capnp.h"
+
 using namespace std;
 
 namespace rn {
@@ -118,12 +123,97 @@ struct UclArchiver {
 /****************************************************************
 ** Testing
 *****************************************************************/
+void writeAddressBook( int fd ) {
+  capnp::MallocMessageBuilder message;
+
+  AddressBook::Builder addressBook =
+      message.initRoot<AddressBook>();
+  capnp::List<Person>::Builder people =
+      addressBook.initPeople( 2 );
+
+  Person::Builder alice = people[0];
+  alice.setId( 123 );
+  alice.setName( "Alice" );
+  alice.setEmail( "alice@example.com" );
+  // Type shown for explanation purposes; normally you'd use
+  // auto.
+  capnp::List<Person::PhoneNumber>::Builder alicePhones =
+      alice.initPhones( 1 );
+  alicePhones[0].setNumber( "555-1212" );
+  alicePhones[0].setType( Person::PhoneNumber::Type::MOBILE );
+  alice.getEmployment().setSchool( "MIT" );
+
+  Person::Builder bob = people[1];
+  bob.setId( 456 );
+  bob.setName( "Bob" );
+  bob.setEmail( "bob@example.com" );
+  auto bobPhones = bob.initPhones( 2 );
+  bobPhones[0].setNumber( "555-4567" );
+  bobPhones[0].setType( Person::PhoneNumber::Type::HOME );
+  bobPhones[1].setNumber( "555-7654" );
+  bobPhones[1].setType( Person::PhoneNumber::Type::WORK );
+  bob.getEmployment().setUnemployed();
+
+  writePackedMessageToFd( fd, message );
+}
+
+void printAddressBook( int fd ) {
+  capnp::PackedFdMessageReader message( fd );
+
+  AddressBook::Reader addressBook =
+      message.getRoot<AddressBook>();
+
+  for( Person::Reader person : addressBook.getPeople() ) {
+    std::cout << person.getName().cStr() << ": "
+              << person.getEmail().cStr() << std::endl;
+    for( Person::PhoneNumber::Reader phone :
+         person.getPhones() ) {
+      const char* typeName = "UNKNOWN";
+      switch( phone.getType() ) {
+        case Person::PhoneNumber::Type::MOBILE:
+          typeName = "mobile";
+          break;
+        case Person::PhoneNumber::Type::HOME:
+          typeName = "home";
+          break;
+        case Person::PhoneNumber::Type::WORK:
+          typeName = "work";
+          break;
+      }
+      std::cout << "  " << typeName
+                << " phone: " << phone.getNumber().cStr()
+                << std::endl;
+    }
+    Person::Employment::Reader employment =
+        person.getEmployment();
+    switch( employment.which() ) {
+      case Person::Employment::UNEMPLOYED:
+        std::cout << "  unemployed" << std::endl;
+        break;
+      case Person::Employment::EMPLOYER:
+        std::cout << "  employer: "
+                  << employment.getEmployer().cStr()
+                  << std::endl;
+        break;
+      case Person::Employment::SCHOOL:
+        std::cout << "  student at: "
+                  << employment.getSchool().cStr() << std::endl;
+        break;
+      case Person::Employment::SELF_EMPLOYED:
+        std::cout << "  self-employed" << std::endl;
+        break;
+    }
+  }
+}
+
 void test_serial() {
+  // ============================================================
   // SaveableOmni omni;
   // UclArchiver  ar;
   // ar.save( "Parent", omni );
   // lg.info( "result:\n{}", ar.result );
 
+  // ============================================================
   pb_test::AddressBook book;
 
   auto& person = *book.mutable_people();
@@ -148,6 +238,16 @@ void test_serial() {
          book2.people().phones().number() );
   CHECK( book.people().phones().type() ==
          book2.people().phones().type() );
+
+  // ============================================================
+  FILE* fp = ::fopen( "capn.out", "w" );
+  CHECK( fp );
+  writeAddressBook( fileno( fp ) );
+  ::fclose( fp );
+  fp = ::fopen( "capn.out", "r" );
+  CHECK( fp );
+  printAddressBook( fileno( fp ) );
+  ::fclose( fp );
 }
 
 } // namespace rn
