6d19af2297ebf1b0783905fb964f0ddcdb7b558e
David P. Sicilia <dpsicilia@gmail.com>
2019-10-03 19:07:28 -0400

    Remove capnproto and proto

--- src/serial.cpp
+++ src/serial.cpp
@@ -15,18 +15,6 @@
 #include "fmt-helper.hpp"
 #include "logging.hpp"
 
-// Proto
-#include <google/protobuf/text_format.h>
-#include "testing.pb.h"
-
-// CapnProto
-#include <capnp/compat/json.h>
-#include <capnp/message.h>
-#include <capnp/serialize-packed.h>
-#include <capnp/serialize.h>
-#include "kj/filesystem.h"
-#include "testing.capnp.h"
-
 // Flatbuffers
 #include "fb/testing_generated.h"
 #include "flatbuffers/flatbuffers.h"
@@ -131,110 +119,6 @@ struct UclArchiver {
 /****************************************************************
 ** Testing
 *****************************************************************/
-// void writeAddressBook( int fd ) {
-void writeAddressBook( FILE* fp ) {
-  capnp::MallocMessageBuilder message;
-
-  AddressBook::Builder addressBook =
-      message.initRoot<AddressBook>();
-  capnp::List<Person>::Builder people =
-      addressBook.initPeople( 2 );
-
-  Person::Builder alice = people[0];
-  alice.setId( 123 );
-  alice.setName( "Alice" );
-  alice.setEmail( "alice@example.com" );
-  // Type shown for explanation purposes; normally you'd use
-  // auto.
-  capnp::List<Person::PhoneNumber>::Builder alicePhones =
-      alice.initPhones( 1 );
-  alicePhones[0].setNumber( "555-1212" );
-  alicePhones[0].setType( Person::PhoneNumber::Type::MOBILE );
-  alice.getEmployment().setSchool( "MIT" );
-
-  Person::Builder bob = people[1];
-  bob.setId( 456 );
-  bob.setName( "Bob" );
-  bob.setEmail( "bob@example.com" );
-  auto bobPhones = bob.initPhones( 2 );
-  bobPhones[0].setNumber( "555-4567" );
-  bobPhones[0].setType( Person::PhoneNumber::Type::HOME );
-  bobPhones[1].setNumber( "555-7654" );
-  bobPhones[1].setType( Person::PhoneNumber::Type::WORK );
-  bob.getEmployment().setUnemployed();
-
-  // writePackedMessageToFd( fd, message );
-  // writeMessageToFd( fd, message );
-
-  capnp::JsonCodec tcodec;
-  tcodec.setPrettyPrint( true );
-
-  auto str = tcodec.encode( addressBook );
-  ::fwrite( str.cStr(), 1, str.size(), fp );
-}
-
-void printAddressBook( int fd ) {
-  // capnp::PackedFdMessageReader message( fd );
-  // capnp::StreamFdMessageReader message( fd );
-
-  capnp::MallocMessageBuilder message;
-  capnp::JsonCodec            json;
-
-  auto readable =
-      kj::newDiskReadableFile( kj::AutoCloseFd( fd ) );
-  auto orphan = json.decode<AddressBook>(
-      readable->readAllText(), message.getOrphanage() );
-
-  message.adoptRoot( std::move( orphan ) );
-
-  // ============================================================
-
-  AddressBook::Reader addressBook =
-      message.getRoot<AddressBook>();
-
-  for( Person::Reader person : addressBook.getPeople() ) {
-    std::cout << person.getName().cStr() << ": "
-              << person.getEmail().cStr() << std::endl;
-    for( Person::PhoneNumber::Reader phone :
-         person.getPhones() ) {
-      const char* typeName = "UNKNOWN";
-      switch( phone.getType() ) {
-        case Person::PhoneNumber::Type::MOBILE:
-          typeName = "mobile";
-          break;
-        case Person::PhoneNumber::Type::HOME:
-          typeName = "home";
-          break;
-        case Person::PhoneNumber::Type::WORK:
-          typeName = "work";
-          break;
-      }
-      std::cout << "  " << typeName
-                << " phone: " << phone.getNumber().cStr()
-                << std::endl;
-    }
-    Person::Employment::Reader employment =
-        person.getEmployment();
-    switch( employment.which() ) {
-      case Person::Employment::UNEMPLOYED:
-        std::cout << "  unemployed" << std::endl;
-        break;
-      case Person::Employment::EMPLOYER:
-        std::cout << "  employer: "
-                  << employment.getEmployer().cStr()
-                  << std::endl;
-        break;
-      case Person::Employment::SCHOOL:
-        std::cout << "  student at: "
-                  << employment.getSchool().cStr() << std::endl;
-        break;
-      case Person::Employment::SELF_EMPLOYED:
-        std::cout << "  self-employed" << std::endl;
-        break;
-    }
-  }
-}
-
 void writeMonster( FILE* fp ) {
   flatbuffers::FlatBufferBuilder builder;
 
@@ -325,46 +209,8 @@ void test_serial() {
   // ar.save( "Parent", omni );
   // lg.info( "result:\n{}", ar.result );
 
-  // == Proto ===================================================
-  pb_test::AddressBook book;
-
-  auto& person = *book.mutable_people();
-  person.set_name( "David" );
-  person.set_id( 5 );
-  auto& phone_number = *person.mutable_phones();
-  phone_number.set_number( "000-0000" );
-  phone_number.set_type( pb_test::Person_PhoneType_MOBILE );
-
-  string pb;
-  google::protobuf::TextFormat::PrintToString( book, &pb );
-
-  lg.info( "proto result:\n{}", pb );
-
-  pb_test::AddressBook book2;
-  google::protobuf::TextFormat::ParseFromString( pb, &book2 );
-
-  CHECK( book.people().name() == book2.people().name() );
-  CHECK( book.people().id() == book2.people().id() );
-  CHECK( book.people().email() == book2.people().email() );
-  CHECK( book.people().phones().number() ==
-         book2.people().phones().number() );
-  CHECK( book.people().phones().type() ==
-         book2.people().phones().type() );
-
-  // == CapnProto ===============================================
-  FILE* fp;
-  fp = ::fopen( "capn.out", "w" );
-  CHECK( fp );
-  // writeAddressBook( fileno( fp ) );
-  writeAddressBook( fp );
-  ::fclose( fp );
-  fp = ::fopen( "capn.out", "r" );
-  CHECK( fp );
-  printAddressBook( fileno( fp ) );
-  //::fclose( fp );
-
   // == Flatbuffers =============================================
-  fp = ::fopen( "fb.out", "wb" );
+  auto fp = ::fopen( "fb.out", "wb" );
   CHECK( fp );
   writeMonster( fp );
   ::fclose( fp );
---
+++ test/flatbuffers.cpp
@@ -0,0 +1,29 @@
+/****************************************************************
+**flatbuffers.cpp
+*
+* Project: Revolution Now
+*
+* Created by dsicilia on 2019-10-03.
+*
+* Description: Unit tests for Flatbuffers integration.
+*
+*****************************************************************/
+#include "testing.hpp"
+
+// Flatbuffers
+#include "fb/testing_generated.h"
+#include "flatbuffers/flatbuffers.h"
+
+// Must be last.
+#include "catch-common.hpp"
+
+namespace {
+
+using namespace std;
+using namespace rn;
+
+TEST_CASE( "[flatbuffers] Monsters." ) {
+  //
+}
+
+} // namespace
