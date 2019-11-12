44c472a3d0bfca8c30499e1cf74292574a1abed5
David P. Sicilia <dpsicilia@gmail.com>
2019-11-12 14:26:48 -0500

    Finish first stage of porting turn module to FSMs

--- src/turn.cpp
+++ src/turn.cpp
@@ -272,6 +272,7 @@ void advance_unit_input_state( UnitInputFsm&            fsm,
         // Kick off animation if needed.
         switch_( *g_player_intent ) {
           case_( TravelAnalysis ) {
+            if( !animate_move( val ) ) break_;
             ASSIGN_CHECK_OPT( d, val.move_src.direction_to(
                                      val.move_target ) );
             landview_animate_move( id, d );
@@ -372,6 +373,29 @@ FSM_DEFINE_FORMAT_RN_( NationTurn );
 void advance_nation_turn_state( NationTurnFsm&           fsm,
                                 tl::function_ref<void()> done,
                                 e_nation nation ) {
+  //  Iterate through the colonies, for each:
+  //
+  //    - advance state of the colony
+  //
+  //    - display messages to user any/or show animations where
+  //      necessary
+  //
+  //    - allow them to enter colony when events happens; in that
+  //      case go to the colony screen game loop. When the user
+  //      exits the colony screen then this colony iteration im-
+  //      mediately proceeds; i.e., user cannot enter any other
+  //      colonies. This prevents the user from making
+  //      last-minute changes to colonies that have not yet been
+  //      advanced in this turn (otherwise that might allow
+  //      cheating in some way).
+  //
+  //    - during this time, the user is not free to scroll map
+  //      (menus?) or make any changes to units. They are also
+  //      not allowed to enter colonies apart from the one that
+  //      has just been processed.
+  //
+  //  Advance the state of the old world, possibly displaying
+  //  messages to the user where necessary. clang-format on
   switch_( fsm.mutable_state() ) {
     case_( NationTurnState::starting ) {
       lg.info( "-------[ Starting turn for {} ]-------",
@@ -442,7 +466,7 @@ void advance_nation_turn_state( NationTurnFsm&           fsm,
             CHECK( maybe_orders.has_value() ==
                    add_to_front.empty() );
             if( !add_to_front.empty() ) {
-              for( auto id : val.response.add_to_front ) {
+              for( auto id : add_to_front ) {
                 doing_units.q.push_front( id );
                 unit_from_id( id ).unfinish_turn();
               }
@@ -464,8 +488,10 @@ void advance_nation_turn_state( NationTurnFsm&           fsm,
             done_uturn = true;
           }
           case_( UnitInputState::executed ) {
-            for( auto id : val.add_to_front )
+            for( auto id : val.add_to_front ) {
               doing_units.q.push_front( id );
+              unit_from_id( id ).unfinish_turn();
+            }
             log_q();
             doing_units.uturn->send_event(
                 UnitInputEvent::process{} );
@@ -632,289 +658,5 @@ void advance_turn_state() {
                     { advance_turn_cycle_state },
                     SG().nation_turn );
 }
-
-#if 0
-e_turn_result turn( e_nation nation ) {
-  // start of turn:
-
-  lg.debug( "------ starting turn ({}) -------", nation );
-
-  // Mark all units as not having moved.
-  map_units( []( Unit& unit ) { unit.new_turn(); } );
-
-  //  Iterate through the colonies, for each:
-  //
-  //    - advance state of the colony
-  //
-  //    - display messages to user any/or show animations where
-  //      necessary
-  //
-  //    - allow them to enter colony when events happens; in that
-  //      case go to the colony screen game loop. When the user
-  //      exits the colony screen then this colony iteration im-
-  //      mediately proceeds; i.e., user cannot enter any other
-  //      colonies. This prevents the user from making
-  //      last-minute changes to colonies that have not yet been
-  //      advanced in this turn (otherwise that might allow
-  //      cheating in some way).
-  //
-  //    - during this time, the user is not free to scroll map
-  //      (menus?) or make any changes to units. They are also
-  //      not allowed to enter colonies apart from the one that
-  //      has just been processed.
-  //
-  //  Advance the state of the old world, possibly displaying
-  //  messages to the user where necessary. clang-format on
-
-  bool orders_taken = false;
-
-  auto& vp_state = viewport_rendering_state();
-
-  // We keep looping until all units that need moving have moved.
-  // We don't know this list a priori because some units may de-
-  // cide to require orders during the course of this process,
-  // and this could happen for various reasons. Perhaps even
-  // units could be created during this process (?).
-  while( true ) {
-    auto units = units_all( nation );
-    // Optional, but makes for good consistency for units to ask
-    // for orders in the order that they were created.
-    // TODO: in the future, try to sort by geographic location.
-    util::sort_by_key( units, []( auto id ) { return id._; } );
-    auto finished = []( UnitId id ) {
-      return unit_from_id( id ).finished_turn();
-    };
-    if( all_of( units.begin(), units.end(), finished ) ) break;
-
-    deque<UnitId> q;
-    for( auto id : units ) q.push_back( id );
-
-    //  Iterate through all units, for each:
-    while( !q.empty() ) {
-      auto id = q.front();
-      if( !unit_exists( id ) ) {
-        // This will happen if the unit is disbanded or it is
-        // cargo of something that was disbanded.
-        lg.debug( "unit {} no longer exists.", id );
-        q.pop_front();
-        continue;
-      }
-      auto& unit = unit_from_id( id );
-      CHECK( unit.nation() == nation );
-      if( unit.finished_turn() ) {
-        q.pop_front();
-        continue;
-      }
-      lg.debug( "processing turn for {}", debug_string( id ) );
-
-      //    clang-format off
-      //
-      //    * if it is it in `goto` mode focus on it and advance
-      //      it
-      //
-      //    * if it is a ship on the high seas then advance it
-      //        if it has arrived in the old world then jump to
-      //        the old world screen (maybe ask user whether they
-      //        want to ignore), which has its own game loop (see
-      //        old-world loop).
-      //
-      //    * if it is in the old world then ignore it, or
-      //      possibly remind the user it is there.
-      //
-      //    * if it is performing an action, such as building a
-      //      road, advance the state.  If it finishes then mark
-      //      it as active so that it will wait for orders in the
-      //      next step.
-      //
-      //    * if it is in an indian village then advance it, and
-      //      mark it active if it is finished.
-
-      //    * if unit is waiting for orders then focus on it, and
-      //      enter a realtime game loop where the user can
-      //      interact with the map and GUI in general.  See
-      //      `unit orders` game loop.
-      //
-      //    clang-format on
-      while( CHECK_INL( !q.empty() ) && q.front() == id &&
-             unit.orders_mean_input_required() &&
-             !unit.mv_pts_exhausted() ) {
-        deduplicate_deque( &q );
-        log_q( q );
-        lg.debug( "asking orders for: {}", debug_string( id ) );
-        orders_taken = true;
-
-        auto coords = coord_for_unit_indirect( id );
-        viewport().ensure_tile_visible( coords,
-                                        /*smooth=*/true );
-
-        auto maybe_orders = pop_unit_orders( id );
-
-        /***************************************************/
-        if( !maybe_orders.has_value() ) {
-          vp_state = viewport_state::blink_unit{};
-          auto& blink_unit =
-              get<viewport_state::blink_unit>( vp_state );
-          blink_unit.id = id;
-          frame_loop( true, [&blink_unit] {
-            return blink_unit.orders.has_value() ||
-                   blink_unit.prioritize.size() > 0;
-          } );
-          for( auto add_id : blink_unit.add_to_back ) {
-            auto& add_unit = unit_from_id( add_id );
-            q.push_back( add_id );
-            add_unit.unfinish_turn();
-          }
-          if( blink_unit.prioritize.size() > 0 ) {
-            CHECK( !blink_unit.orders.has_value() );
-            // The code that produces this prioritization list is
-            // not supposed to allow any units in the list that
-            // have already moved this turn. Doing this makes
-            // this code here simpler since we don't have to deal
-            // with cases where we go to the trouble of
-            // restarting this while loop just for a unit that
-            // won't move anyway and end up e.g. re-scrolling the
-            // viewport for nothing in the process (non-ideal UI
-            // effects).
-            for( auto prio_id : blink_unit.prioritize ) {
-              auto& prio_unit = unit_from_id( prio_id );
-              CHECK( !prio_unit.mv_pts_exhausted() );
-              q.push_front( prio_id );
-              prio_unit.unfinish_turn();
-            }
-            continue;
-          }
-          CHECK( blink_unit.orders.has_value() );
-          maybe_orders = blink_unit.orders;
-        } else {
-          lg.debug( "found queued orders." );
-        }
-        /***************************************************/
-
-        CHECK( maybe_orders.has_value() );
-        auto& orders = *maybe_orders;
-        lg.debug( "received orders: {}", orders );
-
-        if( util::holds<orders::wait>( orders ) ) {
-          q.push_back( q.front() );
-          q.pop_front();
-          break;
-        }
-        auto maybe_intent = player_intent( id, orders );
-        CHECK( maybe_intent.has_value(),
-               "no handler for orders {}", orders );
-        auto const& analysis = maybe_intent.value();
-
-        if( !confirm_explain( analysis ) ) continue;
-
-        // Check if the unit is physically moving; usually at
-        // this point it will be unless it is e.g. a ship of-
-        // floading units.
-        if_v( analysis, TravelAnalysis, mv_res ) {
-          /***************************************************/
-          if( animate_move( *mv_res ) ) {
-            play_sound_effect( e_sfx::move );
-            viewport().ensure_tile_visible( mv_res->move_target,
-                                            /*smooth=*/true );
-            vp_state = viewport_state::slide_unit(
-                id, mv_res->move_target );
-            auto& slide_unit =
-                get<viewport_state::slide_unit>( vp_state );
-            // In this call we specify that it should not collect
-            // any user input (keyboard, mouse) to avoid movement
-            // commands (issued during animation) from getting
-            // swallowed.
-            frame_loop( false, [&slide_unit] {
-              return slide_unit.percent >= 1.0;
-            } );
-          }
-          /***************************************************/
-        }
-        if_v( analysis, CombatAnalysis, combat_res ) {
-          /***************************************************/
-          play_sound_effect( e_sfx::move );
-          viewport().ensure_tile_visible(
-              combat_res->attack_target,
-              /*smooth=*/true );
-          vp_state = viewport_state::slide_unit(
-              id, combat_res->attack_target );
-          auto& slide_unit =
-              get<viewport_state::slide_unit>( vp_state );
-          frame_loop( /*poll_input=*/true, [&slide_unit] {
-            return slide_unit.percent >= 1.0;
-          } );
-          CHECK( combat_res->target_unit );
-          CHECK( combat_res->fight_stats );
-          auto dying_unit =
-              combat_res->fight_stats->attacker_wins
-                  ? *combat_res->target_unit
-                  : id;
-          auto const& dying_unit_desc =
-              unit_from_id( dying_unit ).desc();
-          Opt<e_unit_type> demote_to;
-          if( dying_unit_desc.demoted.has_value() ) {
-            lg.debug( "animating unit demotion to {}",
-                      dying_unit_desc.demoted.value() );
-            demote_to = dying_unit_desc.demoted.value();
-          }
-          vp_state = viewport_state::depixelate_unit(
-              dying_unit, demote_to );
-          auto& depixelate_unit =
-              get<viewport_state::depixelate_unit>( vp_state );
-          play_sound_effect(
-              combat_res->fight_stats->attacker_wins
-                  ? e_sfx::attacker_won
-                  : e_sfx::attacker_lost );
-          conductor::play_request(
-              combat_res->fight_stats->attacker_wins
-                  ? conductor::e_request::won_battle_europeans
-                  : conductor::e_request::lost_battle_europeans,
-              conductor::e_request_probability::rarely );
-          frame_loop( /*poll_input=*/true, [&depixelate_unit] {
-            return depixelate_unit.finished;
-          } );
-          /***************************************************/
-        }
-
-        affect_orders( analysis );
-
-        // Must immediately check if this unit has been killed in
-        // combat or disbanded. If so then all the unit refer-
-        // ences to it are invalid.
-        if( !unit_exists( id ) ) break;
-
-        // Note that it shouldn't hurt if the unit is already in
-        // the queue, since this turn code will never move a unit
-        // after it has already completed its turn, no matter how
-        // many times it appears in the queue.
-        for( auto unit_id : units_to_prioritize( analysis ) ) {
-          CHECK( unit_id != id );
-          q.push_front( unit_id );
-        }
-      }
-
-      // Must check if this unit has been killed in combat or
-      // disbanded. If so then all the unit references to it are
-      // invalid.
-      if( !unit_exists( id ) ) continue;
-
-      // Now we must decide if the unit has finished its turn.
-      // TODO: try to put this in the unit class.
-      if( unit.mv_pts_exhausted() ||
-          !unit.orders_mean_input_required() )
-        unit.finish_turn();
-    }
-  }
-
-  //    clang-format off
-  //    * Make AI moves
-  //        Make European moves
-  //        Make Native moves
-  //        Make expeditionary force moves
-  //    clang-format on
-
-  return orders_taken ? e_turn_result::orders_taken
-                      : e_turn_result::no_orders_taken;
-}
-#endif
 
 } // namespace rn
