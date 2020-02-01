cc03ffdffefdc69996ea739a6fe2dc1ae9674d09
David P. Sicilia <dpsicilia@gmail.com>
2020-02-01 14:59:38 -0500

    Fix issue with colony job ADT

--- src/colony.hpp
+++ src/colony.hpp
@@ -28,11 +28,12 @@
 
 namespace rn {
 
-adt_s_rn( ColonyJob,                 //
-          ( land,                    //
-            ( e_direction, d ) ),    //
-          ( mfg,                     //
-            ( e_mfg_job, mfg_job ) ) //
+adt_s_rn( ColonyJob,                      //
+          ( land,                         //
+            ( e_direction, d ),           //
+            ( e_commodity, producing ) ), //
+          ( mfg,                          //
+            ( e_mfg_job, mfg_job ) )      //
 );
 
 class Colony {
