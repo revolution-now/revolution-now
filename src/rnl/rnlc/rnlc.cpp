e657d28a523d061efa9e49108c4bf4ca24a895b3
David P. Sicilia <dpsicilia@gmail.com>
2020-11-04 13:33:48 -0500

    Add initial rnl.peg file

--- src/rnl/rnlc/rnlc.cpp
+++ src/rnl/rnlc/rnlc.cpp
@@ -13,10 +13,29 @@
 #include <iostream>
 #include <string_view>
 
+// base-util
+#include "base-util/io.hpp"
+#include "base-util/string.hpp"
+
+// C++ PEG-lib
 #include "peglib.h"
 
+std::string remove_comments( std::string const& rnl ) {
+  auto lines = util::split_strip_any( rnl, "\n\r" );
+  std::vector<std::string> res;
+  for( auto line : lines ) {
+    auto n = line.find_first_of( '#' );
+    if( n != line.npos )
+      line = std::string_view( line.begin(), line.begin() + n );
+    line = util::strip( line );
+    if( line.empty() ) continue;
+    res.push_back( std::string( line ) );
+  }
+  return util::join( res, "\n" );
+}
+
 int main( int argc, char** argv ) {
-  if( argc != 3 ) {
+  if( argc != 4 ) {
     std::cerr << "usage: rnlc <rnl-file> <out-file>\n";
     return 1;
   }
@@ -27,8 +46,14 @@ int main( int argc, char** argv ) {
               << "' does not have .rnl extension.\n";
     return 1;
   }
-  std::string_view stem = filename;
-  stem.remove_suffix( 4 );
+
+  std::optional<std::string> rnl =
+      util::read_file_as_string( filename );
+  if( !rnl.has_value() ) {
+    std::cerr << "error: failed to open rnl file: " << filename
+              << ".\n";
+    return 1;
+  }
 
   std::string_view output_file = argv[2];
   if( !output_file.ends_with( ".hpp" ) ) {
@@ -36,12 +61,43 @@ int main( int argc, char** argv ) {
     return 1;
   }
 
-  // std::cerr << "writing output to " << output_file << ".\n";
+  std::string_view peg_file = argv[3];
+  if( !peg_file.ends_with( ".peg" ) ) {
+    std::cerr << "error: peg file must end with '.peg'.\n";
+    return 1;
+  }
 
+  std::optional<std::string> peg =
+      util::read_file_as_string( peg_file );
+  if( !peg.has_value() ) {
+    std::cerr << "error: failed to open peg file: " << peg_file
+              << ".\n";
+    return 1;
+  }
+
+  peg::parser parser( peg->c_str() );
+  if( !parser ) {
+    std::cerr << "error: failed to parse peg file.\n";
+    return 1;
+  }
+  parser.enable_packrat_parsing();
+
+  // Remove comments.
+  std::string rnl_no_comments = remove_comments( *rnl );
+  if( !parser.parse( rnl_no_comments.c_str() ) ) {
+    std::cerr << "error: failed to parse rnl file.\n";
+    return 1;
+  }
+
+  std::string_view stem = filename;
+  stem.remove_suffix( 4 );
   std::ofstream out;
   out.open( std::string( output_file ) );
-  out << "// empty file "
+  out << "// output file: "
       << std::filesystem::path( output_file ).stem() << ".\n";
+
+  for( auto s : util::split( rnl_no_comments, '\n' ) )
+    out << "// " << s << "\n";
 
   return 0;
 }
