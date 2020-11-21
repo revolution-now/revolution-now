d4fb4762c1a299f8e267e1f30042c35fa79a877c
David P. Sicilia <dpsicilia@gmail.com>
2020-11-20 19:12:22 -0500

    Change nations at startup.

--- src/lua/startup.lua
+++ src/lua/startup.lua
@@ -26,41 +26,41 @@ local function create_some_units_in_europort( nation )
   europort.advance_unit_on_high_seas( id )
 end
 
-local function create_some_units_on_land( nation )
+local function create_some_units_on_land( nation1, nation2 )
   local coord = Coord{y=6, x=2}
   local unit = ustate.create_unit_on_map(
-           e.nation.spanish, e.unit_type.free_colonist, coord )
+           nation1, e.unit_type.free_colonist, coord )
   unit:fortify();
 
   coord = Coord{y=6, x=3}
   unit = ustate.create_unit_on_map(
-           e.nation.spanish, e.unit_type.soldier, coord )
+           nation1, e.unit_type.soldier, coord )
   unit:sentry();
 
   coord = Coord{y=6, x=6}
   ustate.create_unit_on_map(
-      e.nation.spanish, e.unit_type.privateer, coord )
+      nation1, e.unit_type.privateer, coord )
 
   coord = Coord{y=7, x=2}
   unit = ustate.create_unit_on_map(
-           e.nation.english, e.unit_type.soldier, coord )
+           nation2, e.unit_type.soldier, coord )
   unit:fortify();
 
   coord = Coord{y=7, x=3}
   unit = ustate.create_unit_on_map(
-           e.nation.english, e.unit_type.soldier, coord )
+           nation2, e.unit_type.soldier, coord )
   unit:sentry();
 
   coord = Coord{y=7, x=6}
   unit = ustate.create_unit_on_map(
-           e.nation.english, e.unit_type.privateer, coord )
+           nation2, e.unit_type.privateer, coord )
   unit:clear_orders();
 end
 
-local function create_some_colonies()
+local function create_some_colonies( nation1, nation2 )
   local coord = Coord{y=2, x=4}
   local unit = ustate.create_unit_on_map(
-           e.nation.spanish, e.unit_type.free_colonist, coord )
+           nation1, e.unit_type.free_colonist, coord )
   local col_id = colony_mgr.found_colony(
            unit:id(), "New London" )
   local colony = cstate.colony_from_id( col_id )
@@ -68,7 +68,7 @@ local function create_some_colonies()
 
   coord = Coord{y=4, x=4}
   unit = ustate.create_unit_on_map(
-           e.nation.english, e.unit_type.free_colonist, coord )
+           nation2, e.unit_type.free_colonist, coord )
   col_id = colony_mgr.found_colony(
              unit:id(), "New York" )
   local colony = cstate.colony_from_id( col_id )
@@ -84,9 +84,13 @@ local function main()
     e.nation.english,
     e.nation.french
   } )
-  create_some_units_in_europort( e.nation.dutch )
-  create_some_units_on_land()
-  create_some_colonies()
+
+  nation1 = e.nation.dutch
+  nation2 = e.nation.french
+
+  create_some_units_in_europort( nation1 )
+  create_some_units_on_land( nation1, nation2 )
+  create_some_colonies( nation1, nation2 )
 end
 
 package_exports = {
