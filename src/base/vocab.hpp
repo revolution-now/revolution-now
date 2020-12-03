fe0069aa3f55b7b802f068df26a1b463a805700f
David P. Sicilia <dpsicilia@gmail.com>
2020-12-03 09:39:27 -0500

    Add the NoCopy type.

---
+++ src/base/vocab.hpp
@@ -0,0 +1,82 @@
+/****************************************************************
+**vocab.hpp
+*
+* Project: Revolution Now
+*
+* Created by dsicilia on 2020-12-03.
+*
+* Description: Some miscellaneous vocabulary types.
+*
+*****************************************************************/
+#pragma once
+
+// C++ standard library.
+#include <type_traits>
+#include <utility>
+
+namespace base {
+
+// This wrapper does everything possible to try to prevent its
+// contained value from being copied.
+template<typename T> /* clang-format off */
+  requires( !std::is_const_v<T> && !std::is_reference_v<T> )
+struct NoCopy {
+  /* clang-format on */
+  /**************************************************************
+  ** Allowed
+  ***************************************************************/
+  NoCopy() requires( std::is_default_constructible_v<T> ) =
+      default;
+
+  template<typename... Args>
+  NoCopy( Args&&... args )
+    : val( std::forward<Args>( args )... ) {}
+
+  NoCopy( T&& val_ ) : val( std::move( val_ ) ) {}
+
+  NoCopy( NoCopy&& ) requires(
+      std::is_trivially_move_constructible_v<T> ) = default;
+
+  NoCopy( NoCopy&& o ) requires(
+      std::is_move_constructible_v<T> &&
+      !std::is_trivially_move_constructible_v<T> )
+    : val( std::move( o.val ) ) {}
+
+  NoCopy& operator=( T&& val_ ) { val = std::move( val_ ); }
+
+  NoCopy& operator                             =( NoCopy&& ) &
+      requires( std::is_move_assignable_v<T> ) = default;
+
+  /**************************************************************
+  ** Now Allowed
+  ***************************************************************/
+  NoCopy( T const& )      = delete;
+  NoCopy( NoCopy const& ) = delete;
+  NoCopy& operator=( T const& ) = delete;
+  NoCopy& operator=( NoCopy const& ) = delete;
+
+  /**************************************************************
+  ** Implicit conversions (only allow moving)
+  ***************************************************************/
+  operator T const &() const& noexcept   = delete;
+  operator T&() & noexcept               = delete;
+  operator T const &&() const&& noexcept = delete;
+
+  operator T&&() && noexcept { return std::move( val ); }
+
+  /**************************************************************
+  ** Comparison, just for convenience.
+  ***************************************************************/
+  template<typename U>
+  bool operator==( U&& u ) const {
+    return ( val == std::forward<U>( u ) );
+  }
+
+  /**************************************************************
+  ** The value.
+  ***************************************************************/
+private:
+  T val;
+};
+
+} // namespace base
---
+++ test/vocab.cpp
@@ -0,0 +1,93 @@
+/****************************************************************
+**vocab.cpp
+*
+* Project: Revolution Now
+*
+* Created by dsicilia on 2020-12-03.
+*
+* Description: Unit tests for the src/vocab.* module.
+*
+*****************************************************************/
+#include "testing.hpp"
+
+// Under test.
+#include "src/base/vocab.hpp"
+
+// Revolution Now
+#include "src/fmt-helper.hpp"
+
+// Must be last.
+#include "catch-common.hpp"
+
+namespace base {
+namespace {
+
+using namespace std;
+
+TEST_CASE( "[vocab] NoCopy int" ) {
+  NoCopy<int> nc( 5 );
+  REQUIRE( nc == 5 );
+  // NoCopy<int> nc2 = nc;
+  static_assert( !is_copy_constructible_v<NoCopy<int>> );
+  NoCopy<int> nc2 = std::move( nc );
+  REQUIRE( nc2 == 5 );
+  // int x = nc2;
+  static_assert( !is_convertible_v<NoCopy<int> const&, int> );
+  static_assert( !is_convertible_v<NoCopy<int>&, int> );
+  static_assert( !is_convertible_v<NoCopy<int> const, int> );
+  static_assert( !is_convertible_v<NoCopy<int> const&&, int> );
+  int x = std::move( nc2 );
+  static_assert( is_convertible_v<NoCopy<int>, int> );
+  static_assert( is_convertible_v<NoCopy<int>&&, int> );
+  REQUIRE( x == 5 );
+}
+
+TEST_CASE( "[vocab] NoCopy string" ) {
+  NoCopy<string> nc( "5" );
+  REQUIRE( nc == "5" );
+  // NoCopy<string> nc2 = nc;
+  static_assert( !is_copy_constructible_v<NoCopy<string>> );
+  NoCopy<string> nc2 = std::move( nc );
+  REQUIRE( nc2 == "5" );
+  // string x = nc2;
+  static_assert(
+      !is_convertible_v<NoCopy<string> const&, string> );
+  static_assert( !is_convertible_v<NoCopy<string>&, string> );
+  static_assert(
+      !is_convertible_v<NoCopy<string> const, string> );
+  static_assert(
+      !is_convertible_v<NoCopy<string> const&&, string> );
+  string x = std::move( nc2 );
+  static_assert( is_convertible_v<NoCopy<string>, string> );
+  static_assert( is_convertible_v<NoCopy<string>&&, string> );
+  REQUIRE( x == "5" );
+}
+
+TEST_CASE( "[vocab] NoCopy NoCopy" ) {
+  NoCopy<NoCopy<string>> nc( "5" );
+  REQUIRE( nc == "5" );
+  // NoCopy<NoCopy<string>> nc2 = nc;
+  static_assert(
+      !is_copy_constructible_v<NoCopy<NoCopy<string>>> );
+  NoCopy<NoCopy<string>> nc2 = std::move( nc );
+  REQUIRE( nc2 == "5" );
+  // NoCopy<string> x = nc2;
+  static_assert( !is_convertible_v<NoCopy<NoCopy<string>> const&,
+                                   NoCopy<string>> );
+  static_assert( !is_convertible_v<NoCopy<NoCopy<string>>&,
+                                   NoCopy<string>> );
+  static_assert( !is_convertible_v<NoCopy<NoCopy<string>> const,
+                                   NoCopy<string>> );
+  static_assert(
+      !is_convertible_v<NoCopy<NoCopy<string>> const&&,
+                        NoCopy<string>> );
+  NoCopy<string> x = std::move( nc2 );
+  static_assert(
+      is_convertible_v<NoCopy<NoCopy<string>>, NoCopy<string>> );
+  static_assert( is_convertible_v<NoCopy<NoCopy<string>>&&,
+                                  NoCopy<string>> );
+  REQUIRE( x == "5" );
+}
+
+} // namespace
+} // namespace base
