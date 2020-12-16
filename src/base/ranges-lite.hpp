25dfc3782b7e0dffefbde036a13a4b8139bf2005
David P. Sicilia <dpsicilia@gmail.com>
2020-12-16 17:53:32 -0500

    More work on ranges-lite.

--- src/base/ranges-lite.hpp
+++ src/base/ranges-lite.hpp
@@ -11,6 +11,7 @@
 #pragma once
 
 // base
+#include "lambda.hpp"
 #include "maybe.hpp"
 #include "stack-trace.hpp"
 
@@ -30,18 +31,94 @@ namespace base::rl {
 #  define rl_assert( ... )
 #endif
 
-template<typename InputView, typename Op>
+/****************************************************************
+** TODO list
+*****************************************************************/
+//      rv::take_while_incl
+//
+//      3 rv::zip
+//      3 rv::take
+//      3 rv::drop
+//      3 rv::ints
+//      3 rv::cycle
+//
+//      2 rv::sliding
+//      2 rv::intersperse
+//      2 rv::enumerate
+//
+//      1 rv::keys
+//      1 rv::join
+//      1 rv::iota
+//      1 rv::group_by
+//      1 rv::drop_while
+//      1 rv::drop_last
+//      1 rv::concat
+//      1 rg::distance
+//      1 rg::any_of
+
+/****************************************************************
+** Metaprogramming helpers.
+*****************************************************************/
+template<typename T, typename = void>
+struct ultimate_view_or_self {
+  using type = T;
+};
+
+template<typename T>
+struct ultimate_view_or_self<
+    T, std::void_t<typename T::ultimate_view_t>> {
+  using type = typename T::ultimate_view_t;
+};
+
+template<typename T>
+using ultimate_view_or_self_t =
+    typename ultimate_view_or_self<T>::type;
+
+/****************************************************************
+** Macros
+*****************************************************************/
+#define RL_LAMBDA( name, ... ) name( L( __VA_ARGS__ ) )
+
+#define rl_keep( ... ) RL_LAMBDA( keep, __VA_ARGS__ )
+#define rl_remove( ... ) RL_LAMBDA( remove, __VA_ARGS__ )
+#define rl_map( ... ) RL_LAMBDA( map, __VA_ARGS__ )
+#define rl_take_while( ... ) RL_LAMBDA( take_while, __VA_ARGS__ )
+
+/****************************************************************
+** View
+*****************************************************************/
+template<typename InputView, typename Cursor>
 class View {
   InputView input_;
-  Op        op_;
+  Cursor    op_;
 
 public:
-  View( InputView&& input, Op&& op )
+  View( InputView&& input, Cursor&& op )
     : input_( std::move( input ) ), op_( std::move( op ) ) {}
 
   using value_type =
-      std::decay_t<decltype( std::declval<Op>().get(
+      std::decay_t<decltype( std::declval<Cursor>().get(
           std::declval<InputView>() ) )>;
+
+  using ultimate_view_t = ultimate_view_or_self_t<InputView>;
+
+  static auto create( ultimate_view_t const& input ) {
+    if constexpr( std::is_default_constructible_v<Cursor> ) {
+      if constexpr( std::is_same_v<ultimate_view_t,
+                                   InputView> ) {
+        return View{ InputView( input ), Cursor{} };
+      } else {
+        return View{ InputView::create( input ), Cursor{} };
+      }
+    } else {
+      // After C++20 stateless lambdas should be default con-
+      // structible, which will make this feature usable more of-
+      // ten. Until then, you must use regular functions (or
+      // function pointers) instead of lambdas.
+      struct CanOnlyUseThisWithDefaultConstructibleLambdas {};
+      return CanOnlyUseThisWithDefaultConstructibleLambdas{};
+    }
+  }
 
   /**************************************************************
   ** Iterator
@@ -49,7 +126,7 @@ public:
   struct Iterator {
     using iterator_category = std::input_iterator_tag;
     using difference_type   = int;
-    using value_type        = value_type;
+    using value_type        = View::value_type;
     using pointer           = value_type const*;
     using reference         = value_type const&;
 
@@ -100,26 +177,65 @@ public:
   Iterator begin() { return Iterator( this ); }
   Iterator end() const { return Iterator(); }
 
+  View copy() const { return *this; }
+
   /**************************************************************
-  ** Materialize
+  ** Materialization
   ***************************************************************/
-  std::vector<value_type> materialize() {
-    // FIXME: pre-size
+  std::vector<value_type> to_vector() {
     return std::vector<value_type>( this->begin(), this->end() );
+  }
+
+  template<typename T>
+  T to() {
+    return T( this->begin(), this->end() );
+  }
+
+  /**************************************************************
+  ** Accumulate
+  ***************************************************************/
+  template<typename Op = std::plus<>>
+  value_type accumulate( Op&& op = {}, value_type init = {} ) {
+    value_type res = init;
+    for( auto const& e : *this )
+      res = std::invoke( std::forward<Op>( op ), res, e );
+    return res;
+  }
+
+  /**************************************************************
+  ** Head
+  ***************************************************************/
+  maybe<value_type> head() {
+    maybe<value_type> res;
+
+    auto it = begin();
+    if( it != end() ) res = *it;
+    return res;
   }
 
   /**************************************************************
   ** Keep
   ***************************************************************/
   template<typename Func>
-  auto keep( Func&& func ) {
-    struct KeepOp {
+  auto keep( Func&& func ) && {
+    struct KeepCursor {
       using input_view_t = View;
       using iterator     = typename input_view_t::iterator;
       using value_type   = typename input_view_t::value_type;
-      KeepOp( Func&& f ) : func_( std::move( f ) ) {}
-      void init( input_view_t& input ) { it = input.begin(); }
-      value_type const& get( input_view_t const& input ) const {
+#ifdef __clang__ // C++20
+      KeepCursor() requires(
+          std::is_default_constructible_v<Func> ) = default;
+#endif
+      KeepCursor( Func&& f ) : func_( std::move( f ) ) {}
+      void init( input_view_t& input ) {
+        it = input.begin();
+        do {
+          if( end( input ) ) break;
+          if( func_( *it ) ) break;
+          ++it;
+        } while( true );
+      }
+      value_type const& get( input_view_t const& ) const {
         return *it;
       }
       void next( input_view_t const& input ) {
@@ -133,32 +249,47 @@ public:
       bool end( input_view_t const& input ) const {
         return it == input.end();
       }
-      iterator pos( input_view_t const& input ) const {
-        return it;
-      }
+      iterator pos( input_view_t const& ) const { return it; }
       iterator it;
       Func     func_;
     };
-    using NewView = View<View<InputView, Op>, KeepOp>;
+    using NewView = View<View<InputView, Cursor>, KeepCursor>;
     return NewView( std::move( *this ),
-                    KeepOp( std::move( func ) ) );
+                    KeepCursor( std::move( func ) ) );
+  }
+
+  /**************************************************************
+  ** Remove
+  ***************************************************************/
+  template<typename Func>
+  auto remove( Func&& func ) && {
+    return std::move( *this ).keep(
+        [func = std::forward<Func>( func )]( auto&& arg ) {
+          return !func( arg );
+        } );
   }
 
   /**************************************************************
   ** Map
   ***************************************************************/
   template<typename Func>
-  auto map( Func&& func ) {
-    struct MapOp {
+  auto map( Func&& func ) && {
+    struct MapCursor {
       using input_view_t = View;
       using iterator     = typename input_view_t::iterator;
-      using value_type   = typename input_view_t::value_type;
-      MapOp( Func&& f ) : func_( std::move( f ) ) {}
+      using value_type =
+          std::invoke_result_t<Func,
+                               typename iterator::value_type>;
+#ifdef __clang__ // C++20
+      MapCursor() requires(
+          std::is_default_constructible_v<Func> ) = default;
+#endif
+      MapCursor( Func&& f ) : func_( std::move( f ) ) {}
       void init( input_view_t& input ) {
         it = input.begin();
         if( !end( input ) ) cache = func_( *it );
       }
-      value_type const& get( input_view_t const& input ) const {
+      value_type const& get( input_view_t const& ) const {
         return cache;
       }
       void next( input_view_t const& input ) {
@@ -169,35 +300,37 @@ public:
       bool end( input_view_t const& input ) const {
         return it == input.end();
       }
-      iterator pos( input_view_t const& input ) const {
-        return it;
-      }
+      iterator   pos( input_view_t const& ) const { return it; }
       iterator   it;
       Func       func_;
       value_type cache;
     };
-    using NewView = View<View<InputView, Op>, MapOp>;
+    using NewView = View<View<InputView, Cursor>, MapCursor>;
     return NewView( std::move( *this ),
-                    MapOp( std::move( func ) ) );
+                    MapCursor( std::move( func ) ) );
   }
 
   /**************************************************************
   ** TakeWhile
   ***************************************************************/
   template<typename Func>
-  auto take_while( Func&& func ) {
-    struct TakeWhileOp {
+  auto take_while( Func&& func ) && {
+    struct TakeWhileCursor {
       using input_view_t = View;
       using iterator     = typename input_view_t::iterator;
       using value_type   = typename input_view_t::value_type;
-      TakeWhileOp( Func&& f )
+#ifdef __clang__ // C++20
+      TakeWhileCursor() requires(
+          std::is_default_constructible_v<Func> ) = default;
+#endif
+      TakeWhileCursor( Func&& f )
         : func_( std::move( f ) ), finished_{ false } {}
       void init( input_view_t& input ) {
         it = input.begin();
         if( it == input.end() || !func_( *it ) )
           finished_ = true;
       }
-      value_type const& get( input_view_t const& input ) const {
+      value_type const& get( input_view_t const& ) const {
         rl_assert( !finished_ );
         return *it;
       }
@@ -208,9 +341,7 @@ public:
         if( it == input.end() || !func_( *it ) )
           finished_ = true;
       }
-      bool end( input_view_t const& input ) const {
-        return finished_;
-      }
+      bool end( input_view_t const& ) const { return finished_; }
       iterator pos( input_view_t const& input ) const {
         if( finished_ ) return input.end();
         return it;
@@ -219,36 +350,35 @@ public:
       Func     func_;
       bool     finished_;
     };
-    using NewView = View<View<InputView, Op>, TakeWhileOp>;
+    using NewView =
+        View<View<InputView, Cursor>, TakeWhileCursor>;
     return NewView( std::move( *this ),
-                    TakeWhileOp( std::move( func ) ) );
+                    TakeWhileCursor( std::move( func ) ) );
   }
 };
 
-// template<typename InputView>
-// auto view( InputView&& ) = delete;
-
+/****************************************************************
+** Free-standing view factories.
+*****************************************************************/
 template<typename InputView,
          typename T = typename InputView::value_type>
 auto view( InputView const& input ) {
   using initial_view_t = std::span<T const>;
-  struct DoNothingOp {
+  struct DoNothingCursor {
     using iterator   = typename initial_view_t::iterator;
     using value_type = typename iterator::value_type;
     void init( initial_view_t& input ) { it = input.begin(); }
-    value_type const& get( initial_view_t const& input ) const {
+    value_type const& get( initial_view_t const& ) const {
       return *it;
     }
     void next( initial_view_t const& ) { ++it; }
     bool end( initial_view_t const& input ) const {
       return it == input.end();
     }
-    iterator pos( initial_view_t const& input ) const {
-      return it;
-    }
+    iterator pos( initial_view_t const& ) const { return it; }
     iterator it;
   };
-  return View( initial_view_t( input ), DoNothingOp{} );
+  return View( initial_view_t( input ), DoNothingCursor{} );
 }
 
 // Reverse view.
@@ -256,23 +386,52 @@ template<typename InputView,
          typename T = typename InputView::value_type>
 auto rview( InputView const& input ) {
   using initial_view_t = std::span<T const>;
-  struct ReverseOp {
+  struct ReverseCursor {
     using iterator   = typename initial_view_t::reverse_iterator;
     using value_type = typename iterator::value_type;
     void init( initial_view_t& input ) { it = input.rbegin(); }
-    value_type const& get( initial_view_t const& input ) const {
+    value_type const& get( initial_view_t const& ) const {
       return *it;
     }
     void next( initial_view_t const& ) { ++it; }
     bool end( initial_view_t const& input ) const {
       return it == input.rend();
     }
-    iterator pos( initial_view_t const& input ) const {
-      return it;
-    }
+    iterator pos( initial_view_t const& ) const { return it; }
     iterator it;
   };
-  return View( initial_view_t( input ), ReverseOp{} );
+  return View( initial_view_t( input ), ReverseCursor{} );
+}
+
+/****************************************************************
+** Views from Temporaries
+*****************************************************************/
+template<typename InputView>
+auto view( InputView&& input ) -> std::enable_if_t<
+    std::is_rvalue_reference_v<
+        decltype( std::forward<InputView>( input ) )>,
+    void> = delete;
+
+template<typename InputView>
+auto view_temporary( InputView&& input ) -> std::enable_if_t<
+    std::is_rvalue_reference_v<
+        decltype( std::forward<InputView>( input ) )>,
+    decltype( view( input ) )> {
+  return view( input );
+}
+
+template<typename InputView>
+auto rview( InputView&& input ) -> std::enable_if_t<
+    std::is_rvalue_reference_v<
+        decltype( std::forward<InputView>( input ) )>,
+    void> = delete;
+
+template<typename InputView>
+auto rview_temporary( InputView&& input ) -> std::enable_if_t<
+    std::is_rvalue_reference_v<
+        decltype( std::forward<InputView>( input ) )>,
+    decltype( rview( input ) )> {
+  return rview( input );
 }
 
 } // namespace base::rl
--- test/ranges-lite.cpp
+++ test/ranges-lite.cpp
@@ -20,36 +20,176 @@
 #include "catch-common.hpp"
 
 namespace base {
-namespace {
 
 using namespace std;
 
+// This function exists just to get the return type; we can't de-
+// cltype it since we can't have lambdas in unevaluated contexts.
+//
+// FIXME: can get rid of this once clang gets support for the
+// C++20 feature called "lambdas in unevaluated contexts".
+auto GetCompoundViewType() {
+  return rl::view_temporary( vector<int>{} )
+      .keep( L( _ % 2 == 1 ) )
+      .map( L( _ * _ ) )
+      .map( L( _ + 1 ) )
+      .take_while( L( _ < 27 ) );
+}
+
+namespace {
+
 using ::Catch::Equals;
 
-TEST_CASE( "[ranges-lite] pipeline" ) {
+TEST_CASE( "[ranges-lite] non-materialized" ) {
   vector<int> input{ 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 
-  auto vec = rl::view( input )              //
+  auto vec = rl::view( input )
+                 .keep( L( _ % 2 == 1 ) )
+                 .map( L( _ * _ ) )
+                 .map( L( _ + 1 ) )
+                 .take_while( L( _ < 27 ) );
+
+  static_assert( sizeof( decltype( vec ) ) == 88 );
+}
+
+TEST_CASE( "[ranges-lite] materialized" ) {
+  vector<int> input{ 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+  auto vec = rl::view( input )
                  .keep( L( _ % 2 == 1 ) )   // 1,3,5,7,9
                  .map( L( _ * _ ) )         // 1,9,25,49,81
                  .map( L( _ + 1 ) )         // 2,10,26,50,82
                  .take_while( L( _ < 27 ) ) // 2,10,26
-                 .materialize();
+                 .to_vector();
 
   REQUIRE_THAT( vec, Equals( vector<int>{ 2, 10, 26 } ) );
 }
 
+TEST_CASE( "[ranges-lite] lambdas with capture" ) {
+  vector<int> input{ 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+  int n = 2;
+
+  auto vec = rl::view( input )
+                 .keep( LC( _ % n == 1 ) ) // 1,3,5,7,9
+                 .map( LC( _ * n ) )       // 2,6,10,14,18
+                 .to_vector();
+
+  REQUIRE_THAT( vec, Equals( vector<int>{ 2, 6, 10, 14, 18 } ) );
+}
+
+#ifdef __clang__ // C++20
+TEST_CASE( "[ranges-lite] static create" ) {
+  using type = decltype( GetCompoundViewType() );
+  static_assert( sizeof( type ) == 88 );
+
+  vector<int> input{ 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+  auto        view = type::create( input );
+  static_assert( sizeof( decltype( view ) ) == 88 );
+
+  REQUIRE_THAT( view.to_vector(),
+                Equals( vector<int>{ 2, 10, 26 } ) );
+}
+#endif
+
 TEST_CASE( "[ranges-lite] rview" ) {
   vector<int> input{ 9, 8, 7, 6, 5, 4, 3, 2, 1 };
 
-  auto vec = rl::rview( input )             //
+  auto vec = rl::rview( input )
                  .keep( L( _ % 2 == 1 ) )   // 1,3,5,7,9
                  .map( L( _ * _ ) )         // 1,9,25,49,81
                  .map( L( _ + 1 ) )         // 2,10,26,50,82
                  .take_while( L( _ < 27 ) ) // 2,10,26
-                 .materialize();
+                 .to_vector();
 
   REQUIRE_THAT( vec, Equals( vector<int>{ 2, 10, 26 } ) );
+}
+
+TEST_CASE( "[ranges-lite] macros" ) {
+  vector<int> input{ 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+  auto vec = rl::view( input )
+                 .rl_keep( _ % 2 == 1 )   // 1,3,5,7,9
+                 .rl_map( _ * _ )         // 1,9,25,49,81
+                 .rl_map( _ + 1 )         // 2,10,26,50,82
+                 .rl_take_while( _ < 27 ) // 2,10,26
+                 .to_vector();
+
+  REQUIRE_THAT( vec, Equals( vector<int>{ 2, 10, 26 } ) );
+}
+
+TEST_CASE( "[ranges-lite] head" ) {
+  vector<int> input{ 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+  auto res = rl::view( input )
+                 .keep( L( _ % 2 == 1 ) )   // 1,3,5,7,9
+                 .map( L( _ * _ ) )         // 1,9,25,49,81
+                 .map( L( _ + 1 ) )         // 2,10,26,50,82
+                 .take_while( L( _ < 27 ) ) // 2,10,26
+                 .head();
+
+  REQUIRE( res == 2 );
+
+  auto res2 = rl::view( input ).rl_take_while( _ > 100 ).head();
+
+  REQUIRE( res2 == nothing );
+}
+
+TEST_CASE( "[ranges-lite] remove" ) {
+  vector<int> input{ 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+  auto odd = L( _ % 2 == 1 );
+
+  auto res = rl::view( input ).remove( odd ).to_vector();
+
+  REQUIRE_THAT( res, Equals( vector<int>{ 2, 4, 6, 8 } ) );
+}
+
+TEST_CASE( "[ranges-lite] to" ) {
+  string msg = "1hello 123 with 345 numbers44";
+
+  auto is_num = L( _ >= '0' && _ <= '9' );
+
+  auto res = rl::view( msg ).remove( is_num ).to<string>();
+
+  REQUIRE( res == "hello  with  numbers" );
+}
+
+TEST_CASE( "[ranges-lite] accumulate" ) {
+  vector<int> input{ 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+  auto res = rl::view( input )
+                 .keep( L( _ % 2 == 1 ) ) // 1,3,5,7,9
+                 .map( L( _ * _ ) )       // 1,9,25,49,81
+                 .map( L( _ + 1 ) )       // 2,10,26,50,82
+                 .accumulate();
+
+  REQUIRE( res == 2 + 10 + 26 + 50 + 82 );
+
+  auto res2 = rl::view( input )
+                  .keep( L( _ % 2 == 1 ) ) // 1,3,5,7,9
+                  .map( L( _ * _ ) )       // 1,9,25,49,81
+                  .map( L( _ + 1 ) )       // 2,10,26,50,82
+                  .accumulate( std::multiplies{}, 1 );
+
+  REQUIRE( res2 == 2 * 10 * 26 * 50 * 82 );
+
+  auto res3 =
+      rl::view( input ).rl_map( to_string( _ ) ).accumulate();
+
+  REQUIRE( res2 == 2 * 10 * 26 * 50 * 82 );
+}
+
+TEST_CASE( "[ranges-lite] mixing" ) {
+  vector<int> input{ 1, 22, 333, 4444, 55555, 666666, 7777777 };
+
+  auto res = rl::view( input )
+                 .keep( L( _ % 2 == 1 ) )
+                 .rl_map( to_string( _ ) )
+                 .rl_map( _.size() )
+                 .to_vector();
+
+  REQUIRE_THAT( res, Equals( vector<size_t>{ 1, 3, 5, 7 } ) );
 }
 
 } // namespace
