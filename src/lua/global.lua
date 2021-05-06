07630265429f29a3a82d4432cc4391f2a0733c87
David P. Sicilia <dpsicilia@gmail.com>
2021-05-06 15:09:56 -0400

    Remove uneeded lua module.

--- src/lua/global.lua
+++ src/lua/global.lua
@@ -12,11 +12,15 @@
 -- Global function
 function grid() terrain.toggle_grid() end
 
+-- Take the currently blinking unit and turn it into a soldier.
 function soldier()
   local blinker = assert( land_view.blinking_unit(),
                           'There is currently no unit asking ' ..
                               'for orders.' )
-  ustate.change_to_soldier( land_view.blinking_unit() )
+  local unit = ustate.unit_from_id( blinker )
+  assert( not unit:desc().ship,
+          'Cannot convert a naval unit to a soldier.' )
+  unit:change_type( e.unit_type.soldier )
 end
 
 -- This module does not export anything since its purpose is to
