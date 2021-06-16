bcf9033f52f4a39afda1a0dec0a021aa2cc0e8c5
David P. Sicilia <dpsicilia@gmail.com>
2021-06-16 13:17:23 -0400

    Don't check stack size after a failed test case.

--- test/luapp/common.hpp
+++ test/luapp/common.hpp
@@ -25,7 +25,25 @@ struct harness {
   c_api   C = L;
 
   ~harness() {
-    if( st.alive() ) { CHECK_EQ( C.stack_size(), 0 ); }
+    // As a sanity check, we want to check that when each test
+    // case finishes, the Lua stack has zero size, meaning that
+    // we've accounted for everything that has been pushed. How-
+    // ever, when a test fails for other reasons (e.g. failed RE-
+    // QUIRE) then Catch2 will throw an exception which will pre-
+    // vent us from cleaning up the stack, and so we only check
+    // that the stack size is zero when the test is otherwise
+    // successful.
+    bool test_case_failed = ( std::uncaught_exceptions() > 0 );
+
+    // Also, some test cases need to close the Lua state before
+    // the end of the test in order to test garbage-collection
+    // related things. So if that has happened then it is not
+    // safe to check the stack size, so check for that as well.
+    bool state_closed_early = !st.alive();
+
+    if( !test_case_failed && !state_closed_early ) {
+      CHECK_EQ( C.stack_size(), 0 );
+    }
   }
 };
 
