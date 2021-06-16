5e8e2ee85826ac62174590acfb0a716e088f8053
David P. Sicilia <dpsicilia@gmail.com>
2021-06-16 11:32:07 -0400

    Fix o.sh so that it works with files in subfolders.

--- scripts/o.sh
+++ scripts/o.sh
@@ -19,13 +19,31 @@ set -o pipefail
   exit 1
 }
 
+die() {
+  echo "$@" >&2
+  exit 1
+}
+
 path=$1
 
 file="$(basename "$path")"
 
-dir="$(dirname "$path")"
+file_dir="$(dirname "$path")"
 
-cmake="$dir/CMakeLists.txt"
+dir="$file_dir"
+while true; do
+  cmake="$dir/CMakeLists.txt"
+  [[ -e $cmake ]] && break
+  [[ $dir == . ]] && \
+    die "cannot locate CMakeLists.txt file for file $file."
+  dir="$(dirname "$dir")"
+  cmake="$dir/CMakeLists.txt"
+done
+
+# We still need the folder structure under the folder containing
+# the CMakeLists.txt because that folder structure is preserved
+# in the build directory.
+sub_folder=${file_dir/$dir}
 
 # Hack for getting the CMake target name in the source folder,
 # since we need that to form the path to the .o file and hence
@@ -37,7 +55,7 @@ cmake="$dir/CMakeLists.txt"
 # from which the target name is extracted.
 target="$(cat "$cmake" | sed -rn 's/set_warning_options\( (.*) \)/\1/p')"
 
-obj="$dir/CMakeFiles/$target.dir/$file.o"
+obj="$dir/CMakeFiles/$target.dir$sub_folder/$file.o"
 
 cd .builds/current
 [[ -f "env-vars.mk" ]] && eval "$(cat env-vars.mk | sed 's/ = /=/')"
