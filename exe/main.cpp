7779fe132c2238dd43a7b563cb906417110b787d
David P. Sicilia <dpsicilia@gmail.com>
2021-07-20 11:47:58 -0400

    Add ui tester to main.

--- exe/main.cpp
+++ exe/main.cpp
@@ -6,34 +6,48 @@
 #include "logging.hpp"
 #include "lua.hpp"
 #include "open-gl.hpp"
+#include "plane-ctrl.hpp"
 #include "screen.hpp"
 #include "util.hpp"
 
 using namespace rn;
 using namespace std;
 
-bool do_game = true;
+enum class e_mode { game, ui_test, gl_test };
 
-int main( int /*unused*/, char** /*unused*/ ) {
-  linker_dont_discard_me();
-  print_bar( '=', "[ Revolution | Now ]" );
-  try {
-    if( do_game ) {
+waitable<> ui_test() {
+  ScopedPlanePush pusher( e_plane_config::black );
+  return make_waitable<>();
+}
+
+void run( e_mode mode ) {
+  switch( mode ) {
+    case e_mode::game:
       run_all_init_routines( e_log_level::debug );
       lua_reload();
       run_lua_startup_main();
       print_bar( '-', "[ Starting Game ]" );
       // The action.
       frame_loop( revolution_now() );
-    } else {
+      break;
+    case e_mode::ui_test: //
+      frame_loop( ui_test() );
+      break;
+    case e_mode::gl_test:
       run_all_init_routines(
           e_log_level::debug,
           { e_init_routine::screen, e_init_routine::lua } );
       test_open_gl();
-    }
+      break;
+  }
+}
 
+int main( int /*unused*/, char** /*unused*/ ) {
+  linker_dont_discard_me();
+  print_bar( '=', "[ Revolution | Now ]" );
+  try {
+    run( e_mode::game );
   } catch( exception_exit const& ) {}
-
   hide_window();
   print_bar( '-', "[ Shutting Down ]" );
   run_all_cleanup_routines();
