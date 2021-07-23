fcaf84af415739f7cc05fe92b603add46c970a7e
David P. Sicilia <dpsicilia@gmail.com>
2021-07-22 23:09:32 -0400

    A bit of code simplification.

--- src/co-lua.hpp
+++ src/co-lua.hpp
@@ -18,6 +18,7 @@
 // luapp
 #include "luapp/any.hpp"
 #include "luapp/cast.hpp"
+#include "luapp/ext-std.hpp"
 #include "luapp/ext-userdata.hpp"
 #include "luapp/rfunction.hpp"
 #include "luapp/rthread.hpp"
@@ -63,16 +64,22 @@ template<GettableOrMonostate R>
 struct LuaWaitable {
   template<LuaAwaitable T, lua::Pushable... Args>
   waitable<R> operator()( T&& o, Args&&... args ) const {
-    static constexpr bool is_mono =
-        std::is_same_v<R, std::monostate>;
     lua::rthread coro =
         internal::create_runner_coro( o.this_cthread() );
     // Ensure that all to-be-closed variables get closed and the
     // coroutine gets de-queued if an error happens.
     SCOPE_EXIT( internal::cleanup_coro( coro ) );
 
-    lua::rfunction      f = lua::cast<lua::rfunction>( o );
     waitable_promise<R> p;
+
+    auto set_result = [&]( lua::any res ) {
+      p.set_value( lua::cast<R>( res ) );
+    };
+    auto set_error = [&]( std::string msg ) {
+      p.template set_exception_emplace<lua_error_exception>(
+          std::move( msg ) );
+    };
+
     // Start running it; if it finishes right away then the re-
     // sulting waitable should be ready right away. If not, then
     // we will suspend on the co_await below. In other words, we
@@ -83,31 +90,20 @@ struct LuaWaitable {
     // Although this version of `resume` will propagate errors
     // (exceptions), that is not relevant here, because the func-
     // tion we are immediately calling (the "runner") uses pcallk
-    // to call `f`, so all errors are caught and diverted into
+    // to call `o`, so all errors are caught and diverted into
     // the promise as stored exceptions. We could have used
     // resume_safe here, it doesn't make a difference, especially
     // because the return type is void and so we won't have any
     // errors converting the return type.
-    coro.resume( /*set_result=*/
-                 [&]( lua::any res ) {
-                   if constexpr( is_mono )
-                     p.set_value_emplace();
-                   else
-                     p.set_value( lua::cast<R>( res ) );
-                 },
-                 /*set_error=*/
-                 [&]( std::string msg ) {
-                   p.template set_exception_emplace<
-                       lua_error_exception>( std::move( msg ) );
-                 },
-                 f, FWD( args )... );
+    coro.resume( std::move( set_result ), std::move( set_error ),
+                 FWD( o ), FWD( args )... );
 
     // If an exception already happened before the first yield
     // point, then the waitable will have an exception in it, and
     // it will be dealt with by the co_await.
     //
     // Need to keep the SCOPE_EXIT alive while we wait.
-    if constexpr( is_mono )
+    if constexpr( std::is_same_v<R, std::monostate> )
       co_await p.waitable();
     else
       co_return co_await p.waitable();
