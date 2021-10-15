45a957af60d8cc41f178352369a18f9bfc15e14d
David P. Sicilia <dpsicilia@gmail.com>
2021-10-15 17:33:34 -0400

    Add ExhaustiveEnumMap data structure.

---
+++ src/enum-map.hpp
@@ -0,0 +1,150 @@
+/****************************************************************
+**enum-map.hpp
+*
+* Project: Revolution Now
+*
+* Created by dsicilia on 2021-10-10.
+*
+* Description: Map data structure for exhaustive enum keys.
+*
+*****************************************************************/
+#pragma once
+
+#include "core-config.hpp"
+
+// Revolution Now
+#include "maybe.hpp"
+
+// Rcl
+#include "rcl/ext.hpp"
+
+// Rds
+#include "rds/helper/enum.hpp"
+
+// base
+#include "base/cc-specific.hpp"
+#include "base/fmt.hpp"
+
+// C++ standard library
+#include <array>
+#include <memory>
+
+namespace rn {
+
+template<ReflectedEnum Enum, typename ValT>
+struct ExhaustiveEnumMap {
+  static constexpr int kSize = enum_traits<Enum>::count;
+
+private:
+  using ArrayT = std::array<std::pair<Enum, ValT>, kSize>;
+
+public:
+  ExhaustiveEnumMap() : data_( std::make_unique<ArrayT>() ) {
+    for( int i = 0; i < kSize; ++i )
+      ( *data_ )[i] = { from_idx( i ), {} };
+  }
+
+  ExhaustiveEnumMap(
+      std::initializer_list<std::pair<Enum, ValT>> il )
+    : ExhaustiveEnumMap() {
+    for( auto& [e, v] : il ) at( e ) = std::move( v );
+  }
+
+  // This map always has a fixed size, so you should not need to
+  // query its size. If you need to know its size, use kSize.
+  constexpr int size() const = delete;
+
+  bool operator==( ExhaustiveEnumMap const& rhs ) const {
+    return *data_ == *rhs.data_;
+  }
+
+  ExhaustiveEnumMap& operator=( ExhaustiveEnumMap const& rhs ) {
+    *data_ = *rhs.data_;
+    return *this;
+  }
+
+  ExhaustiveEnumMap( ExhaustiveEnumMap&& ) = default;
+  ExhaustiveEnumMap& operator=( ExhaustiveEnumMap&& ) = default;
+
+  ValT const& operator[]( Enum i ) const { return at( i ); }
+
+  ValT& operator[]( Enum i ) { return at( i ); }
+
+  ValT const& at( Enum i ) const {
+    return ( *data_ )[to_idx( i )].second;
+  }
+
+  ValT& at( Enum i ) { return ( *data_ )[to_idx( i )].second; }
+
+  auto find( Enum what ) const {
+    int idx = to_idx( what );
+    return begin() + idx;
+  }
+
+  // This is for deserializing from Rcl config files.
+  friend rcl::convert_err<ExhaustiveEnumMap> convert_to(
+      rcl::value const& v, rcl::tag<ExhaustiveEnumMap> ) {
+    static std::string kTypeName =
+        fmt::format( "{}<{}, {}>", "ExhaustiveEnumMap",
+                     base::demangled_typename<Enum>(),
+                     base::demangled_typename<ValT>() );
+    constexpr int kNumFieldsExpected = kSize;
+    base::maybe<std::unique_ptr<rcl::table> const&> mtbl =
+        v.get_if<std::unique_ptr<rcl::table>>();
+    if( !mtbl )
+      return rcl::error( fmt::format(
+          "cannot produce a {} from type {}.", kTypeName,
+          rcl::name_of( rcl::type_of( v ) ) ) );
+    DCHECK( *mtbl != nullptr );
+    rcl::table const& tbl = **mtbl;
+    if( tbl.size() != kNumFieldsExpected )
+      return rcl::error( fmt::format(
+          "table must have precisely {} field(s) for "
+          "conversion to {}.",
+          kNumFieldsExpected, kTypeName ) );
+    ExhaustiveEnumMap res;
+    for( auto& [key, val] : tbl ) {
+      UNWRAP_RETURN( key_conv, rcl::convert_to<Enum>( key ) );
+      UNWRAP_RETURN( val_conv, rcl::convert_to<ValT>( val ) );
+      // We don't need to check for duplicates here because that
+      // will already have been done by the rcl parsing.
+      res[key_conv] = std::move( val_conv );
+    }
+    return res;
+  }
+
+  auto begin() { return data_->begin(); }
+  auto begin() const { return data_->begin(); }
+  auto end() { return data_->end(); }
+  auto end() const { return data_->end(); }
+
+  std::string to_str() const {
+    std::string res = "[";
+    for( auto const& [k, v] : *this )
+      res += fmt::format( "{}={},", k, v );
+    if( res.size() > 1 )
+      res.resize( res.size() - 1 ); // remove trailing comma.
+    res += ']';
+    return res;
+  }
+
+private:
+  static int to_idx( Enum i ) {
+    int idx = static_cast<int>( i );
+    DCHECK( idx < kSize );
+    DCHECK( idx >= 0 );
+    return idx;
+  }
+
+  static Enum from_idx( int i ) {
+    UNWRAP_CHECK( e, enum_traits<Enum>::from_integral( i ) );
+    return e;
+  }
+
+  std::unique_ptr<ArrayT> data_;
+};
+
+} // namespace rn
+
+EVAL( DEFINE_FORMAT_T( ( K, V ), (::rn::ExhaustiveEnumMap<K, V>),
+                       "{}", o.to_str() ) );
---
+++ test/enum-map.cpp
@@ -0,0 +1,176 @@
+/****************************************************************
+**enum-map.cpp
+*
+* Project: Revolution Now
+*
+* Created by dsicilia on 2021-10-10.
+*
+* Description: Unit tests for the src/enum-map.* module.
+*
+*****************************************************************/
+#include "test/testing.hpp"
+
+// Under test.
+#include "src/enum-map.hpp"
+
+// Rds
+#include "rds/helper/rcl.hpp"
+#include "rds/testing.hpp"
+
+// Rcl
+#include "rcl/ext-std.hpp"
+
+// Must be last.
+#include "test/catch-common.hpp"
+
+namespace rn {
+namespace {
+
+using namespace std;
+
+using ::Catch::Matches;
+
+// If this changes then the below tests may need to be updated.
+static_assert( enum_traits<e_color>::count == 3 );
+
+static_assert( is_default_constructible_v<
+               ExhaustiveEnumMap<e_color, int>> );
+static_assert(
+    is_move_constructible_v<ExhaustiveEnumMap<e_color, int>> );
+static_assert(
+    is_move_assignable_v<ExhaustiveEnumMap<e_color, int>> );
+static_assert( is_nothrow_move_constructible_v<
+               ExhaustiveEnumMap<e_color, int>> );
+static_assert( is_nothrow_move_assignable_v<
+               ExhaustiveEnumMap<e_color, int>> );
+
+TEST_CASE( "[enum-map] ExhaustiveEnumMap empty" ) {
+  ExhaustiveEnumMap<e_empty, int> m;
+  static_assert( m.kSize == 0 );
+}
+
+TEST_CASE(
+    "[enum-map] ExhaustiveEnumMap non-primitive construction" ) {
+  ExhaustiveEnumMap<e_color, string> m;
+  static_assert( m.kSize == 3 );
+  REQUIRE( m[e_color::red] == "" );
+  REQUIRE( m[e_color::green] == "" );
+  REQUIRE( m[e_color::blue] == "" );
+}
+
+TEST_CASE(
+    "[enum-map] ExhaustiveEnumMap primitive initialization" ) {
+  ExhaustiveEnumMap<e_color, int> m;
+  static_assert( m.kSize == 3 );
+  REQUIRE( m[e_color::red] == 0 );
+  REQUIRE( m[e_color::green] == 0 );
+  REQUIRE( m[e_color::blue] == 0 );
+}
+
+TEST_CASE( "[enum-map] ExhaustiveEnumMap indexing" ) {
+  ExhaustiveEnumMap<e_color, int> m;
+  m.at( e_color::red ) = 5;
+  REQUIRE( m[e_color::red] == 5 );
+  m[e_color::green] = 7;
+  REQUIRE( m.at( e_color::green ) == 7 );
+  ExhaustiveEnumMap<e_color, int> const m_const;
+  REQUIRE( m_const[e_color::red] == 0 );
+  REQUIRE( m_const.at( e_color::green ) == 0 );
+}
+
+TEST_CASE( "[enum-map] ExhaustiveEnumMap find" ) {
+  ExhaustiveEnumMap<e_color, int> m;
+  m[e_color::green] = 7;
+  auto it           = m.find( e_color::green );
+  REQUIRE( it != m.end() );
+  REQUIRE( it->first == e_color::green );
+  REQUIRE( it->second == 7 );
+}
+
+TEST_CASE( "[enum-map] ExhaustiveEnumMap equality" ) {
+  ExhaustiveEnumMap<e_color, int> m1;
+  ExhaustiveEnumMap<e_color, int> m2;
+  REQUIRE( m1 == m2 );
+  REQUIRE( m1[e_color::blue] == 0 );
+  REQUIRE( m1 == m2 );
+  m1[e_color::blue] = 5;
+  REQUIRE( m1 != m2 );
+  m1[e_color::blue] = 0;
+  REQUIRE( m1 == m2 );
+  m1[e_color::red] = 2;
+  REQUIRE( m1 != m2 );
+  m2 = m1;
+  REQUIRE( m1 == m2 );
+  REQUIRE( m1[e_color::red] == 2 );
+  REQUIRE( m1[e_color::green] == 0 );
+  REQUIRE( m1[e_color::blue] == 0 );
+}
+
+TEST_CASE( "[enum-map] ExhaustiveEnumMap Rcl" ) {
+  using KV = rcl::table::value_type;
+  rcl::table t =
+      rcl::make_table( KV{ "red", "one" }, KV{ "green", "two" },
+                       KV{ "blue", "three" } );
+  UNWRAP_CHECK( ppt, rcl::run_postprocessing( std::move( t ) ) );
+  rcl::value v{
+      std::make_unique<rcl::table>( std::move( ppt ) ) };
+
+  // Test.
+  ExhaustiveEnumMap<e_color, string> expected{
+      { e_color::red, "one" },
+      { e_color::green, "two" },
+      { e_color::blue, "three" } };
+  REQUIRE( rcl::convert_to<ExhaustiveEnumMap<e_color, string>>(
+               v ) == expected );
+}
+
+TEST_CASE( "[enum-map] ExhaustiveEnumMap Rcl e_empty" ) {
+  using KV     = rcl::table::value_type;
+  rcl::table t = rcl::make_table();
+  UNWRAP_CHECK( ppt, rcl::run_postprocessing( std::move( t ) ) );
+  rcl::value v{
+      std::make_unique<rcl::table>( std::move( ppt ) ) };
+
+  // Test.
+  ExhaustiveEnumMap<e_empty, string> expected;
+  REQUIRE( ( rcl::convert_to<ExhaustiveEnumMap<e_empty, string>>(
+                 v ) == expected ) );
+}
+
+TEST_CASE( "[enum-map] ExhaustiveEnumMap Rcl non-exhaustive" ) {
+  using KV     = rcl::table::value_type;
+  rcl::table t = rcl::make_table( KV{ "green", "two" },
+                                  KV{ "blue", "three" } );
+  UNWRAP_CHECK( ppt, rcl::run_postprocessing( std::move( t ) ) );
+  rcl::value v{
+      std::make_unique<rcl::table>( std::move( ppt ) ) };
+
+  // Test.
+  auto res =
+      rcl::convert_to<ExhaustiveEnumMap<e_color, string>>( v );
+  REQUIRE( res.has_error() );
+  REQUIRE_THAT(
+      res.error().what,
+      Matches(
+          "table must have precisely 3 field\\(s\\) for "
+          "conversion to ExhaustiveEnumMap.rn::e_color.*" ) );
+}
+
+TEST_CASE( "[enum-map] ExhaustiveEnumMap Rcl bad enum" ) {
+  using KV = rcl::table::value_type;
+  rcl::table t =
+      rcl::make_table( KV{ "red", "one" }, KV{ "greenx", "two" },
+                       KV{ "blue", "three" } );
+  UNWRAP_CHECK( ppt, rcl::run_postprocessing( std::move( t ) ) );
+  rcl::value v{
+      std::make_unique<rcl::table>( std::move( ppt ) ) };
+
+  // Test.
+  REQUIRE(
+      rcl::convert_to<ExhaustiveEnumMap<e_color, string>>( v ) ==
+      rcl::error( "failed to parse string `greenx' into valid "
+                  "e_color enum value." ) );
+}
+
+} // namespace
+} // namespace rn
