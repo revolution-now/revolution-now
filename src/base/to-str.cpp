3c0e602f22362543d39213927388f92131e4ab7d
David P. Sicilia <dpsicilia@gmail.com>
2022-05-06 12:02:35 -0400

    Add size_t overload for to_str.

--- src/base/to-str.cpp
+++ src/base/to-str.cpp
@@ -22,6 +22,10 @@ void to_str( uint32_t const& o, string& out, ADL_t ) {
   out += to_string( o );
 }
 
+void to_str( size_t const& o, string& out, ADL_t ) {
+  out += to_string( o );
+}
+
 void to_str( long const& o, string& out, ADL_t ) {
   out += to_string( o );
 }
