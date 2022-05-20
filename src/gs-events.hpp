159733d7c22f07eea1fb0391d0f82148dc44dbf0
David P. Sicilia <dpsicilia@gmail.com>
2022-05-20 09:50:47 -0400

    Add gs-events header.

---
+++ src/gs-events.hpp
@@ -0,0 +1,20 @@
+/****************************************************************
+**gs-events.hpp
+*
+* Project: Revolution Now
+*
+* Created by dsicilia on 2022-05-19.
+*
+* Description: Represents events that can happen once (or a small
+*              number of times in the game) and cause the state
+*              of the game to change.
+*
+*****************************************************************/
+#pragma once
+
+#include "core-config.hpp"
+
+// Rds
+#include "gs-events.rds.hpp"
+
+namespace rn {} // namespace rn
