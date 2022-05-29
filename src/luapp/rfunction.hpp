057e2b93fdabf7fe4e5fc49c0e99dbb4309d1bf0
David P. Sicilia <dpsicilia@gmail.com>
2022-05-29 13:00:26 -0400

    Add missing const to some methods.

--- src/luapp/rfunction.hpp
+++ src/luapp/rfunction.hpp
@@ -32,27 +32,27 @@ struct rfunction : public any {
                                          tag<rfunction> );
 
   template<Pushable... Args>
-  any operator()( Args&&... args );
+  any operator()( Args&&... args ) const;
 
   template<GettableOrVoid R = void, Pushable... Args>
-  R call( Args&&... args );
+  R call( Args&&... args ) const;
 
   template<GettableOrVoid R = void, Pushable... Args>
-  error_type_for_return_type<R> pcall( Args&&... args );
+  error_type_for_return_type<R> pcall( Args&&... args ) const;
 };
 
 /****************************************************************
 ** Template implementations.
 *****************************************************************/
 template<Pushable... Args>
-any rfunction::operator()( Args&&... args ) {
+any rfunction::operator()( Args&&... args ) const {
   cthread L = this_cthread();
   push( L, *this );
   return call_lua_unsafe_and_get<any>( L, FWD( args )... );
 }
 
 template<GettableOrVoid R, Pushable... Args>
-R rfunction::call( Args&&... args ) {
+R rfunction::call( Args&&... args ) const {
   cthread L = this_cthread();
   push( L, *this );
   return call_lua_unsafe_and_get<R>( L, FWD( args )... );
@@ -60,7 +60,7 @@ R rfunction::call( Args&&... args ) {
 
 template<GettableOrVoid R, Pushable... Args>
 error_type_for_return_type<R> rfunction::pcall(
-    Args&&... args ) {
+    Args&&... args ) const {
   cthread L = this_cthread();
   push( L, *this );
   return call_lua_safe_and_get<R>( L, FWD( args )... );
