e80ab70aea9928eec39004234b3711f264e951f7
David P. Sicilia <dpsicilia@gmail.com>
2022-06-14 10:58:52 -0400

    Add unit test module for the colony-evolve module.

---
+++ test/colony-evolve.cpp
@@ -0,0 +1,25 @@
+/****************************************************************
+**colony-evolve.cpp
+*
+* Project: Revolution Now
+*
+* Created by dsicilia on 2022-06-06.
+*
+* Description: Unit tests for the src/colony-evolve.* module.
+*
+*****************************************************************/
+#include "test/testing.hpp"
+
+// Under test.
+#include "src/colony-evolve.hpp"
+
+// Must be last.
+#include "test/catch-common.hpp"
+
+namespace rn {
+namespace {
+
+using namespace std;
+
+} // namespace
+} // namespace rn
