e561ecdee665ae45cefd986596cb742121f57d12
David P. Sicilia <dpsicilia@gmail.com>
2022-07-17 21:59:27 -0400

    Add test case for commodity spoilage in colony.

--- test/colony-evolve.cpp
+++ test/colony-evolve.cpp
@@ -16,6 +16,12 @@
 // Testing.
 #include "test/fake/world.hpp"
 
+// refl
+#include "refl/to-str.hpp"
+
+// base
+#include "base/to-str-ext-std.hpp"
+
 // Must be last.
 #include "test/catch-common.hpp"
 
@@ -29,7 +35,10 @@ using namespace std;
 *****************************************************************/
 struct World : testing::World {
   using Base = testing::World;
-  World() : Base() { add_player( e_nation::dutch ); }
+  World() : Base() {
+    add_player( e_nation::dutch );
+    create_default_map();
+  }
 
   void create_default_map() {
     MapSquare const _ = make_ocean();
@@ -48,33 +57,139 @@ struct World : testing::World {
 /****************************************************************
 ** Test Cases
 *****************************************************************/
+TEST_CASE( "[colony-evolve] spoilage" ) {
+  World   W;
+  Colony& colony = W.add_colony( { .x = 1, .y = 1 } );
+  for( e_commodity c : refl::enum_values<e_commodity> )
+    colony.commodities[c] = 101;
+  colony.commodities[e_commodity::food] = 150;
+  ColonyEvolution ev =
+      evolve_colony_one_turn( W.ss(), W.ts(), colony );
+  REQUIRE( ev.production.center_food_production == 5 );
+  // Food is still 150 despite the 5 food produced in the center
+  // square because of horse production, which coincidentally
+  // happens to allow 5 horses (101+24)/25=5 (each consuming one
+  // of the 5 surplus food). That said, the increase in horses
+  // won't be reflected in any of the numbers below because the
+  // new horses will be suppressed because the number of horses
+  // is already over capacity (so the number of horses that will
+  // be reported as having spoiled is actually only 1).
+  REQUIRE( colony.commodities[e_commodity::food] == 150 );
+  REQUIRE( colony.commodities[e_commodity::sugar] == 100 );
+  REQUIRE( colony.commodities[e_commodity::tobacco] == 100 );
+  REQUIRE( colony.commodities[e_commodity::cotton] == 100 );
+  REQUIRE( colony.commodities[e_commodity::fur] == 100 );
+  REQUIRE( colony.commodities[e_commodity::lumber] == 100 );
+  REQUIRE( colony.commodities[e_commodity::ore] == 100 );
+  REQUIRE( colony.commodities[e_commodity::silver] == 100 );
+  REQUIRE( colony.commodities[e_commodity::horses] == 100 );
+  REQUIRE( colony.commodities[e_commodity::rum] == 100 );
+  REQUIRE( colony.commodities[e_commodity::cigars] == 100 );
+  REQUIRE( colony.commodities[e_commodity::cloth] == 100 );
+  REQUIRE( colony.commodities[e_commodity::coats] == 100 );
+  REQUIRE( colony.commodities[e_commodity::trade_goods] == 100 );
+  REQUIRE( colony.commodities[e_commodity::tools] == 100 );
+  REQUIRE( colony.commodities[e_commodity::muskets] == 100 );
+
+  // In practice, for a new empty colony, there should not be any
+  // other notifications.
+  vector<ColonyNotification_t> expected{
+      ColonyNotification::spoilage{
+          .spoiled = {
+              { .type = e_commodity::sugar, .quantity = 1 },
+              { .type = e_commodity::tobacco, .quantity = 1 },
+              { .type = e_commodity::cotton, .quantity = 1 },
+              { .type = e_commodity::fur, .quantity = 1 },
+              { .type = e_commodity::lumber, .quantity = 1 },
+              { .type = e_commodity::ore, .quantity = 1 },
+              { .type = e_commodity::silver, .quantity = 1 },
+              { .type = e_commodity::horses, .quantity = 1 },
+              { .type = e_commodity::rum, .quantity = 1 },
+              { .type = e_commodity::cigars, .quantity = 1 },
+              { .type = e_commodity::cloth, .quantity = 1 },
+              { .type = e_commodity::coats, .quantity = 1 },
+              { .type     = e_commodity::trade_goods,
+                .quantity = 1 },
+              { .type = e_commodity::tools, .quantity = 1 },
+              { .type = e_commodity::muskets, .quantity = 1 },
+          } } };
+
+  REQUIRE( ev.notifications == expected );
+
+  // Now repeat but with a warehouse.
+  colony.buildings[e_colony_building::warehouse] = true;
+
+  for( e_commodity c : refl::enum_values<e_commodity> )
+    colony.commodities[c] = 201;
+  colony.commodities[e_commodity::food] = 800;
+  ev = evolve_colony_one_turn( W.ss(), W.ts(), colony );
+  REQUIRE( ev.production.center_food_production == 5 );
+  // Food goes up by 5 due to center square production and down
+  // by 5 due to horse breeding, then down 200 due to new
+  // colonist being produced, putting the final value at 600. See
+  // corresponding comment above for more info on the horse
+  // breeding.
+  REQUIRE( colony.commodities[e_commodity::food] == 600 );
+  REQUIRE( colony.commodities[e_commodity::sugar] == 200 );
+  REQUIRE( colony.commodities[e_commodity::tobacco] == 200 );
+  REQUIRE( colony.commodities[e_commodity::cotton] == 200 );
+  REQUIRE( colony.commodities[e_commodity::fur] == 200 );
+  REQUIRE( colony.commodities[e_commodity::lumber] == 200 );
+  REQUIRE( colony.commodities[e_commodity::ore] == 200 );
+  REQUIRE( colony.commodities[e_commodity::silver] == 200 );
+  REQUIRE( colony.commodities[e_commodity::horses] == 200 );
+  REQUIRE( colony.commodities[e_commodity::rum] == 200 );
+  REQUIRE( colony.commodities[e_commodity::cigars] == 200 );
+  REQUIRE( colony.commodities[e_commodity::cloth] == 200 );
+  REQUIRE( colony.commodities[e_commodity::coats] == 200 );
+  REQUIRE( colony.commodities[e_commodity::trade_goods] == 200 );
+  REQUIRE( colony.commodities[e_commodity::tools] == 200 );
+  REQUIRE( colony.commodities[e_commodity::muskets] == 200 );
+
+  // In practice, for a new empty colony, there should not be any
+  // other notifications.
+  expected = {
+      ColonyNotification::new_colonist{ .id = 1 },
+      ColonyNotification::spoilage{
+          .spoiled = {
+              { .type = e_commodity::sugar, .quantity = 1 },
+              { .type = e_commodity::tobacco, .quantity = 1 },
+              { .type = e_commodity::cotton, .quantity = 1 },
+              { .type = e_commodity::fur, .quantity = 1 },
+              { .type = e_commodity::lumber, .quantity = 1 },
+              { .type = e_commodity::ore, .quantity = 1 },
+              { .type = e_commodity::silver, .quantity = 1 },
+              { .type = e_commodity::horses, .quantity = 1 },
+              { .type = e_commodity::rum, .quantity = 1 },
+              { .type = e_commodity::cigars, .quantity = 1 },
+              { .type = e_commodity::cloth, .quantity = 1 },
+              { .type = e_commodity::coats, .quantity = 1 },
+              { .type     = e_commodity::trade_goods,
+                .quantity = 1 },
+              { .type = e_commodity::tools, .quantity = 1 },
+              { .type = e_commodity::muskets, .quantity = 1 },
+          } } };
+
+  REQUIRE( ev.notifications == expected );
+}
+
 TEST_CASE( "[colony-evolve] applies production" ) {
   World W;
-  W.create_default_map();
   // TODO
 }
 
 TEST_CASE( "[colony-evolve] construction" ) {
   World W;
-  W.create_default_map();
   // TODO
 }
 
 TEST_CASE( "[colony-evolve] new colonist" ) {
   World W;
-  W.create_default_map();
   // TODO
 }
 
 TEST_CASE( "[colony-evolve] colonist starved" ) {
   World W;
-  W.create_default_map();
-  // TODO
-}
-
-TEST_CASE( "[colony-evolve] spoilage" ) {
-  World W;
-  W.create_default_map();
   // TODO
 }
 
