f319647c1ace1aa897d621b19b8ff25c4285b111
David P. Sicilia <dpsicilia@gmail.com>
2022-08-27 20:57:53 -0400

    Updates to the price model script.

--- scripts/price-group.lua
+++ scripts/price-group.lua
@@ -10,10 +10,14 @@
 |              model from the original game.
 |
 --]] ------------------------------------------------------------
-local goods = { 'rum', 'cigars', 'cloth', 'coats' }
-local equilibrium_prices =
-    { rum=13, cigars=10, cloth=9, coats=10 }
-local initial_prices = { rum=13, cigars=10, cloth=9, coats=10 }
+local GOODS = { 'rum', 'cigars', 'cloth', 'coats' }
+local EQ_PRICES = { rum=10, cigars=13, cloth=10, coats=9 }
+local INITIAL_GOLD = 0
+-- By initializing this to 'e', which means "evolve", the user
+-- can just run the program and immediately start hitting <enter>
+-- to start evolving the initial prices without buying or selling
+-- anything.
+local INITIAL_CMD = 'e'
 
 -----------------------------------------------------------------
 -- State
@@ -24,14 +28,17 @@ local volumes = { rum=0, cigars=0, cloth=0, coats=0 }
 
 local num_turns = 0
 
-local gold = 100000
+local gold = INITIAL_GOLD
+
+local last_cmd = INITIAL_CMD
 
 -----------------------------------------------------------------
 -- Model
 -----------------------------------------------------------------
-local RF = 4 * 100 -- rise/fall
+local RF = 4 * 100
 local VOL = 1
-local EQ_SUM = 42
+local EQ_SUM_MIN = 43
+local EQ_SUM_MAX = 43
 
 local params = {
   -- LuaFormatter off
@@ -52,12 +59,14 @@ local function target_price( good )
   assert( good )
   local p = params[good]
   local v = volumes[good]
-  local eq = equilibrium_prices[good]
+  local eq = EQ_PRICES[good]
   local res = eq
   if v > 0 then res = res - (v // p.rise) end
+  -- if v > 0 then res = res - (v // 100) end
   -- The // integer division operator does weird things when the
   -- numerator is negative, so we'll make it positive.
   if v < 0 then res = res + ((-v) // p.fall) end
+  -- if v < 0 then res = res + ((-v) // 100) end
   return res
 end
 
@@ -76,61 +85,18 @@ local function update_price( good )
   prices[good] = new_price
 end
 
-local function buy( good_to_buy )
-  local p = params[good_to_buy]
-  local quantity = 100
-  gold = gold - quantity * prices[good_to_buy]
-  local sum = price_sum()
-  quantity = quantity * (1 << p.volatility)
-  local volume = quantity
-
-  if sum > EQ_SUM then
-    volume = math.max( volume - p.rise, 0 )
-  end
-  for _, good in ipairs( goods ) do
-    if good ~= good_to_buy then
-      volumes[good] = volumes[good] + (volume // 3)
-    end
-  end
-
-  volumes[good_to_buy] = volumes[good_to_buy] - volume
-  update_price( good_to_buy )
-end
-
-local function sell( good_to_sell )
-  local p = params[good_to_sell]
-  local quantity = 100
-  gold = gold + quantity * prices[good_to_sell]
-  local sum = price_sum()
-  quantity = quantity * (1 << p.volatility)
-  local volume = quantity
-
-  if sum < EQ_SUM then
-    volume = math.max( volume - p.fall, 0 )
-  end
-  for _, good in ipairs( goods ) do
-    if good ~= good_to_buy then
-      volumes[good] = volumes[good] - (volume // 3)
-    end
-  end
-
-  volumes[good_to_sell] = volumes[good_to_sell] + volume
-  update_price( good_to_sell )
-end
-
 -- One round of price recovery for one good. Need to pass in the
 -- current prices sum here because we want to use the one com-
 -- puted before any of the goods begin their recovery.
 local function recover( sum, good )
   local p = params[good]
   local bought_more_than_sold = (volumes[good] < 0)
-  local price_avg_too_high = (sum > EQ_SUM)
+  local price_avg_too_high = (sum > EQ_SUM_MAX)
+  local sold_more_than_bought = (volumes[good] > 0)
+  local price_avg_too_low = (sum < EQ_SUM_MIN)
   if price_avg_too_high and bought_more_than_sold then
     volumes[good] = volumes[good] + p.fall
-  end
-  local sold_more_than_bought = (volumes[good] > 0)
-  local price_avg_too_los = (sum < EQ_SUM)
-  if price_avg_too_los and sold_more_than_bought then
+  elseif price_avg_too_low and sold_more_than_bought then
     volumes[good] = volumes[good] - p.rise
   end
   update_price( good )
@@ -142,7 +108,30 @@ end
 local function evolve()
   local sum = price_sum()
   -- Recovery model.
-  for _, good in ipairs( goods ) do recover( sum, good ) end
+  for _, good in ipairs( GOODS ) do recover( sum, good ) end
+end
+
+local function buy( good_to_buy )
+  -- TODO
+end
+
+local function sell( good_to_sell )
+  local p = params[good_to_sell]
+  local quantity = 100
+  gold = gold + quantity * prices[good_to_sell]
+  local sum = price_sum()
+  local volume = quantity * (1 << p.volatility)
+
+  volumes[good_to_sell] = volumes[good_to_sell] + volume
+
+  local delta = EQ_PRICES[good_to_sell] - prices[good_to_sell]
+  for _, good in ipairs( GOODS ) do
+    volumes[good] = volumes[good] -
+                        ((p.rise // 100) * delta * quantity) //
+                        20
+  end
+
+  update_price( good_to_sell )
 end
 
 -----------------------------------------------------------------
@@ -153,18 +142,21 @@ local prompt = [[
   b2 - buy  cigars  s2 - sell cigars
   b3 - buy  cloth   s3 - sell cloth
   b4 - buy  coats   s4 - sell coats
+  ba - buy  all     sa - sell all
   e to evolve one turn.
-  'reset' to reset to starting state
+  r to reset to starting state
   <enter> to repeat last command.
 
 > ]]
 
 local function reset()
-  for _, good in ipairs( goods ) do
-    prices[good] = initial_prices[good]
+  for _, good in ipairs( GOODS ) do
+    prices[good] = EQ_PRICES[good]
     volumes[good] = 0
   end
   num_turns = 0
+  gold = INITIAL_GOLD
+  last_cmd = INITIAL_CMD
 end
 
 local chart = [[
@@ -191,12 +183,6 @@ end
 local function char( str, idx ) return
     string.sub( str, idx, idx ) end
 
--- By initializing this to 'e', which means "evolve", the user
--- can just run the program and immediately start hitting <enter>
--- to start evolving the initial prices without buying or selling
--- anything.
-local last_cmd = 'e'
-
 local function loop()
   clear_screen()
   print( string.format( chart, num_turns, gold, volumes.rum,
@@ -205,7 +191,8 @@ local function loop()
                         prices.rum + 1, prices.cigars,
                         prices.cigars + 1, prices.cloth,
                         prices.cloth + 1, prices.coats,
-                        prices.coats + 1, price_sum(), last_cmd or 'none' ) )
+                        prices.coats + 1, price_sum(),
+                        last_cmd or 'none' ) )
   print()
   io.write( prompt )
 
@@ -217,19 +204,28 @@ local function loop()
     num_turns = num_turns + 1
     return
   end
-  if choice == 'reset' then
+  if choice == 'r' then
     reset()
     return
   end
   if #choice ~= 2 then return end
-  local good = goods[tonumber( char( choice, 2 ) )]
-  if good == nil then return end
+  local what = char( choice, 2 )
+  local goods_inputted = {}
+  if what == 'a' then
+    goods_inputted = GOODS
+  else
+    local good = GOODS[tonumber( what )]
+    if good == nil then return end
+    table.insert( goods_inputted, good )
+  end
   local buy_sell = char( choice, 1 )
   print()
-  if buy_sell == 'b' then
-    buy( good )
-  elseif buy_sell == 's' then
-    sell( good )
+  for _, good in ipairs( goods_inputted ) do
+    if buy_sell == 'b' then
+      buy( good )
+    elseif buy_sell == 's' then
+      sell( good )
+    end
   end
   print()
 end
