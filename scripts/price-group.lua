58362008bb984125f6df933d52c3afd40b96edf6
David P. Sicilia <dpsicilia@gmail.com>
2022-09-03 16:06:14 -0400

    Next iteration of price-group script.

--- scripts/price-group.lua
+++ scripts/price-group.lua
@@ -11,33 +11,35 @@
 |
 --]] ------------------------------------------------------------
 local GOODS = { 'rum', 'cigars', 'cloth', 'coats' }
-local STARTING_PRICES = { rum=11, cigars=10, cloth=14, coats=9 }
--- local STARTING_PRICES = { rum=12, cigars=9, cloth=14, coats=8 }
+-- local STARTING_PRICES = { rum=11, cigars=10, cloth=14, coats=9 }
+local STARTING_RATIOS = {
+  -- 12/13 9/10 14/15 8/9
+  -- rum=0x1f3,
+  -- cigars=0x277,
+  -- cloth=0x1c6,
+  -- coats=0x2b5
+
+  -- 11/12 10/11 14/15 9/10
+  rum=0x200,
+  cigars=0x21c,
+  cloth=0x19d,
+  coats=0x261
+}
+local STARTING_PRICES = { rum=12, cigars=11, cloth=15, coats=10 }
 local INITIAL_GOLD = 0
 local INITIAL_CMD = 'e'
 
 -----------------------------------------------------------------
 -- Model State
 -----------------------------------------------------------------
-local eq_prices = { rum=0.0, cigars=0.0, cloth=0.0, coats=0.0 }
-local eq_velocity = { rum=0.0, cigars=0.0, cloth=0.0, coats=0.0 }
-local prices = { rum=0, cigars=0, cloth=0, coats=0 }
+local ratios = { rum=512, cigars=512, cloth=512, coats=512 }
 local volumes = { rum=0, cigars=0, cloth=0, coats=0 }
+local prices = { rum=0, cigars=0, cloth=0, coats=0 }
 local gold = INITIAL_GOLD
 
 -----------------------------------------------------------------
 -- Model
 -----------------------------------------------------------------
-
--- Things known for sure:
---
---   1. We know that selling a good manually will bump up the eq
---      prices of the other goods during the sale, since if you
---      sell many of one good in one turn, then start selling an-
---      other in the same turn, you can see that good's price
---      start to rise for the first few sales (as opposed to
---      fall).
---
 -- TODO
 --   * The initial set of equilibrium points are given by the bi-
 --     nomial distribution, and the prices always seem to sum to
@@ -45,19 +47,17 @@ local gold = INITIAL_GOLD
 
 local RF = 4
 local VOL = 0
-local MIN = 0
-local MAX = 19
+local MIN = 1
+local MAX = 20
 -- The only place that the volatility and fall should be used is
 -- together in this manner.
 local ALPHA = (1 << VOL) / RF
-
--- Used to simulate fixed point arithmetic rounding.
-local PRECISION_BITS = 6
 
 local floor = math.floor
 local abs = math.abs
 local min = math.min
 local max = math.max
+local pow = math.pow
 
 -----------------------------------------------------------------
 -- Update Equilibrium Prices.
@@ -86,80 +86,130 @@ local function display_price( good )
   return rounded
 end
 
-local function fake_round( input )
-  return input
-  -- return floor( input * (1 << PRECISION_BITS) ) /
-  --            (1 << PRECISION_BITS)
-end
-
 local function on_all( f )
   for _, good in ipairs( GOODS ) do f( good ) end
-end
-
-local function on_all_except( skip, f )
-  for _, good in ipairs( GOODS ) do
-    if good ~= skip then f( good ) end
-  end
-end
-
-local function eq_price_sum()
-  local sum = 0
-  on_all( function( good ) sum = sum + eq_prices[good] end )
-  return sum
-end
-
-local function scale_cap( tbl, good, by )
-  local delta = tbl[good] * by - tbl[good]
-  -- TODO: see if we need these caps
-  -- delta = clamp( delta, -1, 1 )
-  tbl[good] = tbl[good] + delta
-end
-
-local function sign( x )
-  if x < 0 then return -1 end
-  return 1
 end
 
 -----------------------------------------------------------------
 -- Price Evolution
 -----------------------------------------------------------------
-local function target_price( good )
-  return floor( eq_prices[good] + .5 )
-  -- assert( good )
-  -- local eq = eq_prices[good]
-  -- local current = prices[good]
-  -- local velocity = (eq - current) / 1.0
-  -- velocity = clamp( velocity, -1, 1 )
-  -- return current + velocity
+local function eq_prices_calc()
+  --
+  -- a, b, c, d
+  --
+  -- y/x = a/b
+  -- z/x = a/c
+  -- w/x = a/d
+  --
+  -- x + y + z + w = 48
+  --
+  -- x + x*(a/b) + x*(a/c) + x*(a/d) = 48
+  --
+  -- x( 1 + a/b + a/c + a/d ) = 48
+  --
+  -- x = 48/(1 + a/b + a/c + a/d)
+  -- y = 48/(1 + a/b + a/c + a/d)*(a/b)
+  -- z = 48/(1 + a/b + a/c + a/d)*(a/c)
+  -- w = 48/(1 + a/b + a/c + a/d)*(a/d)
+  --
+  local eq_prices = {}
+
+  local a = max( 0, ratios.rum + volumes.rum )
+  local b = max( 0, ratios.cigars + volumes.cigars )
+  local c = max( 0, ratios.cloth + volumes.cloth )
+  local d = max( 0, ratios.coats + volumes.coats )
+
+  local beta = 48 / (1 + a / b + a / c + a / d)
+  eq_prices.rum = beta
+  eq_prices.cigars = beta * (a / b)
+  eq_prices.cloth = beta * (a / c)
+  eq_prices.coats = beta * (a / d)
+
+  on_all( function( good )
+    -- Test for nan.  Not sure where it comes from...
+    if eq_prices[good] ~= eq_prices[good] then
+      eq_prices[good] = 20
+    end
+    clamp_price( eq_prices, good )
+  end )
+  print( eq_prices.rum, eq_prices.cigars, eq_prices.cloth,
+         eq_prices.coats )
+  return eq_prices
 end
 
-local function update_price( good )
-  assert( good )
+local function eq_prices_walk()
+  local eq_prices = {}
+
+  local m = math.huge
+
+  on_all( function( good )
+    local p = ratios[good]
+    if volumes[good] > 0 then p = p + volumes[good] end
+    p = max( p, 0 )
+    p = 1 / p
+    m = min( m, p )
+    eq_prices[good] = p
+  end )
+
+  local INCREMENT = 1
+  local increments = {}
+  on_all( function( good )
+    increments[good] = INCREMENT * eq_prices[good] / m
+  end )
+
+  while true do
+    local sum = 0
+    on_all( function( good )
+      sum = sum + min( eq_prices[good], MAX )
+    end )
+    -- print( 'sum: ', sum )
+    -- assert( sum >= 0 )
+    if sum >= 48 then break end
+    on_all( function( good )
+      add_to_val( eq_prices, good, increments[good] )
+    end )
+  end
+
+  on_all( function( good ) clamp_price( eq_prices, good ) end )
+  return eq_prices
+end
+
+local eq_prices = eq_prices_walk
+
+local function evolve_ratios()
+  on_all( function( good )
+    local vol = volumes[good]
+    local r = ratios[good]
+    -- if vol > 0 then r = r + vol end
+    r = r + vol
+    -- Simulate the original game's apparent inability to scale
+    -- down an integer when the difference between the result and
+    -- the starting value would be less than one. This critical
+    -- point, which is ~128, is given by 1/(1-.9921875). The
+    -- number .9921875, in turn, is the closest representation of
+    -- .99 that we can have in a fixed point representation with
+    -- 8 fractional bits, which is likely what the original game
+    -- used.
+    if r >= 128 or r <= -128 then r = r * .9921875 end
+    -- if vol > 0 then r = r - vol end
+    r = r - vol
+    ratios[good] = r
+  end )
+end
+
+local function update_price( good, target )
   -- Evolve price.
-  local new_price = target_price( good )
+  local new_price = floor( target + .5 )
   if new_price < MIN then new_price = MIN end
   if new_price > MAX then new_price = MAX end
   prices[good] = new_price
-  clamp_price( prices, good )
-  -- Evolve equilibrium price.
-  local X = 1
-  eq_prices[good] = eq_prices[good] +
-                        clamp( eq_velocity[good], -X, X )
-  clamp_price( eq_prices, good )
-  -- Evolve equilibrium price velocity.
-  if eq_velocity[good] > 0 then
-    add_to_val( eq_velocity, good, -X )
-    clamp_key( eq_velocity, good, 0, 1000 )
-  elseif eq_velocity[good] < 0 then
-    add_to_val( eq_velocity, good, X )
-    clamp_key( eq_velocity, good, -1000, 0 )
-  end
 end
 
--- Evolve the goods in the way that is done when starting a new
--- turn. But note that this will not simulate interactions with
--- foreign markets because there are none in this simulation.
-local function evolve() on_all( update_price ) end
+local function evolve()
+  local eqs = eq_prices()
+  on_all( function( good ) update_price( good, eqs[good] ) end )
+  evolve_ratios()
+end
 
 -----------------------------------------------------------------
 -- Buy/Sell logic.
@@ -169,38 +219,15 @@ local function evolve() on_all( update_price ) end
 local function transaction( good, quantity, unit_price )
   gold = floor( gold + quantity * unit_price )
   volumes[good] = volumes[good] + quantity
-
-  -- We only update prices if there is a net positive volume,
-  -- meaning that more has been sold that bought, since that's
-  -- what the original game seems to do. Note that we have al-
-  -- ready changed the volume at this point, so that will allow
-  -- updating prices when the volume is at its initial value (0)
-  -- and we are selling, which the original game does.
-  if volumes[good] <= 0 then return end
-
-  ---------------------------------------------------------------
-  -- Update Equilibrium Prices.
-  ---------------------------------------------------------------
-  local Q = quantity / 100
-
-  eq_velocity[good] = eq_velocity[good] - Q
-
-  if eq_velocity[good] <= 0.00001 then
-    local delta = abs( Q ) * (9.5 - eq_prices[good]) / 6
-    eq_velocity[good] = eq_velocity[good] + delta
-    on_all_except( good, function( other )
-      add_to_val( eq_velocity, other, Q * .4333 )
-    end )
-  end
-
-  on_all( function( good ) clamp_price( eq_prices, good ) end )
+  evolve_ratios()
 
   ---------------------------------------------------------------
   -- Perturb prices.
   ---------------------------------------------------------------
-  local price_movement = (quantity / 100) * ALPHA
-  prices[good] = prices[good] - price_movement
-  clamp_price( prices, good )
+  -- local price_movement = (quantity / 100) * ALPHA
+  -- prices[good] = prices[good] - price_movement
+  -- clamp_price( prices, good )
+  prices[good] = eq_prices()[good]
 end
 
 local function buy( good, quantity )
@@ -217,7 +244,6 @@ end
 local num_turns = 0
 local num_actions = 0
 local last_cmd = INITIAL_CMD
-local last_input = nil
 
 local prompt = [[
   b1: buy rum    s1: sell rum    b2: buy cigars  s2: sell cigars
@@ -233,27 +259,24 @@ local chart = [[
   turns:   %d
   gold:    %d
   actions: %d
-  eq sum:  %d
   ----------------------------------------------------------
   |     #1      |      #2      |     #3      |     #4      |
   ----------------------------------------------------------
   |      %2d     |      %2d      |      %2d     |      %2d     | <- initial prices
   |  %6d     |  %6d      |  %6d     |  %6d     | <- net volume in europe
-  |      %.1f    |      %.1f     |      %.1f    |      %.1f    | <- eq velocities
-  |      %.1f   |      %.1f    |      %.1f   |      %.1f   | <- eq prices
+  |     %04X    |     %04X     |     %04X    |     %04X    | <- ratios
+  |     %.1f    |     %.1f     |     %.1f    |     %.1f    | <- eq prices
   ----------------------------------------------------------
   |     Rum     |    Cigars    |    Cloth    |    Coats    |
-  |    %2d/%2d    |    %2d/%2d     |    %2d/%2d    |    %2d/%2d    | <- bid prices
+  |    %2d/%2d    |    %2d/%2d     |    %2d/%2d    |    %2d/%2d    | <- prices
   ----------------------------------------------------------
-  last input: %s
-  last cmd:   %s (will be repeated by hitting enter)
+  last cmd: %s (will be repeated by hitting enter)
 ]]
 
 local function reset()
   on_all( function( good )
-    eq_prices[good] = STARTING_PRICES[good]
-    eq_velocity[good] = 0
-    prices[good] = eq_prices[good]
+    ratios[good] = STARTING_RATIOS[good]
+    prices[good] = STARTING_PRICES[good]
     volumes[good] = 0
   end )
   num_turns = 0
@@ -297,8 +320,6 @@ local function parse_cmds( str )
 end
 
 local function run_cmd( cmd )
-  if last_cmd ~= nil and #cmd == 0 then cmd = last_cmd end
-  last_cmd = cmd
   if cmd == 'e' then
     evolve()
     num_turns = num_turns + 1
@@ -330,11 +351,9 @@ local function run_cmd( cmd )
   for _, good in ipairs( goods_inputted ) do
     if buy_sell == 'b' then
       buy( good, 100 )
-      update_price( good )
       num_actions = num_actions + 1
     elseif buy_sell == 's' then
       sell( good, 100, --[[sign=]] 1, prices[good] )
-      update_price( good )
       num_actions = num_actions + 1
     end
   end
@@ -342,37 +361,35 @@ end
 
 local function looped()
   clear_screen()
+  local eqs = eq_prices()
   print( string.format( chart, num_turns, gold, num_actions,
-                        floor( eq_price_sum() ),
                         floor( STARTING_PRICES.rum ),
                         floor( STARTING_PRICES.cigars ),
                         floor( STARTING_PRICES.cloth ),
                         floor( STARTING_PRICES.coats ),
                         volumes.rum, volumes.cigars,
                         volumes.cloth, volumes.coats,
-                        eq_velocity.rum, eq_velocity.cigars,
-                        eq_velocity.cloth, eq_velocity.coats,
-                        eq_prices.rum, eq_prices.cigars,
-                        eq_prices.cloth, eq_prices.coats,
+                        floor( ratios.rum ),
+                        floor( ratios.cigars ),
+                        floor( ratios.cloth ),
+                        floor( ratios.coats ), eqs.rum,
+                        eqs.cigars, eqs.cloth, eqs.coats,
+                        display_price( 'rum' ) - 1,
                         display_price( 'rum' ),
-                        display_price( 'rum' ) + 1,
+                        display_price( 'cigars' ) - 1,
                         display_price( 'cigars' ),
-                        display_price( 'cigars' ) + 1,
+                        display_price( 'cloth' ) - 1,
                         display_price( 'cloth' ),
-                        display_price( 'cloth' ) + 1,
+                        display_price( 'coats' ) - 1,
                         display_price( 'coats' ),
-                        display_price( 'coats' ) + 1, last_input,
                         last_cmd or 'none' ) )
   io.write( prompt )
 
   local choice = io.read()
-  last_input = choice
   if choice == nil then os.exit() end
+  if #choice == 0 and last_cmd then choice = last_cmd end
+  last_cmd = choice
   local cmds = parse_cmds( choice )
-  if #cmds == 0 and last_cmd then
-    run_cmd( last_cmd )
-    print()
-  end
   for _, cmd in ipairs( cmds ) do
     local to_run
     local times = 1
