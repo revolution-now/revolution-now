6d8ab65ed1d48e62f47425f2f41ec70028aff810
David P. Sicilia <dpsicilia@gmail.com>
2022-09-05 12:19:09 -0400

    Add some comments to price-group model.

--- src/lua/prices/price-group.lua
+++ src/lua/prices/price-group.lua
@@ -47,6 +47,12 @@ local function set_price( group, good, price )
   group.prices[good] = round( price )
 end
 
+local function sum_values( tbl )
+  local res = 0
+  for _, v in pairs( tbl ) do res = res + v end
+  return res
+end
+
 -----------------------------------------------------------------
 -- Price Group Model
 -----------------------------------------------------------------
@@ -61,31 +67,93 @@ function PriceGroup:on_all( f )
   for _, good in ipairs( self.config.names ) do f( good ) end
 end
 
+-- This is the function that takes the two volumes (euro volume
+-- and traded volume) and from them derives the equilibrium
+-- prices (the prices toward which the actual player-visible
+-- prices will tend toward). The basic idea is is that the equi-
+-- librium prices are proportional to the inverses of the total
+-- volumes. The OG appears to do this:
+--
+--   1. Get the total volume for each good, ignoring negative
+--      traded volumes, as usual.
+--   2. Normalize them so that their average is one, which will
+--      allow scaling them to our desired target value.
+--   3. Take the inverse of that normalized volume, which is pro-
+--      portional to the price, then scale it up to the target
+--      value (which is fixed at 12).
+--
+-- This process ensures that a couple of things:
+--
+--   1. The price of a good will generally go down when the vol-
+--      umes go up, which makes sense because this is a basic
+--      supply/demand mechanic that one would expect. This is
+--      achieved by letting the prices be proportional to the in-
+--      verse of the volumes.
+--   2. But no matter what is bought/sold, the average price of
+--      the four goods will (approximately) remain constant; if
+--      one falls, the others will rise. This is achieved by nor-
+--      malizing the volumes and then scaling them up to the
+--      target value. This implements what was likely the design-
+--      er's goal of encouraging the production and sale of all
+--      four goods (or at least the more the better) by allowing
+--      the prices to remain stable no matter how much is sold,
+--      so long as multiple of the goods (the more, the better)
+--      are sold. If the player only sells one, its price will
+--      quickly drop and the others will rise. If the player
+--      sells all four of them in alternation, none of the prices
+--      will drop, no matter how much is sold. This way, if the
+--      player can always rely on earning a lot of gold so long
+--      as they are producing all four goods, and the prices will
+--      never drop.
+--
+-- It seems like, ideally, step 3 (taking the inverse) would be
+-- done before step 2 (normalizing), since presumably we want the
+-- average *price* of the goods to remain approximately constant
+-- and not their inverses (volumes) per se. But, that is what the
+-- original game appears to do.
+--
+-- There are some additional technicalities, such as ignoring
+-- negative values in some cases, and rounding/nan behavior, that
+-- may not have reasons per se, but they are just what the orig-
+-- inal game appears to do.
+--
+-- This function appears to reproduces the OG's numbers *exact-
+-- ly*, despite the complex behaviors of the prices, and is thus
+-- quite astonishing.
 function PriceGroup:equilibrium_prices()
   local res = {}
-  local avg_total_volume = 0
+  local total_volumes = {}
   self:on_all( function( good )
-    avg_total_volume =
-        avg_total_volume + self.euro_volumes[good] +
-            max( self.traded_volumes[good], 0 )
+    total_volumes[good] = self.euro_volumes[good] +
+                              max( self.traded_volumes[good], 0 )
   end )
-  avg_total_volume = avg_total_volume / #self.config.names
+  local avg_total_volume = sum_values( total_volumes ) /
+                               #self.config.names
   self:on_all( function( good )
-    local q = self.euro_volumes[good] +
-                  max( self.traded_volumes[good], 0 )
-    -- The original game seems to use floor here and not round,
-    -- and it makes a difference.
-    local normalized = max( q, 0 ) / avg_total_volume
-    res[good] = floor( self.config.target_price / normalized )
+    -- When all the total volumes are equal then this will be 1,
+    -- and then the below will yield the target price.
+    local normalized_volume = max( total_volumes[good], 0 ) /
+                                  avg_total_volume
+    res[good] = self.config.target_price / normalized_volume
     -- nan can happen if both the numerator and denominator in
     -- the above are both zero, which is not expected to happen
     -- in normal game play, but just in case let's handle it.
     if is_nan( res[good] ) then res[good] = math.huge end
+    -- The original game seems to use floor here and not round,
+    -- and it makes a difference.
+    res[good] = floor( res[good] )
     clamp_price( self, res, good )
   end )
   return res
 end
 
+-- This is the function that will evolve the euro volumes. It is
+-- done at the start of each turn, and also when buying selling a
+-- good in the harbor (unless the player is dutch).
+--
+-- This function appears to reproduces the OG's numbers *exact-
+-- ly*, despite the complex behaviors of the prices, and is thus
+-- quite astonishing.
 local function evolve_euro_volume( group, good )
   local r = group.euro_volumes[good]
   -- The original game basically seems to ignore the traded
