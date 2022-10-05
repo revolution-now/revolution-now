9c458e6890fe1f14b64425089c3fd8e7239ec634
David P. Sicilia <dpsicilia@gmail.com>
2022-10-05 16:09:12 -0400

    Add config/old-world and add data on tax changes and boycotts.

---
+++ src/config/old-world.cpp
@@ -0,0 +1,39 @@
+/****************************************************************
+**old-world.cpp
+*
+* Project: Revolution Now
+*
+* Created by dsicilia on 2022-10-05.
+*
+# Description: Config info for things in the Old World.
+*
+*****************************************************************/
+#include "old-world.hpp"
+
+// refl
+#include "refl/to-str.hpp"
+
+using namespace std;
+
+namespace rn {
+
+base::valid_or<string> config::old_world::IntRange::validate()
+    const {
+  REFL_VALIDATE( min <= max, "min must be <= max in range." );
+  return base::valid;
+}
+
+base::valid_or<string> config::old_world::Taxes::validate()
+    const {
+  REFL_VALIDATE( tax_increase_probability >= 0,
+                 "probabilities must be >= 0." );
+  REFL_VALIDATE( tax_increase_probability <= 1.0,
+                 "probabilities must be <= 1.0." );
+  REFL_VALIDATE( intervention_increase_probability >= 0,
+                 "probabilities must be >= 0." );
+  REFL_VALIDATE( intervention_increase_probability <= 1.0,
+                 "probabilities must be <= 1.0." );
+  return base::valid;
+}
+
+} // namespace rn
---
+++ src/config/old-world.hpp
@@ -0,0 +1,16 @@
+/****************************************************************
+**old-world.hpp
+*
+* Project: Revolution Now
+*
+* Created by dsicilia on 2022-10-05.
+*
+# Description: Config info for things in the Old World.
+*
+*****************************************************************/
+#pragma once
+
+// Rds
+#include "old-world.rds.hpp"
+
+namespace rn {} // namespace rn
