a480050594825325eaf66209a5e9217dbb91e96c
David P. Sicilia <dpsicilia@gmail.com>
2022-10-12 18:46:19 -0400

    Forgot to commit file.

---
+++ src/lua/map-gen/classic/terrain-weights-test.lua
@@ -0,0 +1,45 @@
+--[[ ------------------------------------------------------------
+|
+| terrain-weights-test.lua
+|
+| Project: Revolution Now
+|
+| Created by dsicilia on 2022-10-10.
+|
+| Description:  Unit tests for terrain ground type weights.
+|
+--]] ------------------------------------------------------------
+local dist = require( 'map-gen.classic.terrain-weights' )
+
+local U = require( 'test.unit' )
+local ASSERT_EQ = U.ASSERT_EQ
+
+local Test = U.new_test_pack()
+
+-- Map size in original game.
+local MAP_SIZE = { w=56, h=70 }
+
+local function validate_weights( weights, expected )
+  ASSERT_EQ( #weights, #expected,
+             'lengths of result and expected result' )
+  for i = 1, #weights do
+    ASSERT_EQ( weights[i].type, expected[i].type,
+               'types at index ' .. i )
+    ASSERT_EQ( weights[i].value, expected[i].value,
+               'values at index ' .. i )
+  end
+end
+
+function Test.test_dry_weights()
+  -- ASSERT_EQ( 3, 4, '3 and 4' ) --
+end
+
+function Test.test_wet_weights()
+  -- ASSERT_EQ( 3, 4, '3 and 4' ) --
+end
+
+function Test.test_select_from_weights()
+  -- ASSERT_EQ( 3, 4, '3 and 4' ) --
+end
+
+return Test
