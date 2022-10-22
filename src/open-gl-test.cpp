f224b2e50d1e9a6fea8f1c20d999f0ea8fe8efa8
David P. Sicilia <dpsicilia@gmail.com>
2022-10-22 11:55:54 -0400

    Update open-gl-test.

--- src/open-gl-test.cpp
+++ src/open-gl-test.cpp
@@ -23,13 +23,35 @@ namespace {
 void paint_things( rr::Renderer& renderer ) {
   renderer.clear_screen(
       pixel{ .r = 50, .g = 75, .b = 75, .a = 255 } );
+  gfx::rect r;
 
-  rr::Painter painter = renderer.painter();
+  float grad_mag = 1.0 / 100.0;
 
-  painter.draw_solid_rect(
-      rect{ .origin = { .x = 50, .y = 50 },
-            .size   = { .w = 100, .h = 100 } },
-      pixel{ .r = 255, .g = 0, .b = 0, .a = 255 } );
+  pixel const kColor = { .r = 255, .g = 0, .b = 0, .a = 255 };
+
+  int x = 50;
+
+  vector<pair<dsize, double>> directions{
+      { dsize{ .h = -grad_mag }, 1.0 },
+      { dsize{ .w = grad_mag }, 0.0 },
+      { dsize{ .h = grad_mag }, 0.0 },
+      { dsize{ .w = -grad_mag }, 1.0 },
+      { dsize{ .w = -grad_mag, .h = -grad_mag }, 1.0 },
+  };
+
+  for( auto [d, base_stage] : directions ) {
+    SCOPED_RENDERER_MOD_SET( painter_mods.depixelate.stage,
+                             base_stage );
+    SCOPED_RENDERER_MOD_SET(
+        painter_mods.depixelate.stage_gradient, d );
+    r = rect{ .origin = { .x = x, .y = 50 },
+              .size   = { .w = 100, .h = 100 } };
+    SCOPED_RENDERER_MOD_SET(
+        painter_mods.depixelate.stage_anchor,
+        r.origin.to_double() );
+    renderer.painter().draw_solid_rect( r, kColor );
+    x += 110;
+  }
 }
 
 } // namespace
