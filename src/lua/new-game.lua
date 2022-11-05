5e5a2e586a96cd1279146d4800dd29fd9dec0a39
David P. Sicilia <dpsicilia@gmail.com>
2022-11-05 14:01:02 -0400

    Switch to spanish as default nation.

--- src/lua/new-game.lua
+++ src/lua/new-game.lua
@@ -313,8 +313,8 @@ end
 local function add_testing_options( options )
   options.nations = {
     -- english={ human=true, ship_pos=nil },
-    french={ human=true, ship_pos=nil }
-    -- spanish={ human=true, ship_pos=nil },
+    -- french={ human=true, ship_pos=nil },
+    spanish={ human=true, ship_pos=nil }
     -- dutch={ human=true, ship_pos=nil },
   }
   options.difficulty = 'conquistador'
