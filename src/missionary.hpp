88931782501769c79a9f815604d79de3be17e1af
David P. Sicilia <dpsicilia@gmail.com>
2023-01-18 09:20:11 -0500

    Add comment on native convert expiration time.

--- src/missionary.hpp
+++ src/missionary.hpp
@@ -21,6 +21,30 @@ struct Colony;
 struct Player;
 struct Unit;
 
+/****************************************************************
+** Note on convert expiration.
+*****************************************************************/
+// Note: In the OG, if a native convert is not put to work within
+// eight turns then the unit disappears. However, in this game we
+// don't replicate that, since it seems that it was originally
+// added to incentivize the player to use the native convert be-
+// fore it was made stronger at outdoor jobs than free colonists.
+// From the strategy guide:
+//
+//   "The eight-tum rule is a vestige ef an earlier version ef
+//   the game in which Native Converts were not supen'or to Free
+//   Colonists in wry capacity but retained the disadvantages
+//   they possess now. In that version, it was sometimes diffi-
+//   cult to find a use for Native Converts because Free
+//   Colonists and other colonist types were usually equal or su-
+//   pen·or to them. The bonusfor Native Convert production was a
+//   late addition to the game, which now makes them attractive
+//   colonists. It also increases the value efcreating Mission-
+//   aries and establishing missions."
+
+/****************************************************************
+** Public API
+*****************************************************************/
 // This will determine if a colony can bless a missionary; in the
 // original game this requires either a church or cathedral. Note
 // that any human unit can be blessed as a missionary.
