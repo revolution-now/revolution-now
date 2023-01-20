5144f2e5da365a42dec2aae78ff7abea777def62
David P. Sicilia <dpsicilia@gmail.com>
2023-01-20 12:57:51 -0500

    Update IDE contents.

--- scripts/ide/contents/rn.lua
+++ scripts/ide/contents/rn.lua
@@ -1,22 +1,16 @@
 -- List of modules to open when we edit RN.
 
 return {
-  'exe/main',
   'ss/native-enums',
   'config/natives',
   'ss/natives',
   'ss/tribe',
   'ss/dwelling',
-  'ss/native-unit',
-  'unit-mgr',
   'tribe-mgr',
   'alarm',
   'orders-move',
   'enter-dwelling',
-  'treasure',
   'fight',
-  'config/missionary',
-  'missionary',
   'land-view',
   'land-view-render',
   'land-view-anim',
