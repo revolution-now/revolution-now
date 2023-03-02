53f6139911c7a29dad5a0c25f6f5612d6607d7f9
David P. Sicilia <dpsicilia@gmail.com>
2023-03-02 08:54:03 -0500

    Add the roles module.

---
+++ src/roles.cpp
@@ -0,0 +1,57 @@
+/****************************************************************
+**roles.cpp
+*
+* Project: Revolution Now
+*
+* Created by dsicilia on 2023-03-01.
+*
+* Description: Manages the different roles that each player can
+*              have with regard to driving the game and map
+*              visibility.
+*
+*****************************************************************/
+#include "roles.hpp"
+
+// ss
+#include "ss/land-view.rds.hpp"
+#include "ss/players.rds.hpp"
+#include "ss/ref.hpp"
+#include "ss/turn.rds.hpp"
+
+// rds
+#include "rds/switch-macro.hpp"
+
+using namespace std;
+
+namespace rn {
+
+/****************************************************************
+** Public API
+*****************************************************************/
+maybe<e_nation> player_for_role( SSConst const& ss,
+                                 e_player_role  role ) {
+  switch( role ) {
+    case e_player_role::viewer: {
+      SWITCH( ss.land_view.map_revealed ) {
+        CASE( no_special_view ) {
+          if( maybe<e_nation> const human =
+                  player_for_role( ss, e_player_role::human );
+              human.has_value() )
+            return human;
+          return player_for_role( ss, e_player_role::active );
+        }
+        CASE( entire ) { //
+          return nothing;
+        }
+        CASE( nation ) { return o.nation; }
+        END_CASES;
+      }
+    }
+    case e_player_role::human:
+      return ss.players.human;
+    case e_player_role::active:
+      return ss.turn.nation.member( &NationTurnState::nation );
+  }
+}
+
+} // namespace rn
---
+++ test/roles.cpp
@@ -0,0 +1,127 @@
+/****************************************************************
+**roles.cpp
+*
+* Project: Revolution Now
+*
+* Created by dsicilia on 2023-03-01.
+*
+* Description: Unit tests for the src/roles.* module.
+*
+*****************************************************************/
+#include "test/testing.hpp"
+
+// Under test.
+#include "src/roles.hpp"
+
+// Testing
+#include "test/fake/world.hpp"
+
+// ss
+#include "src/ss/land-view.rds.hpp"
+#include "src/ss/players.rds.hpp"
+#include "src/ss/ref.hpp"
+#include "src/ss/turn.rds.hpp"
+
+// refl
+#include "refl/to-str.hpp"
+
+// Must be last.
+#include "test/catch-common.hpp"
+
+namespace rn {
+namespace {
+
+using namespace std;
+
+/****************************************************************
+** Fake World Setup
+*****************************************************************/
+struct World : testing::World {
+  using Base = testing::World;
+  World() : Base() {
+    // !! Do not create players here.
+  }
+};
+
+/****************************************************************
+** Test Cases
+*****************************************************************/
+TEST_CASE( "[roles] player_for_role" ) {
+  World W;
+
+  auto f = [&]( e_player_role role ) {
+    return player_for_role( W.ss(), role );
+  };
+
+  W.land_view().map_revealed = MapRevealed::no_special_view{};
+
+  REQUIRE( f( e_player_role::viewer ) == nothing );
+  REQUIRE( f( e_player_role::human ) == nothing );
+  REQUIRE( f( e_player_role::active ) == nothing );
+
+  // Add french player.
+  W.add_player( e_nation::french );
+  REQUIRE( f( e_player_role::viewer ) == nothing );
+  REQUIRE( f( e_player_role::human ) == nothing );
+  REQUIRE( f( e_player_role::active ) == nothing );
+
+  // Add dutch player.
+  W.add_player( e_nation::dutch );
+  REQUIRE( f( e_player_role::viewer ) == nothing );
+  REQUIRE( f( e_player_role::human ) == nothing );
+  REQUIRE( f( e_player_role::active ) == nothing );
+
+  // Make dutch human.
+  W.players().human = e_nation::dutch;
+  REQUIRE( f( e_player_role::viewer ) == e_nation::dutch );
+  REQUIRE( f( e_player_role::human ) == e_nation::dutch );
+  REQUIRE( f( e_player_role::active ) == nothing );
+
+  // Add spanish player.
+  W.add_player( e_nation::spanish );
+  REQUIRE( f( e_player_role::viewer ) == e_nation::dutch );
+  REQUIRE( f( e_player_role::human ) == e_nation::dutch );
+  REQUIRE( f( e_player_role::active ) == nothing );
+
+  // Make spanish active.
+  W.turn().started = true;
+  W.turn().nation =
+      NationTurnState{ .nation = e_nation::spanish };
+  REQUIRE( f( e_player_role::viewer ) == e_nation::dutch );
+  REQUIRE( f( e_player_role::human ) == e_nation::dutch );
+  REQUIRE( f( e_player_role::active ) == e_nation::spanish );
+
+  // Remove human status.
+  W.players().human = nothing;
+  REQUIRE( f( e_player_role::viewer ) == e_nation::spanish );
+  REQUIRE( f( e_player_role::human ) == nothing );
+  REQUIRE( f( e_player_role::active ) == e_nation::spanish );
+
+  // Add english player.
+  W.add_player( e_nation::english );
+  REQUIRE( f( e_player_role::viewer ) == e_nation::spanish );
+  REQUIRE( f( e_player_role::human ) == nothing );
+  REQUIRE( f( e_player_role::active ) == e_nation::spanish );
+
+  // Switch to entire-map-view.
+  W.land_view().map_revealed = MapRevealed::entire{};
+  REQUIRE( f( e_player_role::viewer ) == nothing );
+  REQUIRE( f( e_player_role::human ) == nothing );
+  REQUIRE( f( e_player_role::active ) == e_nation::spanish );
+
+  // Switch to english nation-map-view.
+  W.land_view().map_revealed =
+      MapRevealed::nation{ .nation = e_nation::english };
+  REQUIRE( f( e_player_role::viewer ) == e_nation::english );
+  REQUIRE( f( e_player_role::human ) == nothing );
+  REQUIRE( f( e_player_role::active ) == e_nation::spanish );
+
+  // Make french human.
+  W.players().human = e_nation::french;
+  REQUIRE( f( e_player_role::viewer ) == e_nation::english );
+  REQUIRE( f( e_player_role::human ) == e_nation::french );
+  REQUIRE( f( e_player_role::active ) == e_nation::spanish );
+}
+
+} // namespace
+} // namespace rn
