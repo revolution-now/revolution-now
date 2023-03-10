bfab248b0c63d8efccac8663e32c73758513e2b3
David P. Sicilia <dpsicilia@gmail.com>
2023-03-10 17:43:46 -0500

    Remove old unused lua function.

--- src/lua/global.lua
+++ src/lua/global.lua
@@ -14,17 +14,4 @@ local M = {}
 -- Global function
 function grid() terrain.toggle_grid() end
 
--- Take the currently blinking unit and turn it into a soldier.
-function soldier()
-  local blinker = assert( land_view.blinking_unit(),
-                          'There is currently no unit asking ' ..
-                              'for orders.' )
-  local unit = unit_mgr.unit_from_id( blinker )
-  assert( not unit:desc().ship,
-          'Cannot convert a naval unit to a soldier.' )
-  unit:change_type( unit_composer.UnitComposition
-                        .create_with_type_obj(
-                        unit_type.UnitType.create( 'soldier' ) ) )
-end
-
 return M
