57ed966b6f1176e1995889ac65e68fbbbab12882
David P. Sicilia <dpsicilia@gmail.com>
2023-03-10 19:47:17 -0500

    Add some LHS wrappers to the no-default module.

--- src/base/no-default.hpp
+++ src/base/no-default.hpp
@@ -11,8 +11,14 @@
 *****************************************************************/
 #pragma once
 
+// C++ standard library
+#include <utility>
+
 namespace base {
 
+/****************************************************************
+** no_default
+*****************************************************************/
 // This is used to allow forcing the initialization of aggregate
 // fields during aggregate initialization. The compiler does not
 // enforce that they be initialized, and sometimes we don't want
@@ -47,9 +53,10 @@ namespace base {
 //
 // Another way of achieving the same thing would be to wrap the
 // field types in a wrapper that has no default constructor, but
-// wrappers introduce other problems and inconveniences. This ap-
-// proach is clean and wrapper-free, at the expense that the er-
-// rors come at link time.
+// wrappers introduce other problems and inconveniences (that
+// said, if you need one, then see below). This approach is clean
+// and wrapper-free, at the expense that the errors come at link
+// time.
 //
 namespace detail {
 
@@ -74,5 +81,109 @@ struct no_default_t<void> {
 // value. For non-const references, specify it explicitly.
 template<typename T = void>
 inline constexpr detail::no_default_t<T> no_default;
+
+/****************************************************************
+** no_default_wrapper
+*****************************************************************/
+// This is used to allow forcing the initialization of an aggre-
+// gate field during aggregate initialization when that field is
+// initialized via default construction in the struct definition:
+//
+//   struct Foo {
+//     int bar = {};
+//     T baz   = {};
+//   };
+//
+// Assuming that the type of baz is default constructible (which
+// it has to be otherwise the above wouldn't compile) then com-
+// piler won't force the initialization of baz. One solution is
+// to use no_default above; however, that is a "right hand side"
+// solution, which in some cases we don't have the ability to do.
+// This is a "left hand side" solution where we wrap the type in
+// a wrapper that has no default constructor.
+//
+//   struct Foo {
+//     int bar = {};
+//     base::no_default_wrapper<T> baz;
+//   };
+//
+// But note that we had to remove the "= {}" from the rhs. Then,
+// if someone only partially initializes the fields:
+//
+//   Foo foo{ .bar = 4 };
+//
+// Then we will get a compile-time error.
+//
+template<typename T>
+requires std::equality_comparable<T>
+struct no_default_wrapper {
+  template<typename U>
+  requires std::is_constructible_v<T, U&&>
+  no_default_wrapper( U&& o ) : val_( std::forward<U>( o ) ) {}
+
+  T&       get() { return val_; }
+  T const& get() const { return val_; }
+
+  no_default_wrapper() = delete;
+
+  bool operator==( no_default_wrapper const& ) const = default;
+
+ private:
+  T val_;
+};
+
+/****************************************************************
+** no_default_linker_wrapper
+*****************************************************************/
+// This is used when we want a left hand side solution like the
+// one above but where we are unable to remove the "= {}" from
+// the right hand side. In that case, we need a wrapper that does
+// have a default constructor, but will trigger a linker error if
+// it is actually called:
+//
+//   struct Foo {
+//     int bar = {};
+//     T baz   = {};
+//   };
+//
+// becomes:
+//
+//   struct Foo {
+//     int bar                                = {};
+//     base::no_default_linker_wrapper<T> baz = {};
+//   };
+//
+// where, again, notice that we have kept the "= {}". Then, if
+// someone only partially initializes the fields:
+//
+//   Foo foo{ .bar = 4 };
+//
+// Then we will get a linker error.
+//
+template<typename T>
+requires std::equality_comparable<T>
+struct no_default_linker_wrapper {
+  template<typename U>
+  requires std::is_constructible_v<T, U&&>
+  no_default_linker_wrapper( U&& o )
+    : val_( std::forward<U>( o ) ) {}
+
+  T&       get() { return val_; }
+  T const& get() const { return val_; }
+
+  struct Dummy {};
+  static Dummy
+  you_forgot_to_initialize_a_required_aggregate_field();
+  no_default_linker_wrapper(
+      Dummy =
+          you_forgot_to_initialize_a_required_aggregate_field() ) {
+  }
+
+  bool operator==( no_default_linker_wrapper const& ) const =
+      default;
+
+ private:
+  T val_;
+};
 
 } // namespace base
