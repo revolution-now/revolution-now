cc3e1ae0bc772d08a5fc0ffa81c91b26dc98b72a
David P. Sicilia <dpsicilia@gmail.com>
2023-03-12 13:14:39 -0400

    Add mock IMapUpdater.

---
+++ test/mocks/imap-updater.hpp
@@ -0,0 +1,40 @@
+/****************************************************************
+**imap-updater.hpp
+*
+* Project: Revolution Now
+*
+* Created by dsicilia on 2023-03-11.
+*
+* Description: For dependency injection in unit tests.
+*
+*****************************************************************/
+#pragma once
+
+// Revolution Now
+#include "src/imap-updater.hpp"
+
+// mock
+#include "src/mock/mock.hpp"
+
+// refl
+#include "refl/to-str.hpp"
+
+namespace rn {
+
+/****************************************************************
+** MockIMapUpdater
+*****************************************************************/
+struct MockIMapUpdater : IMapUpdater {
+  MOCK_METHOD( bool, modify_map_square,
+               ( Coord, SquareUpdateFunc ), () );
+  MOCK_METHOD( void, modify_entire_map, ( MapUpdateFunc ), () );
+  MOCK_METHOD( bool, make_square_visible, ( Coord, e_nation ),
+               () );
+  MOCK_METHOD( bool, make_square_fogged, ( Coord, e_nation ),
+               () );
+  MOCK_METHOD( void, redraw, (), () );
+};
+
+static_assert( !std::is_abstract_v<MockIMapUpdater> );
+
+} // namespace rn
