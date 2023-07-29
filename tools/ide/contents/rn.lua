e95ea54414644dc18de9d6d0b8fb15910e0b124e
David P. Sicilia <dpsicilia@gmail.com>
2023-07-29 18:38:52 -0400

    Update IDE contents.

--- tools/ide/contents/rn.lua
+++ tools/ide/contents/rn.lua
@@ -4,15 +4,16 @@ return {
   'config/natives', -- 2
   'turn', -- 3
   'native-turn', -- 4
-  'icombat', -- 5
-  'combat', -- 6
-  'attack-handlers', -- 7
-  'combat-outcome', -- 8
-  'inative-mind', -- 9
-  'ai-native-mind', -- 10
-  'ieuro-mind', -- 11
-  'human-euro-mind', -- 12
+  'anim-builders', -- 5
+  'raid', -- 6
+  'icombat', -- 7
+  'combat', -- 8
+  'attack-handlers', -- 9
+  'inative-mind', -- 10
+  'ai-native-mind', -- 11
+  'ieuro-mind', -- 12
+  'human-euro-mind', -- 13
 
   -- 1-based.
-  current_tab=8, -- combat-outcome
+  current_tab=9, -- attack-handlers
 }
