69ef4b731dc0a7b70ed2605db140de54d2111b8a
David P. Sicilia <dpsicilia@gmail.com>
2023-09-17 09:46:56 -0400

    Update IDE contents.

--- tools/ide/contents/rn.lua
+++ tools/ide/contents/rn.lua
@@ -4,18 +4,19 @@ return {
   'config/natives', -- 2
   'turn', -- 3
   'native-turn', -- 4
-  'attack-handlers', -- 5
-  'raid', -- 6
-  'raid-effects', -- 7
-  'combat-effects', -- 8
-  'icombat', -- 9
-  'combat', -- 10
-  'imind', -- 11
-  'inative-mind', -- 12
-  'ai-native-mind', -- 13
-  'ieuro-mind', -- 14
-  'human-euro-mind', -- 15
+  'land-view-anim', -- 5
+  'map-search', -- 6
+  'raid', -- 7
+  'visibility', -- 8
+  'game', -- 9
+  'icombat', -- 10
+  'combat', -- 11
+  'imind', -- 12
+  'inative-mind', -- 13
+  'ai-native-mind', -- 14
+  'ieuro-mind', -- 15
+  'human-euro-mind', -- 16
 
   -- 1-based.
-  current_tab=5, -- attack-handlers
+  current_tab=6, -- map-search
 }
