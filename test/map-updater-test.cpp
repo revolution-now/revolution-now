eb7d8934e5a68402b89a3283c37fd28616e87529
David P. Sicilia <dpsicilia@gmail.com>
2023-10-07 11:47:44 -0400

    Add a dedicated test case for NonRenderingMapUpdater::make_squares_fogged.

--- test/map-updater-test.cpp
+++ test/map-updater-test.cpp
@@ -56,6 +56,102 @@ struct World : testing::World {
 /****************************************************************
 ** Test Cases
 *****************************************************************/
+TEST_CASE(
+    "[map-updater] "
+    "NonRenderingMapUpdater::make_squares_fogged" ) {
+  World                  W;
+  NonRenderingMapUpdater map_updater( W.ss() );
+  vector<BuffersUpdated> expected_buffers;
+  e_nation const         nation = W.default_nation();
+  PlayerTerrain&         player_terrain =
+      W.ss()
+          .mutable_terrain_use_with_care.mutable_player_terrain(
+              nation );
+  Coord const tile = { .x = 1, .y = 1 };
+
+  MapSquare&       real_square = W.square( tile );
+  maybe<FogSquare> expected_fog_square;
+
+  auto f = [&] {
+    return map_updater.make_squares_fogged( nation, { tile } );
+  };
+
+  auto fog_square = [&]() -> maybe<FogSquare>& {
+    return player_terrain.map[tile];
+  };
+
+  // Initially totally not visible.
+  REQUIRE( !fog_square().has_value() );
+
+  // On hidden.
+  expected_buffers    = { { .tile        = tile,
+                            .landscape   = false,
+                            .obfuscation = false } };
+  expected_fog_square = nothing;
+  REQUIRE( f() == expected_buffers );
+  REQUIRE( fog_square() == expected_fog_square );
+
+  // Now make it visible.
+  fog_square()        = FogSquare{ .square             = real_square,
+                                   .fog_of_war_removed = true };
+  expected_buffers    = { { .tile        = tile,
+                            .landscape   = false,
+                            .obfuscation = true } };
+  expected_fog_square = FogSquare{ .square = real_square,
+                                   .fog_of_war_removed = false };
+  REQUIRE( f() == expected_buffers );
+  REQUIRE( fog_square() == expected_fog_square );
+
+  // Now change the real square and verify no update.
+  BASE_CHECK( !real_square.road );
+  real_square.road    = true;
+  expected_buffers    = { { .tile        = tile,
+                            .landscape   = false,
+                            .obfuscation = false } };
+  expected_fog_square = FogSquare{ .square = real_square,
+                                   .fog_of_war_removed = false };
+  expected_fog_square->square.road = false;
+  REQUIRE( f() == expected_buffers );
+  REQUIRE( fog_square() == expected_fog_square );
+
+  // Remove fog and try again.
+  fog_square()->fog_of_war_removed = true;
+  // landscape is false here because the make_squares_fogged
+  // method assumes that, if the fog was removed, then the tile
+  // has already been rendered in its actual state.
+  expected_buffers = { { .tile        = tile,
+                         .landscape   = false,
+                         .obfuscation = true } };
+  BASE_CHECK( real_square.road );
+  expected_fog_square = FogSquare{ .square = real_square,
+                                   .fog_of_war_removed = false };
+  REQUIRE( f() == expected_buffers );
+  REQUIRE( fog_square() == expected_fog_square );
+
+  // Try again but with fog rendering disabled.
+  {
+    auto _ = map_updater.push_options_and_redraw(
+        []( auto& options ) {
+          options.render_fog_of_war = false;
+        } );
+    fog_square()->fog_of_war_removed = true;
+
+    expected_buffers = { { .tile      = tile,
+                           .landscape = false,
+                           // The difference is here.
+                           .obfuscation = false } };
+
+    expected_fog_square = FogSquare{
+        .square = real_square, .fog_of_war_removed = false };
+    BASE_CHECK( real_square.road );
+    REQUIRE( f() == expected_buffers );
+    REQUIRE( fog_square() == expected_fog_square );
+  }
+}
+
+// This test case will do some additional (possibly redundant)
+// testing on the transitioning of tiles to and from various
+// states of visible, hidden, and fog.
 TEST_CASE( "[map-updater] fog of war" ) {
   World                  W;
   NonRenderingMapUpdater map_updater( W.ss() );
@@ -120,12 +216,13 @@ TEST_CASE( "[map-updater] fog of war" ) {
   }
 
   {
-    expected                   = { { .tile        = coord1,
-                                     .landscape   = true,
-                                     .obfuscation = true },
-                                   { .tile        = coord2,
-                                     .landscape   = false,
-                                     .obfuscation = true } };
+    expected = { { .tile        = coord1,
+                   .landscape   = true,
+                   .obfuscation = true },
+                 { .tile        = coord2,
+                   .landscape   = false,
+                   .obfuscation = true } };
+
     player_terrain.map[coord1] = nothing;
     REQUIRE( map_updater.make_squares_visible(
                  nation, { coord1, coord2 } ) == expected );
