a9586c71e727e5514860f432f51eee0e28aa7fbb
David P. Sicilia <dpsicilia@gmail.com>
2023-10-08 14:47:05 -0400

    When disbanding a ship on a land square containing units, offboard them first to simulate OG's behavior.

--- src/command-disband.cpp
+++ src/command-disband.cpp
@@ -12,7 +12,9 @@
 
 // Revolution Now
 #include "co-wait.hpp"
+#include "map-square.hpp"
 #include "ts.hpp"
+#include "unit-mgr.hpp"
 #include "unit-ownership.hpp"
 
 // config
@@ -20,6 +22,7 @@
 
 // ss
 #include "ss/ref.hpp"
+#include "ss/terrain.hpp"
 #include "ss/units.hpp"
 
 // Rds
@@ -31,6 +34,43 @@ namespace rn {
 
 namespace {
 
+// NOTE regarding disbanding ships at sea carrying units. As
+// background, the OG does not allow disbanding ships at sea con-
+// taining units, and this is very likely in order to avoid an
+// ambiguity as to which units need to be destroyed as a result.
+// Specifically, in the OG, units cannot technically be on ships;
+// instead, units onboard ships at sea are still on the map but
+// are "dragged around" by the ship as it moves in order to simu-
+// late being onboard. That poses a problem when there are mul-
+// tiple ships containing units on the same sea square: the game
+// would not know in general which units were on which ships and
+// hence would not know which units to destroy. In fact, the OG
+// has a known "bug" where two ships on the same tile that both
+// have units onboard can exchange units depending on which ship
+// is the first to move off of the square. All of that said, even
+// in the OG this restriction can be worked around by just
+// clicking on the ship-at-sea, activating the units on it, and
+// disbanding them prior to disbanding the ship, with the same
+// effect. Technically, in the OG, this can't be done if one of
+// the units on the ship has already moved in the turn (which
+// will prevent it from being activated), but that is an arcane
+// edge case that is probably not worth replicating. Thus, since
+// the NG does have proper support for units on ships, and since
+// even the OG has a workaround, we just always allow it since it
+// shouldn't have any net effect on gameplay.
+//
+// There is one more wrinkle though: first note that this re-
+// striction of the OG does not apply to ships in a colony port,
+// because in that case the units don't have to be destroyed when
+// disbanding the ship, since they are at the colony gate, thus
+// there is no ambiguity. In order to replicate this behavior, we
+// will automatically offboard any units in the cargo of ships in
+// a colony port (or on a land square) before disbanding the
+// ship, even though we technically don't have to.
+
+/****************************************************************
+** DisbandHandler
+*****************************************************************/
 struct DisbandHandler : public CommandHandler {
   DisbandHandler( SS& ss, TS& ts, UnitId unit_id )
     : ss_( ss ), ts_( ts ), unit_id_( unit_id ) {}
@@ -46,7 +86,21 @@ struct DisbandHandler : public CommandHandler {
     co_return answer == ui::e_confirm::yes;
   }
 
+  void offload_if_needed() const {
+    Unit&       unit = ss_.units.unit_for( unit_id_ );
+    Coord const tile =
+        coord_for_unit_indirect_or_die( ss_.units, unit.id() );
+    bool const is_on_land =
+        is_land( ss_.terrain.square_at( tile ) );
+    bool const contains_units = !unit.cargo().units().empty();
+
+    if( unit.desc().ship && is_on_land && contains_units )
+      // This is for compatibility with the OG.
+      offboard_units_on_ship( ss_, ts_, unit );
+  }
+
   wait<> perform() override {
+    offload_if_needed();
     UnitOwnershipChanger( ss_, unit_id_ ).destroy();
     co_return;
   }
---
+++ test/command-disband-test.cpp
@@ -0,0 +1,271 @@
+/****************************************************************
+**command-disband-test.cpp
+*
+* Project: Revolution Now
+*
+* Created by David P. Sicilia on 2023-10-08.
+*
+* Description: Unit tests for the command-disband module.
+*
+*****************************************************************/
+#include "test/testing.hpp"
+
+// Under test.
+#include "src/command-disband.hpp"
+
+// Testing.
+#include "test/fake/world.hpp"
+#include "test/mocking.hpp"
+#include "test/mocks/igui.hpp"
+#include "test/util/coro.hpp"
+
+// ss
+#include "src/ss/unit-composer.hpp"
+#include "src/ss/unit.hpp"
+#include "src/ss/units.hpp"
+
+// Must be last.
+#include "test/catch-common.hpp" // IWYU pragma: keep
+
+namespace rn {
+namespace {
+
+using namespace std;
+
+using ::mock::matchers::_;
+
+/****************************************************************
+** Fake World Setup
+*****************************************************************/
+struct World : testing::World {
+  using Base = testing::World;
+  World() : Base() {
+    add_default_player();
+    create_default_map();
+  }
+
+  void create_default_map() {
+    MapSquare const   _ = make_ocean();
+    MapSquare const   L = make_grassland();
+    vector<MapSquare> tiles{
+        _, L, _, //
+        L, L, L, //
+        _, L, L, //
+    };
+    build_map( std::move( tiles ), 3 );
+  }
+};
+
+/****************************************************************
+** Test Cases
+*****************************************************************/
+TEST_CASE( "[command-disband] confirm+perform" ) {
+  World W;
+
+  UnitId unit_id = {};
+
+  auto confirm = [&] {
+    auto handler =
+        handle_command( W.ss(), W.ts(), W.default_player(),
+                        unit_id, command::disband{} );
+    return co_await_test( handler->confirm() );
+  };
+
+  auto perform = [&] {
+    auto handler =
+        handle_command( W.ss(), W.ts(), W.default_player(),
+                        unit_id, command::disband{} );
+    return co_await_test( handler->perform() );
+  };
+
+  auto require_on_land = [&]( Unit const& unit ) {
+    REQUIRE( as_const( W.units() )
+                 .ownership_of( unit.id() )
+                 .holds<UnitOwnership::world>() );
+  };
+
+  auto require_sentried = [&]( Unit const& unit ) {
+    REQUIRE( unit.orders().holds<unit_orders::sentry>() );
+  };
+
+  auto require_not_sentried = [&]( Unit const& unit ) {
+    REQUIRE( !unit.orders().holds<unit_orders::sentry>() );
+  };
+
+  SECTION( "cancelled" ) {
+    UnitId const ship_id =
+        W.add_unit_on_map( e_unit_type::galleon,
+                           { .x = 0, .y = 0 } )
+            .id();
+    unit_id = ship_id;
+    W.gui().EXPECT__choice( _, _ ).returns<maybe<string>>(
+        nothing );
+    REQUIRE( confirm() == false );
+    REQUIRE( W.units().exists( ship_id ) );
+  }
+
+  SECTION( "selected no" ) {
+    UnitId const ship_id =
+        W.add_unit_on_map( e_unit_type::galleon,
+                           { .x = 0, .y = 0 } )
+            .id();
+    unit_id = ship_id;
+    W.gui().EXPECT__choice( _, _ ).returns<maybe<string>>(
+        "no" );
+    REQUIRE( confirm() == false );
+    REQUIRE( W.units().exists( ship_id ) );
+  }
+
+  SECTION( "selected yes" ) {
+    UnitId const ship_id =
+        W.add_unit_on_map( e_unit_type::galleon,
+                           { .x = 0, .y = 0 } )
+            .id();
+    unit_id = ship_id;
+    W.gui().EXPECT__choice( _, _ ).returns<maybe<string>>(
+        "yes" );
+    REQUIRE( confirm() == true );
+    perform();
+    REQUIRE_FALSE( W.units().exists( ship_id ) );
+  }
+
+  SECTION( "selected yes / land" ) {
+    UnitId const ship_id =
+        W.add_unit_on_map( e_unit_type::galleon,
+                           { .x = 0, .y = 0 } )
+            .id();
+    UnitId const free_colonist_id =
+        W.add_unit_on_map( e_unit_type::free_colonist,
+                           { .x = 1, .y = 1 } )
+            .id();
+    unit_id = free_colonist_id;
+    W.gui().EXPECT__choice( _, _ ).returns<maybe<string>>(
+        "yes" );
+    REQUIRE( confirm() == true );
+    perform();
+    REQUIRE( W.units().exists( ship_id ) );
+    REQUIRE_FALSE( W.units().exists( free_colonist_id ) );
+  }
+
+  SECTION( "unit on ship" ) {
+    UnitId const ship_id =
+        W.add_unit_on_map( e_unit_type::galleon,
+                           { .x = 0, .y = 0 } )
+            .id();
+    UnitId const cargo_unit_id =
+        W.add_unit_in_cargo( e_unit_type::free_colonist,
+                             ship_id )
+            .id();
+    unit_id = cargo_unit_id;
+    W.gui().EXPECT__choice( _, _ ).returns<maybe<string>>(
+        "yes" );
+    REQUIRE( confirm() == true );
+    perform();
+    REQUIRE( W.units().exists( ship_id ) );
+    REQUIRE_FALSE( W.units().exists( cargo_unit_id ) );
+    require_not_sentried( W.units().unit_for( ship_id ) );
+  }
+
+  SECTION( "ship with units on ocean" ) {
+    UnitId const ship_id =
+        W.add_unit_on_map( e_unit_type::galleon,
+                           { .x = 0, .y = 0 } )
+            .id();
+    UnitId const cargo_unit_id =
+        W.add_unit_in_cargo( e_unit_type::free_colonist,
+                             ship_id )
+            .id();
+    unit_id = ship_id;
+    W.gui().EXPECT__choice( _, _ ).returns<maybe<string>>(
+        "yes" );
+    REQUIRE( confirm() == true );
+    perform();
+    REQUIRE_FALSE( W.units().exists( ship_id ) );
+    REQUIRE_FALSE( W.units().exists( cargo_unit_id ) );
+  }
+
+  SECTION( "ship with units on land" ) {
+    UnitId const ship_id =
+        W.add_unit_on_map( e_unit_type::galleon,
+                           { .x = 1, .y = 1 } )
+            .id();
+    UnitId const cargo_unit_id =
+        W.add_unit_in_cargo( e_unit_type::free_colonist,
+                             ship_id )
+            .id();
+    unit_id = ship_id;
+    W.gui().EXPECT__choice( _, _ ).returns<maybe<string>>(
+        "yes" );
+    REQUIRE( confirm() == true );
+    perform();
+    REQUIRE_FALSE( W.units().exists( ship_id ) );
+    REQUIRE( W.units().exists( cargo_unit_id ) );
+    require_on_land( W.units().unit_for( cargo_unit_id ) );
+    require_sentried( W.units().unit_for( cargo_unit_id ) );
+  }
+
+  SECTION( "ship with more units on water" ) {
+    UnitId const ship_id =
+        W.add_unit_on_map( e_unit_type::galleon,
+                           { .x = 0, .y = 0 } )
+            .id();
+    UnitId const cargo_unit_id1 =
+        W.add_unit_in_cargo( e_unit_type::free_colonist,
+                             ship_id )
+            .id();
+    UnitId const cargo_unit_id2 =
+        W.add_unit_in_cargo( e_unit_type::free_colonist,
+                             ship_id )
+            .id();
+    UnitId const cargo_unit_id3 =
+        W.add_unit_in_cargo( e_unit_type::free_colonist,
+                             ship_id )
+            .id();
+    unit_id = ship_id;
+    W.gui().EXPECT__choice( _, _ ).returns<maybe<string>>(
+        "yes" );
+    REQUIRE( confirm() == true );
+    perform();
+    REQUIRE_FALSE( W.units().exists( ship_id ) );
+    REQUIRE_FALSE( W.units().exists( cargo_unit_id1 ) );
+    REQUIRE_FALSE( W.units().exists( cargo_unit_id2 ) );
+    REQUIRE_FALSE( W.units().exists( cargo_unit_id3 ) );
+  }
+
+  SECTION( "ship with more units on land" ) {
+    UnitId const ship_id =
+        W.add_unit_on_map( e_unit_type::galleon,
+                           { .x = 1, .y = 1 } )
+            .id();
+    UnitId const cargo_unit_id1 =
+        W.add_unit_in_cargo( e_unit_type::free_colonist,
+                             ship_id )
+            .id();
+    UnitId const cargo_unit_id2 =
+        W.add_unit_in_cargo( e_unit_type::free_colonist,
+                             ship_id )
+            .id();
+    UnitId const cargo_unit_id3 =
+        W.add_unit_in_cargo( e_unit_type::free_colonist,
+                             ship_id )
+            .id();
+    unit_id = ship_id;
+    W.gui().EXPECT__choice( _, _ ).returns<maybe<string>>(
+        "yes" );
+    REQUIRE( confirm() == true );
+    perform();
+    REQUIRE_FALSE( W.units().exists( ship_id ) );
+    REQUIRE( W.units().exists( cargo_unit_id1 ) );
+    REQUIRE( W.units().exists( cargo_unit_id2 ) );
+    REQUIRE( W.units().exists( cargo_unit_id3 ) );
+    require_on_land( W.units().unit_for( cargo_unit_id1 ) );
+    require_on_land( W.units().unit_for( cargo_unit_id2 ) );
+    require_on_land( W.units().unit_for( cargo_unit_id3 ) );
+    require_sentried( W.units().unit_for( cargo_unit_id1 ) );
+    require_sentried( W.units().unit_for( cargo_unit_id2 ) );
+    require_sentried( W.units().unit_for( cargo_unit_id3 ) );
+  }
+}
+
+} // namespace
+} // namespace rn
