0615d100211ccdd840dce3eae4e453b8022d6493
David P. Sicilia <dpsicilia@gmail.com>
2023-10-22 00:58:44 -0400

    Implement some more of the lua SAV parser.

--- tools/sav/save-parser.lua
+++ tools/sav/save-parser.lua
@@ -104,6 +104,29 @@ function SAVParser:backtrace()
   return res
 end
 
+function SAVParser:stats()
+  local res = {}
+  local curr = self.sav_file_:seek()
+  res.bytes_read = curr
+  local size = self.sav_file_:seek( 'end' )
+  self.sav_file_:seek( 'set', curr )
+  local remaining = size - curr
+  assert( remaining >= 0 )
+  res.bytes_remaining = remaining
+  return res
+end
+
+function SAVParser:byte()
+  local c = assert( self.sav_file_:read( 1 ), 'eof' )
+  return string.byte( c )
+end
+
+function SAVParser:bytes( n )
+  local res = {}
+  for _ = 1, n do table.insert( res, self:byte() ) end
+  return res
+end
+
 function SAVParser:struct( struct )
   dbg( 'parsing struct [%s]', self:backtrace() )
   assert( struct )
@@ -118,6 +141,16 @@ function SAVParser:struct( struct )
       res[e] = self:entity( struct, e )
     end
   end
+  res.__key_order = struct.__key_order
+  return res
+end
+
+function SAVParser:struct_array( count, struct )
+  dbg( 'parsing struct [%s]', self:backtrace() )
+  assert( count )
+  assert( count >= 0 )
+  local res = {}
+  for _ = 1, count do table.insert( res, self:struct( struct ) ) end
   return res
 end
 
@@ -125,15 +158,60 @@ function SAVParser:bit_struct( bit_struct )
   dbg( 'parsing bit_struct [%s]', self:backtrace() )
   assert( bit_struct )
   assert( bit_struct.__key_order )
+  local total_bits = 0
+  for _, e in ipairs( bit_struct.__key_order ) do
+    assert( bit_struct[e] )
+    if not e:match( '__' ) then
+      total_bits = total_bits + bit_struct[e].size
+    end
+  end
+  assert( total_bits % 8 == 0,
+          format( 'found %d bits in bit_struct.', total_bits ) )
+  local total_bytes = total_bits // 8
+  -- TODO: for now just read the bytes.
+  for _ = 1, total_bytes do self:byte() end
   return JNULL
 end
 
-function SAVParser:string( desc )
+function SAVParser:string( size )
   dbg( 'parsing string [%s]', self:backtrace() )
-  assert( desc )
-  assert( desc.type == 'str' )
-  assert( type( desc.size ) == 'number' )
-  return JNULL
+  assert( size )
+  assert( type( size ) == 'number' )
+  local res = ''
+  local hit_zero = false
+  for _ = 1, size do
+    local b = self:byte()
+    if b == 0 then hit_zero = true end
+    if not hit_zero then res = res .. char( b ) end
+    -- Need to keep reading until the end of the loop even if we
+    -- hit zero in order to seek past the bytes.
+  end
+  assert( #res <= size )
+  return res
+end
+
+function SAVParser:uint( size )
+  assert( size )
+  assert( type( size ) == 'number' )
+  assert( size > 0 )
+  local ns = {}
+  for _ = 1, size do
+    -- Insert at beginning to make reverse order.
+    table.insert( ns, 1, self:byte() )
+  end
+  local n = 0
+  for _, b in ipairs( ns ) do
+    n = n << 8
+    n = n + b
+  end
+  return n
+end
+
+function SAVParser:lookup_count( count )
+  if type( count ) == 'number' then return count end
+  assert( type( count ) == 'string' )
+  assert( self.saved_[count] )
+  return self.saved_[count]
 end
 
 function SAVParser:entity( parent, field )
@@ -143,24 +221,40 @@ function SAVParser:entity( parent, field )
   assert( type( tbl ) == 'table',
           format( 'field %s is not a table', field ) )
   table.insert( self.backtrace_, field )
-  local res = JNULL
+  local res
   if tbl.struct then
-    res = self:struct( tbl.struct )
+    if tbl.count then
+      local count = self:lookup_count( tbl.count )
+      assert( type( count ) == 'number' )
+      res = self:struct_array( count, tbl.struct )
+    else
+      res = self:struct( tbl.struct )
+    end
   elseif tbl.bit_struct then
     res = self:bit_struct( tbl.bit_struct )
-  elseif tbl.size then
+  else
+    assert( type( tbl.size ) == 'number' )
     if tbl.type == 'str' then
-      res = self:string( tbl ) --
+      res = self:string( tbl.size )
+    elseif tbl.type == 'uint' then
+      res = self:uint( tbl.size )
+    else
+      self:bytes( tbl.size )
+      res = JNULL
     end
   end
+  res = res or JNULL
+  if tbl.save_meta then self.saved_[field] = res end
   table.remove( self.backtrace_ )
   return res
 end
 
-local function NewSAVParser( metadata )
+local function NewSAVParser( sav_file, metadata )
   local obj = {}
+  obj.sav_file_ = sav_file
   obj.metadata_ = metadata
   obj.backtrace_ = {}
+  obj.saved_ = {}
   setmetatable( obj, {
     __newindex=function() error( 'cannot modify parsers.', 2 ) end,
     __index=SAVParser,
@@ -171,14 +265,19 @@ end
 
 local function parse_sav( structure, sav )
   assert( sav )
-  local parser = NewSAVParser( assert( structure.__metadata ) )
-  local res = { _type='SAV' }
+  assert( structure.__metadata )
+  local parser = NewSAVParser( sav, structure.__metadata )
+  local res
   local success, msg = pcall( function()
-    res.HEAD = parser:struct( structure )
+    -- res = parser:struct( structure, 1 )
+    res = {}
+    res.HEAD = parser:entity( structure, 'HEAD' )
+    -- Last.
+    -- res._type = 'SAV'
   end )
   assert( success, format( 'error at location [%s]: %s',
                            parser:backtrace(), msg ) )
-  return res
+  return res, parser:stats()
 end
 
 -----------------------------------------------------------------
@@ -207,9 +306,12 @@ local function pprint( o, prefix, spaces )
     if #spaces == 0 then s = prefix end
     s = s .. name .. '{\n'
     spaces = spaces .. '  '
-    local keys = {}
-    for k, _ in pairs( o ) do table.insert( keys, k ) end
-    table.sort( keys )
+    local keys = o.__key_order
+    if not keys then
+      keys = {}
+      for k, _ in pairs( o ) do table.insert( keys, k ) end
+      table.sort( keys )
+    end
 
     for _, k in ipairs( keys ) do
       if not tostring( k ):match( '^_' ) then
@@ -251,10 +353,16 @@ local function main( args )
   check( colony_sav:match( '^COLONY%d%d%.SAV$' ),
          'colony_sav %s has invalid format.', colony_sav )
   log( 'reading save file %s', colony_sav )
-  local res = parse_sav( structure,
-                         assert( io.open( colony_sav, 'rb' ) ) )
+  local res, stats = parse_sav( structure, assert(
+                                    io.open( colony_sav, 'rb' ) ) )
+  log( 'finished parsing. stats:' )
+  log( 'bytes read: %d', stats.bytes_read )
+  log( 'bytes remaining: %d', stats.bytes_remaining )
   log( 'finished parsing. output:' )
   print( pprint( res, '', '' ) )
+  if stats.bytes_remaining > 0 then
+    err( 'bytes remaining: %d', stats.bytes_remaining )
+  end
   return 0
 end
 
