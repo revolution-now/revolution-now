1f39bd9df0f9bd58fb4677eba85c03d752b50ef8
David P. Sicilia <dpsicilia@gmail.com>
2023-11-10 10:47:42 -0500

    In binary round-trip test, only pick one random file at a time.

--- test/sav/binary-test.cpp
+++ test/sav/binary-test.cpp
@@ -52,9 +52,9 @@ fs::path output_folder() {
   return res;
 }
 
-bool attempt_round_trip( fs::path const& folder,
-                         fs::path const& file,
-                         fs::path const& tmp ) {
+bool binary_round_trip( fs::path const& folder,
+                        fs::path const& file,
+                        fs::path const& tmp ) {
   static ColonySAV sav;
   fs::path const   in  = folder / file;
   fs::path const   out = tmp / file;
@@ -63,59 +63,58 @@ bool attempt_round_trip( fs::path const& folder,
   return file_contents_same( in, out );
 }
 
+template<typename T>
+T const& pick_one( vector<T> const& v ) {
+  BASE_CHECK( !v.empty() );
+  random_device         r;
+  default_random_engine e1( r() );
+
+  uniform_int_distribution<int> uniform_dist( 0, v.size() - 1 );
+  return v[uniform_dist( e1 )];
+}
+
 /****************************************************************
 ** Test Cases
 *****************************************************************/
-TEST_CASE( "[sav/binary] round trip" ) {
+TEST_CASE( "[sav/binary] binary roundtrip" ) {
   fs::path const tmp = output_folder();
 
-  SECTION( "new" ) {
-    fs::path const folder =
-        classic_sav_dir() / "dutch-viceroy-playthrough";
+  fs::path const classic_dir = classic_sav_dir() / "1990s";
+  fs::path const modern_dir =
+      classic_sav_dir() / "dutch-viceroy-playthrough";
 
-    // clang-format off
-    static fs::path const paths[] = {
-        "dutch-viceroy-1492-options-set-cheat-enabled.SAV",
-        "dutch-viceroy-1492-start.SAV",
-        "dutch-viceroy-1500-first-colony.SAV",
-        "dutch-viceroy-1516-two-colonies-two-trade-routes.SAV",
-        "dutch-viceroy-1519-at-war-with-natives-with-treasure.SAV",
-        "dutch-viceroy-1524-met-the-english-at-war.SAV",
-        "dutch-viceroy-1525-just-after-declaration.SAV",
-        "dutch-viceroy-1525-just-before-declaration.SAV",
-        "dutch-viceroy-1526-ref-landed.SAV",
-        "dutch-viceroy-1533-mid-war.SAV",
-        "dutch-viceroy-1534-all-tribes-killed.SAV",
-        "dutch-viceroy-1534-most-tribes-killed.SAV",
-        "dutch-viceroy-1542-almost-won-independence.SAV",
-        "dutch-viceroy-1551-ref-gone-win-next-turn.SAV",
-        "dutch-viceroy-1552-post-independence-keep-playing.SAV",
-        "dutch-viceroy-1560-post-independence-cleared-all-"
-        "entities.SAV",
+  // clang-format off
+    static vector<pair<fs::path, fs::path>> const paths{
+        { modern_dir, "dutch-viceroy-1492-options-set-cheat-enabled.SAV" },
+        { modern_dir, "dutch-viceroy-1492-start.SAV" },
+        { modern_dir, "dutch-viceroy-1500-first-colony.SAV" },
+        { modern_dir, "dutch-viceroy-1516-two-colonies-two-trade-routes.SAV" },
+        { modern_dir, "dutch-viceroy-1519-at-war-with-natives-with-treasure.SAV" },
+        { modern_dir, "dutch-viceroy-1524-met-the-english-at-war.SAV" },
+        { modern_dir, "dutch-viceroy-1525-just-after-declaration.SAV" },
+        { modern_dir, "dutch-viceroy-1525-just-before-declaration.SAV" },
+        { modern_dir, "dutch-viceroy-1526-ref-landed.SAV" },
+        { modern_dir, "dutch-viceroy-1533-mid-war.SAV" },
+        { modern_dir, "dutch-viceroy-1534-all-tribes-killed.SAV" },
+        { modern_dir, "dutch-viceroy-1534-most-tribes-killed.SAV" },
+        { modern_dir, "dutch-viceroy-1542-almost-won-independence.SAV" },
+        { modern_dir, "dutch-viceroy-1551-ref-gone-win-next-turn.SAV" },
+        { modern_dir, "dutch-viceroy-1552-post-independence-keep-playing.SAV" },
+        { modern_dir, "dutch-viceroy-1560-post-independence-cleared-all-entities.SAV" },
+        { classic_dir, "COLONY00.SAV" },
+        { classic_dir, "COLONY01.SAV" },
+        { classic_dir, "COLONY02.SAV" },
+        { classic_dir, "COLONY08.SAV" },
+        { classic_dir, "COLONY09.SAV" },
+        { classic_dir, "COLONY10.SAV" },
     };
-    // clang-format on
+  // clang-format on
 
-    for( auto const& file : paths ) {
-      INFO( fmt::format( "path: {}", file ) );
-      bool const good = attempt_round_trip( folder, file, tmp );
-      REQUIRE( good );
-    }
-  }
-
-  SECTION( "old" ) {
-    fs::path const folder = classic_sav_dir() / "1990s";
-
-    static fs::path const paths[] = {
-        "COLONY00.SAV", "COLONY01.SAV", "COLONY02.SAV",
-        "COLONY08.SAV", "COLONY09.SAV", "COLONY10.SAV",
-    };
-
-    for( auto const& file : paths ) {
-      INFO( fmt::format( "path: {}", file ) );
-      bool const good = attempt_round_trip( folder, file, tmp );
-      REQUIRE( good );
-    }
-  }
+  // Can't do all of these cause it's too slow.
+  auto& [dir, file] = pick_one( paths );
+  INFO( fmt::format( "path: {}", dir / file ) );
+  bool const good = binary_round_trip( dir, file, tmp );
+  REQUIRE( good );
 }
 
 } // namespace
