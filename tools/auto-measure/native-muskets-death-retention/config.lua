a4ad5834dab8698d39909c0f16ad65b086c650ac
David P. Sicilia <dpsicilia@gmail.com>
2024-01-27 12:14:04 -0500

    Add comments describing the auto-measure scenarios.

--- tools/auto-measure/native-muskets-death-retention/config.lua
+++ tools/auto-measure/native-muskets-death-retention/config.lua
@@ -1,3 +1,17 @@
+-- Test the probability that, when an armed brave is destroyed,
+-- the tribe will retain the muskets.
+--
+-- In this test we have a european military unit waiting for
+-- order sitting next to an armed brave. The european unit at-
+-- tacks the branch. If the european unit loses then that is
+-- recorded, but it is not relevant. If the european unit wins
+-- (meaning that the armed brave has been destroyed) we measure
+-- whether the tribe has retained the muskets. The tribe's mus-
+-- kets start at 0 and if they end up at 1 then that means that
+-- the tribe retained the muskets. Ideally the european unit
+-- should be as strong as possible to minimize the number of out-
+-- comes where it loses, although it is OK if it loses since that
+-- result will be sifted out.
 return {
   -- Variable parameter.
   difficulty={
--- tools/auto-measure/native-muskets-distribution/config.lua
+++ tools/auto-measure/native-muskets-distribution/config.lua
@@ -1,3 +1,19 @@
+-- Measure the probability that, when a brave is given muskets,
+-- the tribe's stockpile reduces.
+--
+-- This test starts with a single native dwelling of a tribe that
+-- has some muskets, and there are no european units involved.
+-- The end of turn is blinking. Broadly speaking there are two
+-- types of this test, "old" and "new", parametrized below. For
+-- the "old" test, we have an existing brave sitting right on top
+-- of the dwelling, then we advance the turn and then measure
+-- whether the brave has been given muskets (i.e., it becomes an
+-- armed brave) and, if so, whether the tribe has reduced its
+-- stockpile. In the "new" case we have no braves to start, but
+-- the dwelling's growth_counter is set to 20 so that when we ad-
+-- vance the turn it will always create a brave, and then we
+-- again measure whether it was given muskets and, if so, whether
+-- the tribe's stockpile was decreased as a result.
 return {
   -- Variable parameters.
   difficulty={
