e3b2fad157b2f9d5705fd3d18a29e49194d3c2f9
David P. Sicilia <dpsicilia@gmail.com>
2024-10-17 08:36:20 -0400

    Enhance clang-formatting script.

--- tools/clang-format-all.sh
+++ tools/clang-format-all.sh
@@ -8,9 +8,38 @@ cd ..           # project root folder
 [[ -d src/ ]]   # sanity check
 
 files() {
-  fdfind '\.?pp$' src/ exe/ test/ \
-    --type=f                      \
-    --exclude='test/data'
+  fdfind '\.?pp$' src/ exe/ test/     \
+    --type=f                          \
+    --exclude='test/data'             \
+    --exclude='sav-struct.*'
 }
 
-clang-format -i $(files)
+erase_current_line() {
+  echo -ne "                                                  \r"
+}
+
+# Will check that the files are formatted and exit with an error
+# code if not.
+check() {
+  local out=/tmp/clang-format-tmp-output.txt
+  for file in $(files); do
+    echo -ne "checking: $file                                \r"
+    clang-format --fail-on-incomplete-format "$file" &>"$out"
+    if ! diff -u "$file" "$out" &>/dev/null; then
+      erase_current_line
+      diff --color=always -u "$file" "$out"
+      echo
+      echo "unformatted file: $file"
+    fi
+  done
+  erase_current_line
+  echo "all source files already formatted."
+}
+
+# Actually format the files (in place).
+format() {
+  clang-format -i $(files)
+}
+
+check
+#format
