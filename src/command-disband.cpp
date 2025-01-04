47c18c9ed527c942e2c3f712dcef5909c5654420
David P. Sicilia <dpsicilia@gmail.com>
2025-01-04 16:32:54 -0500

    Forgot to co_await.

--- src/command-disband.cpp
+++ src/command-disband.cpp
@@ -53,7 +53,7 @@ struct DisbandHandler : public CommandHandler {
   wait<> perform() override {
     point const tile =
         coord_for_unit_indirect_or_die( ss_.units, unit_id_ );
-    execute_disband( ss_, ts_, *viz_, tile, entities_ );
+    co_await execute_disband( ss_, ts_, *viz_, tile, entities_ );
     co_return;
   }
 
