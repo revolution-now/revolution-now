ff5b0d2f46ecfcc7eb33b1ba6bb3b7f6e4b6b60a
David P. Sicilia <dpsicilia@gmail.com>
2025-02-22 12:56:47 -0500

    Update the watch-test script.

--- tools/watch-test.sh
+++ tools/watch-test.sh
@@ -1,20 +1,9 @@
 #!/bin/bash
 set -o pipefail
 
-build_threads() {
-  local threads=4
-  if [[ "$(uname)" == Linux ]]; then
-    threads=$(nproc --all)
-  elif [[ "$(uname)" == Darwin ]]; then
-    threads=$(sysctl -n hw.ncpu)
-  fi
-  echo "$threads"
-}
-export -f build_threads
-
 do_build() {
   clear
-  make test -j$(build_threads)
+  make test
 }
 export -f do_build
 
