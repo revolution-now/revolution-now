e1243d008400e7a508c064187f692e1cb8ec9b49
David P. Sicilia <dpsicilia@gmail.com>
2025-06-22 01:11:51 -0400

    Change some ss fields tracking post-declaration state.

--- src/ss/revolution.cpp
+++ src/ss/revolution.cpp
@@ -35,10 +35,25 @@ using ::base::valid_or;
 ** RevolutionState
 *****************************************************************/
 valid_or<string> RevolutionState::validate() const {
+  // Check that rebel sentiment is in the right range.
   REFL_VALIDATE( rebel_sentiment >= 0 && rebel_sentiment <= 100,
                  "rebel sentiment must be in the range 0-100, "
                  "but is equal to {}.",
                  rebel_sentiment );
+
+  // Check that, if we haven't declared, that the
+  // post-declaration event flags are not set.
+  if( status < e_revolution_status::declared ) {
+    REFL_VALIDATE( !continental_army_mobilized,
+                   "the continental_army_mobilized flag cannot "
+                   "be set prior to the declaration." );
+    REFL_VALIDATE( !gave_independence_war_hints,
+                   "the gave_independence_war_hints flag cannot "
+                   "be set prior to the declaration." );
+    REFL_VALIDATE( !intervention_force_deployed,
+                   "the intervention_force_deployed flag cannot "
+                   "be set prior to the declaration." );
+  }
 
   return valid;
 }
@@ -68,7 +83,10 @@ LUA_STARTUP( lua::state& st ) {
 
   u["rebel_sentiment"] = &U::rebel_sentiment;
   u["status"]          = &U::status;
-  u["armies_promoted"] = &U::armies_promoted;
+  u["continental_army_mobilized"] =
+      &U::continental_army_mobilized;
+  u["gave_independence_war_hints"] =
+      &U::gave_independence_war_hints;
   u["intervention_force_deployed"] =
       &U::intervention_force_deployed;
   u["ref_will_forfeit"]    = &U::ref_will_forfeit;
