d1421354881fcb6244506ce91e0b887f348da27f
David P. Sicilia <dpsicilia@gmail.com>
2025-07-19 21:25:48 -0400

    Add unit tests for the show-anim module.

--- src/show-anim.cpp
+++ src/show-anim.cpp
@@ -32,47 +32,43 @@ namespace {
 
 using ::gfx::point;
 
+bool show_moves_for_society( SSConst const& ss,
+                             Society const& society ) {
+  auto const& options =
+      ss.settings.in_game_options.game_menu_options;
+  SWITCH( society ) {
+    using enum e_game_menu_option;
+    CASE( european ) { return options[show_foreign_moves]; }
+    CASE( native ) { return options[show_indian_moves]; }
+  }
+}
+
 bool should_animate_society( SSConst const& ss,
                              maybe<e_player> const viewer,
                              Society const& society ) {
+  if( show_moves_for_society( ss, society ) ) return true;
+  // Since the relevant game option flag does not allow us to au-
+  // tomatically show the moves, we will now try to determine if
+  // the society is "friendly" or not. However, the definition of
+  // what constitutes "friendly" depends on a few things.
   SWITCH( society ) {
+    CASE( native ) { return false; }
     CASE( european ) {
-      // In the below, our goal is to determine whether the euro-
-      // pean player on this square is "foreign" or not, and then
-      // use the "show foreign moves" game option. However, the
-      // definition of what constitutes "foreign" depends on a
-      // few different things.
-      auto const switch_foreign = [&]( bool const foreign ) {
-        if( !foreign ) return true;
-        return ss.settings.in_game_options.game_menu_options
-            [e_game_menu_option::show_foreign_moves];
-      };
-
-      if( viewer.has_value() ) {
+      if( viewer.has_value() )
         // We are viewing from one player's perspective, so a
         // foreign power is any other player.
-        bool const foreign = european.player != *viewer;
-        return switch_foreign( foreign );
-      } else {
-        // We see the entire map. So in that case we need to
-        // check the player control. Since if the player has re-
-        // vealed the entire map they can still turn off foreign
-        // moves and not see the moves of AI players. Another
-        // possibility is that there are no human players in the
-        // game and it is the natives' turn. In that case it also
-        // seems sensible to go based on control. So we'll just
-        // assume that the AI players are the ones that the user
-        // would consider "foreign".
-        UNWRAP_CHECK_T( Player const& european_player,
-                        ss.players.players[european.player] );
-        bool const foreign =
-            european_player.control == e_player_control::ai;
-        return switch_foreign( foreign );
-      }
-    }
-    CASE( native ) {
-      return ss.settings.in_game_options.game_menu_options
-          [e_game_menu_option::show_indian_moves];
+        return european.player == *viewer;
+      // We see the entire map. So in that case we need to check
+      // the player control. Since if the player has revealed the
+      // entire map they can still turn off foreign moves and not
+      // see the moves of AI players. Another possibility is that
+      // there are no human players in the game and it is the na-
+      // tives' turn. In that case it also seems sensible to go
+      // based on control. So just assume that the AI players are
+      // the ones that the viewer would consider "foreign".
+      UNWRAP_CHECK_T( Player const& european_player,
+                      ss.players.players[european.player] );
+      return european_player.control == e_player_control::human;
     }
   }
 }
--- test/show-anim-test.cpp
+++ test/show-anim-test.cpp
@@ -16,6 +16,17 @@
 // Testing.
 #include "test/fake/world.hpp"
 
+// Revolution Now
+#include "src/anim-builder.rds.hpp"
+#include "src/imap-updater.hpp"
+
+// ss
+#include "src/ss/land-view.rds.hpp"
+#include "src/ss/ref.hpp"
+#include "src/ss/settings.rds.hpp"
+#include "src/ss/unit-composition.hpp"
+#include "src/ss/unit.hpp"
+
 // Must be last.
 #include "test/catch-common.hpp" // IWYU pragma: keep
 
@@ -29,7 +40,10 @@ using namespace std;
 *****************************************************************/
 struct world : testing::World {
   world() {
-    add_default_player();
+    add_player( e_player::english );
+    add_player( e_player::spanish );
+    set_default_player_type( e_player::english );
+    set_human_player_and_rest_ai( e_player::english );
     create_default_map();
   }
 
@@ -38,7 +52,7 @@ struct world : testing::World {
     MapSquare const L = make_grassland();
     vector<MapSquare> tiles{
       L, L, L, //
-      L, _, L, //
+      L, L, L, //
       L, L, L, //
     };
     build_map( std::move( tiles ), 3 );
@@ -50,10 +64,185 @@ struct world : testing::World {
 *****************************************************************/
 TEST_CASE( "[show-anim] should_animate_tile" ) {
   world w;
+  AnimatedTile anim_tile;
+  e_player const player = w.default_player_type();
+
+  w.land_view().map_revealed =
+      MapRevealed::player{ .type = player };
+
+  auto const f = [&] [[clang::noinline]] {
+    return should_animate_tile( w.ss(), anim_tile );
+  };
+
+  using enum e_game_menu_option;
+
+  auto& options = w.settings().in_game_options.game_menu_options;
+
+  // Default.
+  anim_tile = { .tile = { .x = 0, .y = 0 }, .inhabitants = {} };
+  REQUIRE_FALSE( f() );
+
+  anim_tile                  = { .tile        = { .x = 0, .y = 0 },
+                                 .inhabitants = { Society::native{} } };
+  options[show_indian_moves] = false;
+  REQUIRE_FALSE( f() );
+  options[show_indian_moves] = true;
+  REQUIRE_FALSE( f() );
+
+  anim_tile = {
+    .tile        = { .x = 0, .y = 0 },
+    .inhabitants = { Society::european{ .player = player } } };
+  options[show_foreign_moves] = false;
+  REQUIRE_FALSE( f() );
+  options[show_foreign_moves] = true;
+  REQUIRE_FALSE( f() );
+
+  anim_tile                   = { .tile        = { .x = 0, .y = 0 },
+                                  .inhabitants = { Society::european{
+                                    .player = e_player::spanish } } };
+  options[show_foreign_moves] = false;
+  REQUIRE_FALSE( f() );
+  options[show_foreign_moves] = true;
+  REQUIRE_FALSE( f() );
+
+  w.map_updater().make_squares_visible( player,
+                                        { { .x = 0, .y = 0 } } );
+
+  // Default.
+  anim_tile = { .tile = { .x = 0, .y = 0 }, .inhabitants = {} };
+  REQUIRE_FALSE( f() );
+
+  anim_tile                  = { .tile        = { .x = 0, .y = 0 },
+                                 .inhabitants = { Society::native{} } };
+  options[show_indian_moves] = false;
+  REQUIRE_FALSE( f() );
+  options[show_indian_moves] = true;
+  REQUIRE( f() );
+
+  anim_tile = {
+    .tile        = { .x = 0, .y = 0 },
+    .inhabitants = { Society::european{ .player = player } } };
+  options[show_foreign_moves] = false;
+  REQUIRE( f() );
+  options[show_foreign_moves] = true;
+  REQUIRE( f() );
+
+  anim_tile                   = { .tile        = { .x = 0, .y = 0 },
+                                  .inhabitants = { Society::european{
+                                    .player = e_player::spanish } } };
+  options[show_foreign_moves] = false;
+  REQUIRE_FALSE( f() );
+  options[show_foreign_moves] = true;
+  REQUIRE( f() );
+
+  w.map_updater().make_squares_fogged( player,
+                                       { { .x = 0, .y = 0 } } );
+
+  w.land_view().map_revealed = MapRevealed::entire{};
+
+  // Default.
+  anim_tile = { .tile = { .x = 0, .y = 0 }, .inhabitants = {} };
+  REQUIRE_FALSE( f() );
+
+  anim_tile                  = { .tile        = { .x = 0, .y = 0 },
+                                 .inhabitants = { Society::native{} } };
+  options[show_indian_moves] = false;
+  REQUIRE_FALSE( f() );
+  options[show_indian_moves] = true;
+  REQUIRE( f() );
+
+  anim_tile = {
+    .tile        = { .x = 0, .y = 0 },
+    .inhabitants = { Society::european{ .player = player } } };
+  options[show_foreign_moves] = false;
+  REQUIRE( f() );
+  options[show_foreign_moves] = true;
+  REQUIRE( f() );
+
+  anim_tile                   = { .tile        = { .x = 0, .y = 0 },
+                                  .inhabitants = { Society::european{
+                                    .player = e_player::spanish } } };
+  options[show_foreign_moves] = false;
+  REQUIRE_FALSE( f() );
+  options[show_foreign_moves] = true;
+  REQUIRE( f() );
 }
 
 TEST_CASE( "[show-anim] should_animate_seq" ) {
   world w;
+  AnimationSequence seq;
+  e_player const player = w.default_player_type();
+
+  w.land_view().map_revealed =
+      MapRevealed::player{ .type = player };
+
+  UnitId const unit_id =
+      w.add_unit_on_map( e_unit_type::free_colonist,
+                         { .x = 0, .y = 0 } )
+          .id();
+  UnitId const foreign_unit_id_1 =
+      w.add_unit_on_map( e_unit_type::free_colonist,
+                         { .x = 0, .y = 1 }, e_player::spanish )
+          .id();
+  UnitId const foreign_unit_id_2 =
+      w.add_unit_on_map( e_unit_type::free_colonist,
+                         { .x = 1, .y = 1 }, e_player::spanish )
+          .id();
+
+  auto const f = [&] [[clang::noinline]] {
+    return should_animate_seq( w.ss(), seq );
+  };
+
+  using enum e_game_menu_option;
+
+  auto& options = w.settings().in_game_options.game_menu_options;
+
+  seq = AnimationSequence{
+    .sequence = { { AnimationAction{
+      .primitive = AnimationPrimitive::hide_unit{
+        .unit_id = foreign_unit_id_1 } } } } };
+  REQUIRE_FALSE( f() );
+
+  seq = AnimationSequence{
+    .sequence = {
+      { AnimationAction{ .primitive =
+                             AnimationPrimitive::hide_unit{
+                               .unit_id = foreign_unit_id_1 } },
+        AnimationAction{
+          .primitive = AnimationPrimitive::hide_unit{
+            .unit_id = foreign_unit_id_2 } } } } };
+  REQUIRE_FALSE( f() );
+
+  seq = AnimationSequence{
+    .sequence = { { AnimationAction{
+      .primitive = AnimationPrimitive::hide_unit{
+        .unit_id = unit_id } } } } };
+  REQUIRE( f() );
+
+  seq = AnimationSequence{
+    .sequence = {
+      { AnimationAction{ .primitive =
+                             AnimationPrimitive::hide_unit{
+                               .unit_id = foreign_unit_id_1 } },
+        AnimationAction{ .primitive =
+                             AnimationPrimitive::hide_unit{
+                               .unit_id = foreign_unit_id_2 } },
+        AnimationAction{ .primitive =
+                             AnimationPrimitive::hide_unit{
+                               .unit_id = unit_id } } } } };
+  REQUIRE( f() );
+
+  options[show_foreign_moves] = true;
+
+  seq = AnimationSequence{
+    .sequence = {
+      { AnimationAction{ .primitive =
+                             AnimationPrimitive::hide_unit{
+                               .unit_id = foreign_unit_id_1 } },
+        AnimationAction{
+          .primitive = AnimationPrimitive::hide_unit{
+            .unit_id = foreign_unit_id_2 } } } } };
+  REQUIRE( f() );
 }
 
 } // namespace
