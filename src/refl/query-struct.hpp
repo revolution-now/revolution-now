f76941f4ac06708d8ae5847e70c232069cfd8736
David P. Sicilia <dpsicilia@gmail.com>
2025-09-04 09:58:06 -0400

    Add the on_each_field function.

--- src/refl/query-struct.hpp
+++ src/refl/query-struct.hpp
@@ -38,4 +38,17 @@ template<ReflectedStruct S>
 using member_type_list_t = detail::member_type_list_impl_t<
     std::remove_cvref_t<decltype( traits<S>::fields )>>;
 
+template<ReflectedStruct S, typename Fn>
+void on_each_field( S& strukt, Fn& fn ) {
+  WARNING_THIS_FUNCTION_HAS_NOT_BEEN_TESTED;
+  auto constexpr& kFieldDescriptors = traits<S>::fields;
+  static constexpr auto kNumFields =
+      std::tuple_size_v<decltype( kFieldDescriptors )>;
+  FOR_CONSTEXPR_IDX( Idx, kNumFields ) {
+    auto const& field_desc = std::get<Idx>( kFieldDescriptors );
+    auto& field_val        = strukt.*field_desc.accessor;
+    return fn( field_desc.name, field_val );
+  };
+}
+
 } // namespace refl
