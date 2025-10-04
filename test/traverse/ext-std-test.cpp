d8df3297d2353656be1346cd0972e8f5079160f7
David P. Sicilia <dpsicilia@gmail.com>
2025-10-04 14:07:27 -0400

    Add test cases for the traverse/ext-std module.

--- test/traverse/ext-std-test.cpp
+++ test/traverse/ext-std-test.cpp
@@ -13,6 +13,12 @@
 // Under test.
 #include "src/traverse/ext-std.hpp"
 
+// refl
+#include "src/refl/to-str.hpp"
+
+// base
+#include "src/base/to-str-ext-std.hpp"
+
 // Must be last.
 #include "test/catch-common.hpp" // IWYU pragma: keep
 
@@ -25,27 +31,250 @@ using namespace std;
 ** Test Cases
 *****************************************************************/
 TEST_CASE( "[traverse/ext-std] pair" ) {
+  using T          = pair<string, int>;
+  using K_expected = string_view;
+  T o;
+
+  vector<string> v;
+  auto const traversing_fn = [&]<typename V, typename K>(
+                                 V const& val, K const key ) {
+    static_assert( is_same_v<K, K_expected> );
+    v.push_back( format( "{}", key ) );
+    if constexpr( is_same_v<V, int> ) v.push_back( "int" );
+    if constexpr( is_same_v<V, string> ) v.push_back( "string" );
+    v.push_back( format( "{}", val ) );
+  };
+
+  auto const f = [&] [[clang::noinline]] {
+    trv::traverse( o, traversing_fn );
+  };
+
+  o.first  = "hello";
+  o.second = 3;
+  f();
+
+  REQUIRE( v == vector<string>{
+                  "first",
+                  "string",
+                  "hello",
+                  "second",
+                  "int",
+                  "3",
+                } );
 }
 
 TEST_CASE( "[traverse/ext-std] vector (range)" ) {
+  using T          = vector<string>;
+  using K_expected = int;
+  T o;
+
+  vector<string> v;
+  auto const traversing_fn = [&]<typename V, typename K>(
+                                 V const& val, K const key ) {
+    static_assert( is_same_v<K, K_expected> );
+    v.push_back( format( "{}", key ) );
+    if constexpr( is_same_v<V, int> ) v.push_back( "int" );
+    if constexpr( is_same_v<V, string> ) v.push_back( "string" );
+    v.push_back( format( "{}", val ) );
+  };
+
+  auto const f = [&] [[clang::noinline]] {
+    trv::traverse( o, traversing_fn );
+  };
+
+  o = { "hello", "world" };
+  f();
+
+  REQUIRE( v == vector<string>{
+                  "0",
+                  "string",
+                  "hello",
+                  "1",
+                  "string",
+                  "world",
+                } );
 }
 
 TEST_CASE( "[traverse/ext-std] unordered_map" ) {
+  using T          = unordered_map<string, int>;
+  using K_expected = string;
+  T o;
+
+  vector<string> v;
+  auto const traversing_fn = [&]<typename V, typename K>(
+                                 V const& val, K const key ) {
+    static_assert( is_same_v<K, K_expected> );
+    v.push_back( format( "{}", key ) );
+    if constexpr( is_same_v<V, int> ) v.push_back( "int" );
+    if constexpr( is_same_v<V, string> ) v.push_back( "string" );
+    v.push_back( format( "{}", val ) );
+  };
+
+  auto const f = [&] [[clang::noinline]] {
+    trv::traverse( o, traversing_fn );
+  };
+
+  o["hello"] = 3;
+  o["world"] = 4;
+  f();
+
+  sort( v.begin(), v.end() );
+  REQUIRE( v == vector<string>{
+                  "3",
+                  "4",
+                  "hello",
+                  "int",
+                  "int",
+                  "world",
+                } );
 }
 
 TEST_CASE( "[traverse/ext-std] unordered_set" ) {
+  using T          = unordered_set<int>;
+  using K_expected = trv::none_t;
+  T o;
+
+  vector<string> v;
+  auto const traversing_fn = [&]<typename V, typename K>(
+                                 V const& val, K const ) {
+    static_assert( is_same_v<K, K_expected> );
+    if constexpr( is_same_v<V, int> ) v.push_back( "int" );
+    if constexpr( is_same_v<V, string> ) v.push_back( "string" );
+    v.push_back( format( "{}", val ) );
+  };
+
+  auto const f = [&] [[clang::noinline]] {
+    trv::traverse( o, traversing_fn );
+  };
+
+  o.insert( 3 );
+  o.insert( 5 );
+  f();
+
+  sort( v.begin(), v.end() );
+  REQUIRE( v == vector<string>{
+                  "3",
+                  "5",
+                  "int",
+                  "int",
+                } );
 }
 
 TEST_CASE( "[traverse/ext-std] map" ) {
+  using T          = map<string, int>;
+  using K_expected = string;
+  T o;
+
+  vector<string> v;
+  auto const traversing_fn = [&]<typename V, typename K>(
+                                 V const& val, K const key ) {
+    static_assert( is_same_v<K, K_expected> );
+    v.push_back( format( "{}", key ) );
+    if constexpr( is_same_v<V, int> ) v.push_back( "int" );
+    if constexpr( is_same_v<V, string> ) v.push_back( "string" );
+    v.push_back( format( "{}", val ) );
+  };
+
+  auto const f = [&] [[clang::noinline]] {
+    trv::traverse( o, traversing_fn );
+  };
+
+  o["hello"] = 3;
+  o["world"] = 4;
+  f();
+
+  REQUIRE( v == vector<string>{
+                  "hello",
+                  "int",
+                  "3",
+                  "world",
+                  "int",
+                  "4",
+                } );
 }
 
 TEST_CASE( "[traverse/ext-std] set" ) {
+  using T          = set<int>;
+  using K_expected = trv::none_t;
+  T o;
+
+  vector<string> v;
+  auto const traversing_fn = [&]<typename V, typename K>(
+                                 V const& val, K const ) {
+    static_assert( is_same_v<K, K_expected> );
+    if constexpr( is_same_v<V, int> ) v.push_back( "int" );
+    if constexpr( is_same_v<V, string> ) v.push_back( "string" );
+    v.push_back( format( "{}", val ) );
+  };
+
+  auto const f = [&] [[clang::noinline]] {
+    trv::traverse( o, traversing_fn );
+  };
+
+  o.insert( 3 );
+  o.insert( 5 );
+  f();
+
+  REQUIRE( v == vector<string>{
+                  "int",
+                  "3",
+                  "int",
+                  "5",
+                } );
 }
 
 TEST_CASE( "[traverse/ext-std] unique_ptr" ) {
+  using T          = unique_ptr<int>;
+  using K_expected = trv::none_t;
+  T o;
+
+  vector<string> v;
+  auto const traversing_fn = [&]<typename V, typename K>(
+                                 V const& val, K const ) {
+    static_assert( is_same_v<K, K_expected> );
+    if constexpr( is_same_v<V, int> ) v.push_back( "int" );
+    if constexpr( is_same_v<V, string> ) v.push_back( "string" );
+    v.push_back( format( "{}", val ) );
+  };
+
+  auto const f = [&] [[clang::noinline]] {
+    trv::traverse( o, traversing_fn );
+  };
+
+  o = make_unique<int>( 7 );
+  f();
+
+  REQUIRE( v == vector<string>{
+                  "int",
+                  "7",
+                } );
 }
 
 TEST_CASE( "[traverse/ext-std] shared_ptr" ) {
+  using T          = shared_ptr<int>;
+  using K_expected = trv::none_t;
+  T o;
+
+  vector<string> v;
+  auto const traversing_fn = [&]<typename V, typename K>(
+                                 V const& val, K const ) {
+    static_assert( is_same_v<K, K_expected> );
+    if constexpr( is_same_v<V, int> ) v.push_back( "int" );
+    if constexpr( is_same_v<V, string> ) v.push_back( "string" );
+    v.push_back( format( "{}", val ) );
+  };
+
+  auto const f = [&] [[clang::noinline]] {
+    trv::traverse( o, traversing_fn );
+  };
+
+  o = make_shared<int>( 7 );
+  f();
+
+  REQUIRE( v == vector<string>{
+                  "int",
+                  "7",
+                } );
 }
 
 } // namespace
