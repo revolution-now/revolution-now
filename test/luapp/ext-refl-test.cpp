9c8de51b9bbbac640d6e3a4983cbec8425483d15
David P. Sicilia <dpsicilia@gmail.com>
2025-12-09 17:32:34 -0500

    Add test case for the luapp/ext-refl module.

--- test/luapp/ext-refl-test.cpp
+++ test/luapp/ext-refl-test.cpp
@@ -13,6 +13,13 @@
 // Under test.
 #include "src/luapp/ext-refl.hpp"
 
+// Testing.
+#include "test/luapp/common.hpp"
+#include "test/rds/testing.rds.hpp"
+
+// refl
+#include "src/refl/to-str.hpp"
+
 // Must be last.
 #include "test/catch-common.hpp" // IWYU pragma: keep
 
@@ -24,10 +31,52 @@ using namespace std;
 /****************************************************************
 ** Test Cases
 *****************************************************************/
-TEST_CASE( "[luapp/ext-refl] ReflectedStruct API" ) {
-}
+// Here we are testing both the variant and struct at once be-
+// cause the variant's members are reflected structs.
+LUA_TEST_CASE(
+    "[luapp/ext-refl] Reflected Variant/Struct API" ) {
+  using ::rn::MySumtype;
+  /*
+   sumtype.MySumtype {
+    none {},
+    some {
+      s 'std::string',
+      y 'int',
+    },
+    more {
+      d 'double',
+    },
+  }
+  */
 
-TEST_CASE( "[luapp/ext-refl] Reflected Variant API" ) {
+  st.lib.open_all();
+  define_usertype_for( st, tag<MySumtype>{} );
+  define_usertype_for( st, tag<MySumtype::none>{} );
+  define_usertype_for( st, tag<MySumtype::some>{} );
+  define_usertype_for( st, tag<MySumtype::more>{} );
+
+  auto constexpr script   = R"lua(
+    local s = ...
+    assert( s )
+    assert( s.xyz == nil )
+    assert( not pcall( function()
+      s.select_xyz()
+    end ) )
+    local some = s.some
+    assert( some.s == "hello" )
+    assert( some.y == 5 )
+    local more = s.select_more()
+    more.d = 4.5
+    assert( s.more.d == 4.5 )
+    return 42
+  )lua";
+  lua::rfunction const fn = st.script.load( script );
+
+  MySumtype s = MySumtype::some{ .s = "hello", .y = 5 };
+
+  REQUIRE( fn.pcall<int>( s ) == 42 );
+
+  REQUIRE( s == MySumtype::more{ .d = 4.5 } );
 }
 
 } // namespace
