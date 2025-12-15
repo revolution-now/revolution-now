ef4c098a0c7b96154625ae744858e7fe7800ac40
David P. Sicilia <dpsicilia@gmail.com>
2025-12-14 22:04:47 -0500

    Add unit tests for the charter module.

--- test/charter-test.cpp
+++ test/charter-test.cpp
@@ -11,10 +11,18 @@
 #include "test/testing.hpp"
 
 // Under test.
-// #include "src/charter.hpp"
+#include "src/charter.hpp"
 
 // Testing.
 #include "test/fake/world.hpp"
+#include "test/mocks/igui.hpp"
+#include "test/util/coro.hpp"
+
+// ss
+#include "src/ss/player.rds.hpp"
+#include "src/ss/ref.hpp"
+#include "src/ss/settings.rds.hpp"
+#include "src/ss/turn.rds.hpp"
 
 // Must be last.
 #include "test/catch-common.hpp" // IWYU pragma: keep
@@ -61,19 +69,172 @@ struct world : testing::World {
 ** Test Cases
 *****************************************************************/
 TEST_CASE( "[charter] charter_end_year" ) {
-  world w;
+  REQUIRE( charter_end_year() == 1600 );
 }
 
 TEST_CASE( "[charter] should_warn_about_charter_end" ) {
   world w;
+
+  auto const f = [&] [[clang::noinline]] (
+                     e_player const player_type =
+                         e_player::english ) {
+    return should_warn_about_charter_end( w.ss(), player_type );
+  };
+
+  w.turn().time_point.year = 0;
+  REQUIRE_FALSE( f() );
+
+  w.turn().time_point.year = 1574;
+  REQUIRE_FALSE( f() );
+
+  w.turn().time_point.year = 1575;
+  REQUIRE( f() );
+
+  w.turn().time_point.year = 1599;
+  REQUIRE( f() );
+
+  w.turn().time_point.year = 1600;
+  REQUIRE( f() );
+
+  w.turn().time_point.year = 1700;
+  REQUIRE( f() );
+
+  w.turn().time_point.year = 9999;
+  REQUIRE( f() );
+
+  w.add_player( e_player::french );
+  w.french().control = e_player_control::human;
+
+  w.turn().time_point.year = 9999;
+  REQUIRE_FALSE( f() );
+
+  w.french().control = e_player_control::inactive;
+  REQUIRE( f() );
+
+  REQUIRE_FALSE( f( e_player::french ) );
+
+  w.english().revolution.status = e_revolution_status::declared;
+  REQUIRE_FALSE( f() );
+
+  w.english().revolution.status =
+      e_revolution_status::not_declared;
+  REQUIRE( f() );
+
+  w.add_player( e_player::ref_english );
+  w.ref_english().control = e_player_control::human;
+  w.english().control     = e_player_control::inactive;
+  REQUIRE_FALSE( f( e_player::ref_english ) );
+
+  w.ref_english().control = e_player_control::inactive;
+  w.english().control     = e_player_control::human;
+  REQUIRE( f() );
 }
 
 TEST_CASE( "[charter] should_end_charter" ) {
   world w;
+
+  auto const f =
+      [&] [[clang::noinline]] ( e_player const player_type =
+                                    e_player::english ) {
+        return should_end_charter( w.ss(), player_type );
+      };
+
+  w.add_player( e_player::french );
+
+  SECTION( "no colonies" ) {
+    w.turn().time_point.year = 0;
+    REQUIRE_FALSE( f() );
+
+    w.turn().time_point.year = 1599;
+    REQUIRE_FALSE( f() );
+
+    w.turn().time_point.year = 1600;
+    REQUIRE( f() );
+
+    w.turn().time_point.year = 1700;
+    REQUIRE( f() );
+
+    w.turn().time_point.year = 1800;
+    REQUIRE( f() );
+  }
+
+  SECTION( "no colonies + two humans" ) {
+    w.french().control = e_player_control::human;
+
+    w.turn().time_point.year = 0;
+    REQUIRE_FALSE( f() );
+
+    w.turn().time_point.year = 1599;
+    REQUIRE_FALSE( f() );
+
+    w.turn().time_point.year = 1600;
+    REQUIRE_FALSE( f() );
+
+    w.turn().time_point.year = 1700;
+    REQUIRE_FALSE( f() );
+
+    w.turn().time_point.year = 1800;
+    REQUIRE_FALSE( f() );
+  }
+
+  SECTION( "with colonies" ) {
+    w.add_colony( { .x = 1, .y = 1 }, e_player::french );
+
+    w.turn().time_point.year = 0;
+    REQUIRE_FALSE( f() );
+
+    w.turn().time_point.year = 1599;
+    REQUIRE_FALSE( f() );
+
+    w.turn().time_point.year = 1600;
+    REQUIRE( f() );
+
+    w.turn().time_point.year = 1700;
+    REQUIRE( f() );
+
+    w.turn().time_point.year = 1800;
+    REQUIRE( f() );
+  }
+
+  SECTION( "with colonies" ) {
+    w.add_colony( { .x = 1, .y = 1 } );
+
+    w.turn().time_point.year = 0;
+    REQUIRE_FALSE( f() );
+
+    w.turn().time_point.year = 1599;
+    REQUIRE_FALSE( f() );
+
+    w.turn().time_point.year = 1600;
+    REQUIRE_FALSE( f() );
+
+    w.turn().time_point.year = 1700;
+    REQUIRE_FALSE( f() );
+
+    w.turn().time_point.year = 1800;
+    REQUIRE_FALSE( f() );
+  }
 }
 
-TEST_CASE( "[charter] do_end_charter" ) {
+TEST_CASE( "[charter] end_charter_ui_seq" ) {
   world w;
+
+  auto const f = [&] [[clang::noinline]] {
+    co_await_test( end_charter_ui_seq(
+        w.ss(), w.gui(), w.default_player_type() ) );
+  };
+
+  w.settings().game_setup_options.difficulty =
+      e_difficulty::explorer;
+  w.default_player().name = "Some Name";
+
+  w.gui().EXPECT__message_box(
+      "Explorer Some Name. Our time in the New World has "
+      "achieved little of consequence. We will be promptly "
+      "ending your tenure as Viceroy in favour of someone more "
+      "capable. It is, though, still your privilege to kiss our "
+      "royal pinky ring." );
+  f();
 }
 
 } // namespace
