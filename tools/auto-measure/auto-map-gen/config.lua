59dab5fdeaf04cdca6ac4b384a1436f45fbe997c
David P. Sicilia <dpsicilia@gmail.com>
2025-12-18 18:33:01 -0500

    Add some comments.

--- tools/auto-measure/auto-map-gen/config.lua
+++ tools/auto-measure/auto-map-gen/config.lua
@@ -1,8 +1,12 @@
 return {
   target_count=42, --
 
-  land_mass=1, --
-  land_form=1, --
+  -- Values:
+  --   -1 ==> up
+  --    0 ==> middle
+  --    1 ==> down
+  land_mass=-1, --
+  land_form=0, --
   temperature=0, --
   climate=0, --
 }
