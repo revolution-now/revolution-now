a27308caf5f557380853948d7bbe0bc73a9075ff
David P. Sicilia <dpsicilia@gmail.com>
2026-03-21 11:55:45 -0400

    Remove unused local.

--- tools/auto-measure/auto-map-gen/biomes/empirical.lua
+++ tools/auto-measure/auto-map-gen/biomes/empirical.lua
@@ -12,7 +12,6 @@ local plot = require( 'moon.plot' )
 -----------------------------------------------------------------
 local insert = table.insert
 local format = string.format
-local concat = table.concat
 local max = math.max
 
 -----------------------------------------------------------------
