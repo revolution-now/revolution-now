f98b818cf737249511d2d59dc22f107ada1c3cef
David P. Sicilia <dpsicilia@gmail.com>
2026-07-25 14:35:46 -0400

    Add a test case.

--- test/rand/perlin-test.cpp
+++ test/rand/perlin-test.cpp
@@ -21,10 +21,45 @@ namespace {
 
 using namespace std;
 
+using ::base::valid;
+using ::Catch::Contains;
+
 /****************************************************************
 ** Test Cases
 *****************************************************************/
 TEST_CASE( "[rng/perlin] PerlinFractalOptions" ) {
+  PerlinFractalOptions o{
+    .n_octaves   = 1,
+    .persistence = 1.0,
+    .lacunarity  = 1.0,
+  };
+
+  REQUIRE( o.validate() == valid );
+
+  o.n_octaves = 0;
+  REQUIRE_THAT( o.validate().error(),
+                Contains( "n_octaves must be >= 1"s ) );
+  o.n_octaves = 1;
+
+  o.persistence = -0.01;
+  REQUIRE_THAT( o.validate().error(),
+                Contains( "persistence must be >= 0"s ) );
+
+  o.persistence = 0.0;
+  REQUIRE( o.validate() == valid );
+
+  o.persistence = 0.0001;
+  REQUIRE( o.validate() == valid );
+
+  o.lacunarity = -0.01;
+  REQUIRE_THAT( o.validate().error(),
+                Contains( "lacunarity must be >= 0"s ) );
+
+  o.lacunarity = 0.0;
+  REQUIRE( o.validate() == valid );
+
+  o.lacunarity = 0.0001;
+  REQUIRE( o.validate() == valid );
 }
 
 TEST_CASE( "[rng/perlin] perlin_noise_2d" ) {
